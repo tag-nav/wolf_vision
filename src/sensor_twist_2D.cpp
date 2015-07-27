84b8dfe87a6af12e9503590eb295cce398f7e2cf
jvallve <jvallve@224674b8-e365-4e73-a4a8-558dbbfec58c>
2015-07-27 09:58:23 +0000

    implementant twist i v i w al states

---
+++ src/sensor_twist_2D.cpp
@@ -0,0 +1,22 @@
+#include "sensor_odom_2D.h"
+
+SensorTwist2D::SensorTwist2D(StatePoint3D* _p_ptr, StateOrientation* _o_ptr, const WolfScalar& _lineal_noise, const WolfScalar&  _angular_noise) :
+        SensorBase(TWIST_2D, _p_ptr, _o_ptr, 2)
+{
+    params_ << _lineal_noise, _angular_noise;
+}
+
+SensorTwist2D::~SensorTwist2D()
+{
+    //
+}
+
+WolfScalar SensorTwist2D::getLinealNoise() const
+{
+    return params_(0);
+}
+
+WolfScalar SensorTwist2D::getAngularNoise() const
+{
+    return params_(1);
+}
