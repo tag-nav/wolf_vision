cd50973d00bc3806cfdbfe6368782902822c0348
jsola <jsola@224674b8-e365-4e73-a4a8-558dbbfec58c>
2015-11-19 15:13:29 +0000

    Before changing name

--- src/capture_relative.cpp
+++ src/capture_relative.cpp
@@ -1,46 +1,46 @@
 #include "capture_relative.h"
 
-CaptureRelative::CaptureRelative(const TimeStamp& _init_ts, const TimeStamp& _final_ts, SensorBase* _sensor_ptr) :
+CaptureMotion::CaptureMotion(const TimeStamp& _init_ts, const TimeStamp& _final_ts, SensorBase* _sensor_ptr) :
     CaptureBase(_init_ts, _sensor_ptr),
     final_time_stamp_(_final_ts)
 {
     //
 }
 
-CaptureRelative::CaptureRelative(const TimeStamp& _init_ts, const TimeStamp& _final_ts, SensorBase* _sensor_ptr, const Eigen::VectorXs& _data) :
+CaptureMotion::CaptureMotion(const TimeStamp& _init_ts, const TimeStamp& _final_ts, SensorBase* _sensor_ptr, const Eigen::VectorXs& _data) :
 	CaptureBase(_init_ts, _sensor_ptr, _data),
     final_time_stamp_(_final_ts)
 {
 	//
 }
 
-CaptureRelative::CaptureRelative(const TimeStamp& _init_ts, const TimeStamp& _final_ts, SensorBase* _sensor_ptr, const Eigen::VectorXs& _data, const Eigen::MatrixXs& _data_covariance) :
+CaptureMotion::CaptureMotion(const TimeStamp& _init_ts, const TimeStamp& _final_ts, SensorBase* _sensor_ptr, const Eigen::VectorXs& _data, const Eigen::MatrixXs& _data_covariance) :
 	CaptureBase(_init_ts, _sensor_ptr, _data, _data_covariance),
     final_time_stamp_(_final_ts)
 {
 	//
 }
-CaptureRelative::~CaptureRelative()
+CaptureMotion::~CaptureMotion()
 {
 	//
 }
 
-TimeStamp CaptureRelative::getInitTimeStamp() const
+TimeStamp CaptureMotion::getInitTimeStamp() const
 {
     return this->getTimeStamp();
 }
 
-TimeStamp CaptureRelative::getFinalTimeStamp() const
+TimeStamp CaptureMotion::getFinalTimeStamp() const
 {
     return final_time_stamp_;
 }
 
-void CaptureRelative::setInitTimeStamp(const TimeStamp & _ts)
+void CaptureMotion::setInitTimeStamp(const TimeStamp & _ts)
 {
     this->setTimeStamp(_ts);
 }
 
-void CaptureRelative::setFinalTimeStamp(const TimeStamp & _ts)
+void CaptureMotion::setFinalTimeStamp(const TimeStamp & _ts)
 {
     final_time_stamp_ = _ts;
 }
--- src/capture_relative.h
+++ src/capture_relative.h
@@ -10,23 +10,23 @@
 #include "capture_base.h"
 
 //class CaptureBase
-class CaptureRelative : public CaptureBase
+class CaptureMotion : public CaptureBase
 {
     protected:
         TimeStamp final_time_stamp_; ///< Final Time stamp
 
     public:
-        CaptureRelative(const TimeStamp& _init_ts, const TimeStamp& _final_ts, SensorBase* _sensor_ptr);
+        CaptureMotion(const TimeStamp& _init_ts, const TimeStamp& _final_ts, SensorBase* _sensor_ptr);
         
-        CaptureRelative(const TimeStamp& _init_ts, const TimeStamp& _final_ts, SensorBase* _sensor_ptr, const Eigen::VectorXs& _data);
+        CaptureMotion(const TimeStamp& _init_ts, const TimeStamp& _final_ts, SensorBase* _sensor_ptr, const Eigen::VectorXs& _data);
 
-        CaptureRelative(const TimeStamp& _init_ts, const TimeStamp& _final_ts, SensorBase* _sensor_ptr, const Eigen::VectorXs& _data, const Eigen::MatrixXs& _data_covariance);
+        CaptureMotion(const TimeStamp& _init_ts, const TimeStamp& _final_ts, SensorBase* _sensor_ptr, const Eigen::VectorXs& _data, const Eigen::MatrixXs& _data_covariance);
 
-        virtual ~CaptureRelative();
+        virtual ~CaptureMotion();
 
-        virtual void integrateCapture(CaptureRelative* _new_capture) = 0;
+        virtual void integrateCapture(CaptureMotion* _new_capture) = 0;
 
-        virtual CaptureRelative* interpolateCapture(const TimeStamp& _ts) = 0;
+        virtual CaptureMotion* interpolateCapture(const TimeStamp& _ts) = 0;
 
         TimeStamp getInitTimeStamp() const;
 
