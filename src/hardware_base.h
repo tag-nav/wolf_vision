ef4c7e4e4805ede7c59cf61d8b75a2d2ad17c2d9
Joan Sola <jsola@iri.upc.edu>
2015-12-11 16:16:56 +0100

    Formatted tabs. No code change.

--- src/hardware_base.h
+++ src/hardware_base.h
@@ -1,4 +1,3 @@
-
 #ifndef HARDWARE_BASE_H_
 #define HARDWARE_BASE_H_
 
@@ -10,7 +9,7 @@
 #include "sensor_base.h"
 
 //class HardwareBase
-class HardwareBase : public NodeLinked<WolfProblem,SensorBase>
+class HardwareBase : public NodeLinked<WolfProblem, SensorBase>
 {
     public:
         /** \brief Constructor
@@ -18,34 +17,34 @@ class HardwareBase : public NodeLinked<WolfProblem,SensorBase>
          * Constructor
          *
          **/
-		HardwareBase();
+        HardwareBase();
 
         /** \brief Default destructor (not recommended)
          *
          * Default destructor (please use destruct() instead of delete for guaranteeing the wolf tree integrity)
          *
-         **/        
+         **/
         ~HardwareBase();
-        
+
         /** \brief Adds a sensor
-		 *
-		 * Adds a sensor
-		 *
-		 **/
+         *
+         * Adds a sensor
+         *
+         **/
         virtual void addSensor(SensorBase* _sensor_ptr);
 
         /** \brief Removes a sensor
-		 *
-		 * Removes a sensor
-		 *
-		 **/
+         *
+         * Removes a sensor
+         *
+         **/
         void removeSensor(const SensorBaseIter& _sensor_iter);
 
         /** \brief Removes a sensor
-		 *
-		 * Removes a sensor
-		 *
-		 **/
+         *
+         * Removes a sensor
+         *
+         **/
         void removeSensor(SensorBase* _sensor_ptr);
 
         /** \brief Returns Sensor list
@@ -54,6 +53,6 @@ class HardwareBase : public NodeLinked<WolfProblem,SensorBase>
          * 
          **/
         SensorBaseList* getSensorListPtr();
-        
+
 };
 #endif
