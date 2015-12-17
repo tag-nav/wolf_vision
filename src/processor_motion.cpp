07c53ea8c4ce0660f348eec2100408f4a64c6460
Joan Sola <jsola@iri.upc.edu>
2015-12-17 16:31:47 +0100

    added some methods without defining them yet

--- src/processor_motion.cpp
+++ src/processor_motion.cpp
@@ -11,3 +11,11 @@ ProcessorMotion::~ProcessorMotion()
 void ProcessorMotion::integrate(TimeStamp& _time_stamp)
 {
 }
+
+void ProcessorMotion::computeRelativeState(TimeStamp& _t_start, TimeStamp& _t_end)
+{
+}
+
+void ProcessorMotion::computeAbsoluteState(TimeStamp& _time_stamp)
+{
+}
--- src/processor_motion.h
+++ src/processor_motion.h
@@ -15,5 +15,7 @@ class ProcessorMotion : public ProcessorBase{
         ProcessorMotion();
         virtual ~ProcessorMotion();
         void integrate(TimeStamp& _time_stamp);
+        void computeRelativeState(TimeStamp& _t_start, TimeStamp& _t_end);
+        void computeAbsoluteState(TimeStamp& _time_stamp);
 };
 #endif /* SRC_PROCESSOR_MOTION_H_ */
