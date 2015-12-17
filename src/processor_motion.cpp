73ee62a66a2ed179dbf9e2390f18adf0fde03841
Joan Sola <jsola@iri.upc.edu>
2015-12-17 15:45:24 +0100

    start work on motion processors

---
+++ src/processor_motion.cpp
@@ -0,0 +1,13 @@
+#include "processor_motion.h"
+
+ProcessorMotion::ProcessorMotion()
+{
+}
+
+ProcessorMotion::~ProcessorMotion()
+{
+}
+
+void ProcessorMotion::integrate(TimeStamp& _time_stamp)
+{
+}
