64706328354ffd34701d64cc1de19c5f98885190
Joan Sola <jsola@iri.upc.edu>
2016-02-28 15:35:17 +0100

    Clean up documentation

--- src/state_block.h
+++ src/state_block.h
@@ -24,74 +24,40 @@ class StateBlock
     public:
         /** \brief Constructor with scalar pointer
          * 
-         * Constructor with scalar pointer
          * \param _state is state vector
-         * \param _type parametrization of the state
          * \param _fixed Indicates this state is not estimated and thus acts as a fixed parameter
-         * 
          **/
         StateBlock(const Eigen::VectorXs _state, bool _fixed = false);
         
         /** \brief Destructor
-         * 
-         * Destructor
-         * 
          **/
         virtual ~StateBlock();
 
         /** \brief Returns the pointer to the first element of the state
-         *
-         * Returns the scalar pointer to the first element of the state
-         *
          **/
         WolfScalar* getPtr();
         
         /** \brief Returns the state vector
-         *
-         * Returns the state vector
-         *
          **/
         Eigen::VectorXs getVector();
 
         /** \brief Sets the state vector
-         *
-         * Sets the state vector
-         *
          **/
         void setVector(const Eigen::VectorXs& _state);
 
-//        /** \brief Returns the parametrization of the state
-//         *
-//         * Returns the parametrizationType (see wolf.h) of the state
-//         *
-//         **/
-//        virtual StateType getType() const;
-
         /** \brief Returns the state size
-         *
-         * Returns the state size
-         *
          **/
         unsigned int getSize() const;
 
         /** \brief Returns if the state is fixed (not estimated)
-         *
-         * Returns if the state is fixed (not estimated)
-         *
          **/
         bool isFixed() const;
 
         /** \brief Sets the state as fixed
-         *
-         * Sets the state as fixed
-         *
          **/
         void fix();
 
         /** \brief Sets the state as estimated
-         *
-         * Sets the state as estimated
-         *
          **/
         void unfix();
 
@@ -100,9 +66,6 @@ class StateBlock
         LocalParametrizationBase* getLocalParametrizationPtr();
 
         /** \brief Prints all the elements of the state unit
-         *
-         * Prints all the elements of the state unit
-         *
          **/
         virtual void print(unsigned int _ntabs = 0, std::ostream& _ost = std::cout) const;
 };
