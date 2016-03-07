a5faeee2dc667da049fa5ecf77035af8c5e43256
Joan Sola <jsola@iri.upc.edu>
2016-03-07 18:06:55 +0100

    Test of stateQuaternion

---
+++ src/examples/test_state_quaternion.cpp
@@ -0,0 +1,37 @@
+/*
+ * \file test_state_quaternion.cpp
+ *
+ *  Created on: Mar 7, 2016
+ *      \author: jsola
+ */
+
+
+#include "frame_base.h"
+#include "state_quaternion.h"
+#include "time_stamp.h"
+
+#include <iostream>
+
+int main (void)
+{
+    // 3D
+    StateBlock* pp = new StateBlock(3);
+    StateQuaternion* op = new StateQuaternion;
+    StateBlock* vp = new StateBlock(3);
+
+    TimeStamp t;
+
+    FrameBase pqv(t,pp,op,vp);
+
+    std::cout << "P local param: " << pqv.getPPtr()->getLocalParametrizationPtr() << std::endl;
+    std::cout << "Q local param: " << pqv.getOPtr()->getLocalParametrizationPtr() << std::endl;
+    std::cout << "V local param: " << pqv.getVPtr()->getLocalParametrizationPtr() << std::endl;
+
+    delete pp;
+    delete op;
+    delete vp;
+
+    std::cout << "Done" << std::endl;
+
+    return 1;
+}
