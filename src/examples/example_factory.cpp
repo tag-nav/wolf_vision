6b0d54d2c6b9c6bf4d51b23fae2181652ca9b329
Joan Sola <jsola@iri.upc.edu>
2016-03-12 13:06:25 +0100

    Factory example
    Based on Modern C++ Design: Generic Programming and Design Patterns
    Applied By Andrei Alexandrescu

--- src/examples/example_factory.cpp
+++ src/examples/example_factory.cpp
@@ -1,9 +1,40 @@
-/*
- * \landmark_factory_test.cpp
+/**
+ * \file example_factory.cpp
  *
  *  Created on: 09/03/2016
  *      \author: jsola
  */
 
-#include "landmark_factory.h"
 
+#include "example_factory.h"
+
+#include <list>
+
+int main() {
+
+    std::list<Shape*> shapes;
+
+    shapes.push_back(ShapeFactory::instance()->createShape(Shape::POINT));
+    shapes.push_back(ShapeFactory::instance()->createShape(Shape::POINT));
+    shapes.push_back(ShapeFactory::instance()->createShape(Shape::POINT));
+    shapes.push_back(ShapeFactory::instance()->createShape(Shape::LINE));
+    shapes.push_back(ShapeFactory::instance()->createShape(Shape::POINT));
+    shapes.push_back(ShapeFactory::instance()->createShape(Shape::LINE));
+    shapes.push_back(ShapeFactory::instance()->createShape(Shape::LINE));
+    shapes.push_back(ShapeFactory::instance()->createShape(Shape::LINE));
+    shapes.push_back(ShapeFactory::instance()->createShape(Shape::POINT));
+    shapes.push_back(ShapeFactory::instance()->createShape(Shape::POINT));
+    shapes.push_back(ShapeFactory::instance()->createShape(Shape::LINE));
+    shapes.push_back(ShapeFactory::instance()->createShape(Shape::LINE));
+
+    for (auto shape : shapes){
+        shape->draw();
+    }
+    (*(shapes.rbegin()))->draw();
+
+    std::cout << "Asking for a CIRCLE which did not register to the factory..." << std::endl;
+    shapes.push_back(ShapeFactory::instance()->createShape(Shape::CIRCLE));
+    (*(shapes.rbegin()))->draw();
+
+    return 0;
+}
---
+++ src/examples/example_factory.h
@@ -0,0 +1,182 @@
+/*
+ * \example_factory.h
+ *
+ *  Created on: 11/03/2016
+ *      \author: jsola
+ */
+
+#ifndef EXAMPLE_FACTORY_H_
+#define EXAMPLE_FACTORY_H_
+
+#include <map>
+
+class Shape
+{
+    public:
+        typedef enum
+        {
+            POINT, LINE, CIRCLE
+        } Type;
+    public:
+        virtual ~Shape();
+        virtual void draw() const = 0;
+    protected:
+        int id_;
+};
+
+class Point : public Shape
+{
+    public:
+        Point();
+        virtual ~Point();
+        virtual void draw() const;
+        Shape* createPoint();
+    private:
+        int id_;
+        static int id_count_;
+};
+
+class Line : public Shape
+{
+    public:
+        Line();
+        virtual ~Line();
+        virtual void draw() const;
+    private:
+        int id_;
+        static int id_count_;
+};
+
+class ShapeFactory
+{
+    public:
+        typedef Shape* (*CreateShapeCallback)();
+    private:
+        typedef std::map<Shape::Type, CreateShapeCallback> CallbackMap;
+    public:
+        // Returns 'true' if registration was successful
+        bool registerShape(Shape::Type _shape_type, CreateShapeCallback createFn);
+        // Returns 'true' if the _shape_type was registered before
+        bool unregisterShape(Shape::Type _shape_type);
+        Shape* createShape(Shape::Type _shape_type);
+    private:
+        CallbackMap callbacks_;
+
+        // Singleton
+    public:
+        static ShapeFactory* instance(); // Unique point of access;
+    private:
+        ShapeFactory()
+        {
+        }
+        // Prevent clients from creating a new Singleton
+        ShapeFactory(const ShapeFactory&)
+        {
+        }
+        // Prevent clients from creating a copy of the Singleton
+        static ShapeFactory* pInstance_;
+};
+
+// IMPLEMENTATION //
+
+#include <iostream>
+#include <stdexcept>
+
+// Point ----------------------------------------------------------------------
+
+int Point::id_count_ = 0;
+
+inline Point::Point() :
+        id_(++id_count_)
+{
+}
+
+inline Point::~Point()
+{
+}
+
+inline Shape::~Shape()
+{
+}
+
+inline void Point::draw() const
+{
+    std::cout << "point " << id_ << std::endl;
+}
+
+inline Shape* Point::createPoint()
+{
+    return new Point;
+}
+
+namespace
+{
+Shape* CreatePoint()
+{
+    return new Point;
+}
+const bool registered = ShapeFactory::instance()->registerShape(Shape::POINT, CreatePoint);
+}
+
+// Line -----------------------------------------------------------------------
+
+int Line::id_count_ = 0;
+
+inline Line::Line() :
+        id_(++id_count_)
+{
+}
+
+inline Line::~Line()
+{
+}
+
+inline void Line::draw() const
+{
+    std::cout << "line  " << id_ << std::endl;
+}
+
+namespace
+{
+Shape* CreateLine()
+{
+    return new Line;
+}
+bool registered2 = ShapeFactory::instance()->registerShape(Shape::LINE, CreateLine);
+}
+
+// ShapeFactory ---------------------------------------------------------------
+
+ShapeFactory* ShapeFactory::pInstance_ = nullptr;
+
+inline ShapeFactory* ShapeFactory::instance() // Unique point of access
+{
+    if (!pInstance_)
+        pInstance_ = new ShapeFactory;
+
+    return pInstance_;
+}
+
+inline bool ShapeFactory::registerShape(Shape::Type _shape_type, CreateShapeCallback createFn)
+{
+    return callbacks_.insert(CallbackMap::value_type(_shape_type, createFn)).second;
+}
+
+inline bool ShapeFactory::unregisterShape(Shape::Type _shape_type)
+{
+    return callbacks_.erase(_shape_type) == 1;
+}
+
+inline Shape* ShapeFactory::createShape(Shape::Type _shape_type)
+{
+    CallbackMap::const_iterator i = callbacks_.find(_shape_type);
+    if (i == callbacks_.end())
+    {
+        // not found
+        throw std::runtime_error("Unknown Shape type");
+    }
+    // Invoke the creation function
+    return (i->second)();
+}
+
+#endif /* EXAMPLE_FACTORY_H_ */
