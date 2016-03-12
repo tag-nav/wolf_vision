91c98da938f4f967c2fa0687e49853267e4c2b59
Joan Sola <jsola@iri.upc.edu>
2016-03-12 16:21:47 +0100

    WIP on the API of ProcessorMotion
    Now using Eigen::VectorXs& as in/out parameters of most function
    compositions. This seems to be not generic enough, but it's a try.

--- src/processor_motion.h
+++ src/processor_motion.h
@@ -9,20 +9,26 @@
 #define SRC_PROCESSOR_MOTION_H_
 
 #include "time_stamp.h"
+#include "wolf.h"
 
 class ProcessorMotion : public ProcessorBase{
     public:
         ProcessorMotion();
         virtual ~ProcessorMotion();
 
-        // TODO: These thre fncns will use plus() and minus() below
+        // TODO: Implement the pure virtual from base
+        virtual void process(CaptureBase* _capture_ptr){};
+
+        // TODO: These three functions will use plus() and minus() below
         void integrate(TimeStamp& _time_stamp){};
-        void getDeltaState(TimeStamp& _t_start, TimeStamp& _t_end){};
-        void getState(TimeStamp& _time_stamp){};
+        void composeDeltaState(const TimeStamp& _t_start, TimeStamp& _t_end, Eigen::VectorXs&){};
+        void composeState(const TimeStamp& _time_stamp, Eigen::VectorXs&){};
 
     protected:
         // TODO: Think about the API of these:
-        void plus() = 0;
-        void minus() = 0;
+        virtual void plus(const Eigen::VectorXs& _x, const Eigen::VectorXs& _delta, Eigen::VectorXs& _x_plus_delta) = 0;
+        virtual void minus(const Eigen::VectorXs& _x1, const Eigen::VectorXs& _x0, Eigen::VectorXs& _x1_minus_x0) = 0;
+
+        void composeOriginState(Eigen::VectorXs&);
 };
 #endif /* SRC_PROCESSOR_MOTION_H_ */
