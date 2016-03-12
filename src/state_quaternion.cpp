dfe273cb82366982f8cb593bb11d26afe7d1bba4
Joan Sola <jsola@iri.upc.edu>
2016-03-12 23:01:35 +0100

    Add default local parametrization to StateBlock constructor
    It defaults to nullptr

--- src/state_quaternion.cpp
+++ src/state_quaternion.cpp
@@ -9,17 +9,15 @@
 #include "local_parametrization_quaternion.h"
 
 StateQuaternion::StateQuaternion(const Eigen::VectorXs _state, bool _fixed) :
-        StateBlock(_state, _fixed)
+        StateBlock(_state, _fixed, new LocalParametrizationQuaternion)
 {
     assert(_state.size() == 4 && "Quaternion must be size 4.");
-    local_param_ptr_ = new LocalParametrizationQuaternion;
 }
 
 StateQuaternion::StateQuaternion(bool _fixed) :
-        StateBlock(4, _fixed)
+        StateBlock(4, _fixed, new LocalParametrizationQuaternion)
 {
-    local_param_ptr_ = new LocalParametrizationQuaternion;
-
+    //
 }
 
 StateQuaternion::~StateQuaternion()
