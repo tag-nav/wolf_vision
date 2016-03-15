c403347adcd83be9a263c8f788171c1160df6f96
Joan Sola <jsola@iri.upc.edu>
2016-03-15 10:19:30 +0100

    Rearrange file for better reading

--- src/examples/example_template_factory.cpp
+++ src/examples/example_template_factory.cpp
@@ -12,128 +12,203 @@
 // general includes
 #include <iostream>
 
+// In their respective object .h and .cpp files
 // things constrained == anything, deriving from a base type
-class Object{}; //  type base
-class ObjectA:public Object{}; // type A
-class ObjectB:public Object{}; // type B
-class ObjectC:public Object{}; // type C
+//  type base
+class Object
+{
+};
+// type A
+class ObjectA : public Object
+{
+};
+// type B
+class ObjectB : public Object
+{
+};
+// type C
+class ObjectC : public Object
+{
+};
 
-// c_base.h
+//----------------------------------------------------------------------
+// In constraint_base.h
 class Constraint
 {
     public:
-        virtual ~Constraint(){}
+        virtual ~Constraint()
+        {
+        }
         virtual void print()
         {
             std::cout << "constraint base" << std::endl;
         }
 };
-
-// factory.h
+// Default factory throws
 template<typename SelfType, typename OtherType>
-class ConstraintFactory{
+class ConstraintFactory
+{
     public:
         static Constraint* newConstraint()
         {
-            throw std::invalid_argument("Unknown Constraint between types ");// << typeid(SelfType).name() << " and " << typeid(SelfType).name() << "!");
+            throw std::invalid_argument("Unknown Constraint between types "); // << typeid(SelfType).name() << " and " << typeid(SelfType).name() << "!");
         }
 };
 
-// c_a_b.h
-class ConstraintAB : public Constraint{virtual void print(){std::cout << "constraint A-B" << std::endl;}}; // constraint A-B
-
+//----------------------------------------------------------------------
+// in c_a_b.h
+// constraint A-B
+class ConstraintAB : public Constraint
+{
+        virtual void print()
+        {
+            std::cout << "constraint A-B" << std::endl;
+        }
+};
 template<>
-class ConstraintFactory<ObjectA, ObjectB>{
+class ConstraintFactory<ObjectA, ObjectB>
+{
     public:
-        static Constraint* newConstraint(){
+        static Constraint* newConstraint()
+        {
             return new ConstraintAB;
         }
 };
 
-// c_a_c.h
-class ConstraintAC : public Constraint{virtual void print(){std::cout << "constraint A-C" << std::endl;}}; // constraint A-C
-
+//----------------------------------------------------------------------
+// In c_a_c.h
+// constraint A-C
+class ConstraintAC : public Constraint
+{
+        virtual void print()
+        {
+            std::cout << "constraint A-C" << std::endl;
+        }
+};
 template<>
-class ConstraintFactory<ObjectA, ObjectC>{
+class ConstraintFactory<ObjectA, ObjectC>
+{
     public:
-       static Constraint* newConstraint(){
+        static Constraint* newConstraint()
+        {
             return new ConstraintAC;
         }
 };
 
-// c_b_c.h
-class ConstraintBC : public Constraint{virtual void print(){std::cout << "constraint B-C" << std::endl;}}; // constraint B-C
-
+//----------------------------------------------------------------------
+// In c_b_c.h
+// constraint B-C
+class ConstraintBC : public Constraint
+{
+        virtual void print()
+        {
+            std::cout << "constraint B-C" << std::endl;
+        }
+};
 template<>
-class ConstraintFactory<ObjectB, ObjectC>{
+class ConstraintFactory<ObjectB, ObjectC>
+{
     public:
-      static Constraint* newConstraint(){
+        static Constraint* newConstraint()
+        {
             return new ConstraintBC;
         }
 };
 
-// p_base.h
+//----------------------------------------------------------------------
+// In p_base.h
 class Processor
 {
     public:
-        virtual ~Processor(){};
+        virtual ~Processor()
+        {
+        }
+        ;
         template<typename SelfType, typename OtherType>
-        void process(SelfType* _self_ptr, OtherType* _other_ptr){
-            Constraint* c_ptr = ConstraintFactory<SelfType,OtherType>::newConstraint();
+        void process(SelfType* _self_ptr, OtherType* _other_ptr)
+        {
+            Constraint* c_ptr = ConstraintFactory<SelfType, OtherType>::newConstraint();
             c_ptr->print();
         }
 };
 
-// p_a_b.h : has no derived pointers as members
-class ProcessorAB : public Processor{
+//----------------------------------------------------------------------
+// In p_a_b.h : has no derived pointers as members
+class ProcessorAB : public Processor
+{
     public:
-        virtual ~ProcessorAB(){};
+        virtual ~ProcessorAB()
+        {
+        }
 };
 
-// p_a_c.h : has one derived pointer ads member
-class ProcessorAC : public Processor{
+//----------------------------------------------------------------------
+// In p_a_c.h : has one derived pointer ads member
+class ProcessorAC : public Processor
+{
     public:
         ObjectA* ta_;
-        ProcessorAC(Object* _ta):ta_((ObjectA*)_ta){}
-        void process(Object* _tc){Processor::process(ta_,(ObjectC*)_tc);}
-        virtual ~ProcessorAC(){};
+        ProcessorAC(Object* _ta) :
+                ta_((ObjectA*)_ta)
+        {
+        }
+        void process(Object* _tc)
+        {
+            Processor::process(ta_, (ObjectC*)_tc); // cast needed
+        }
+        virtual ~ProcessorAC()
+        {
+        }
 };
 
-// p_b_c.h : has both derived pointers as members
-class ProcessorBC : public Processor{
+//----------------------------------------------------------------------
+// In p_b_c.h : has both derived pointers as members
+class ProcessorBC : public Processor
+{
     public:
         ObjectB* tb_;
         ObjectC* tc_;
-        ProcessorBC(Object* _tb, Object* _tc):tb_((ObjectB*)_tb),tc_((ObjectC*)_tc){}
-        void process(){Processor::process(tb_,tc_);}
-        virtual ~ProcessorBC(){};
+        ProcessorBC(Object* _tb, Object* _tc) :
+                tb_((ObjectB*)_tb), tc_((ObjectC*)_tc)
+        {
+        }
+        void process()
+        {
+            Processor::process(tb_, tc_);
+        }
+        virtual ~ProcessorBC()
+        {
+        }
 };
 
+//----------------------------------------------------------------------
+// In application.cpp
 int main()
 {
-    Object* a = new ObjectA;
-    Object* b = new ObjectB;
-    Object* c = new ObjectC;
+    Object* a_p = new ObjectA;
+    Object* b_p = new ObjectB;
+    Object* c_p = new ObjectC;
 
     ProcessorAB pab;
-    ProcessorAC pac(a);
-    ProcessorBC pbc(b,c);
+    ProcessorAC pac(a_p);
+    ProcessorBC pbc(b_p, c_p);
 
     Processor* pab_p = new ProcessorAB;
-    Processor* pac_p = new ProcessorAC(a);
-    Processor* pbc_p = new ProcessorBC(b,c);
+    Processor* pac_p = new ProcessorAC(a_p);
+    Processor* pbc_p = new ProcessorBC(b_p, c_p);
 
     std::cout << "object processors:" << std::endl;
-    pab.process((ObjectA*)a,(ObjectB*)b); // This use base process() --> need to cast when calling
-    pac.process(c); // This uses overloaded process() --> casted by the class
+    pab.process((ObjectA*)a_p, (ObjectB*)b_p); // This use base process() --> need to cast when calling
+    pac.process(c_p); // This uses overloaded process() --> casted by the class
     pbc.process(); // This uses overloaded process() --> casted by the class
 
     std::cout << "\npointers to processors:" << std::endl;
-    pab_p->process((ObjectA*)a,(ObjectB*)b); // This use base process() --> need to cast when calling
-    pac_p->process((ObjectA*)a,(ObjectC*)c);
-    pbc_p->process((ObjectB*)b,(ObjectC*)c);
+    pab_p->process((ObjectA*)a_p, (ObjectB*)b_p); // This uses base process() --> need to cast when calling
+    pac_p->process((ObjectA*)a_p, (ObjectC*)c_p);
+    pbc_p->process((ObjectB*)b_p, (ObjectC*)c_p);
 
-//    pab.process(tb,ta);
+    std::cout << "\ncall with base pointers->throw:" << std::endl;
+    pab.process(a_p,b_p);
 
     return 0;
 }
