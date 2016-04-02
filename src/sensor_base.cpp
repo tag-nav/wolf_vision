252ca91b881e0f4ca9761c191d22348b9492d8d3
Joan Sola <jsola@iri.upc.edu>
2016-04-02 16:50:13 +0200

    Fix warning about uninitialized type_ in SensorBase constructor

--- src/sensor_base.cpp
+++ src/sensor_base.cpp
@@ -20,6 +20,7 @@ SensorBase::SensorBase(const SensorType& _tp, StateBlock* _p_ptr, StateBlock* _o
 SensorBase::SensorBase(const SensorType & _tp, StateBlock* _p_ptr, StateBlock* _o_ptr, StateBlock* _intr_ptr,
                        const Eigen::VectorXs & _noise_std, const bool _extr_dyn) :
         NodeLinked(MID, "SENSOR"),
+        type_(_tp),
         p_ptr_(_p_ptr),
         o_ptr_(_o_ptr),
         intrinsic_ptr_(_intr_ptr),
