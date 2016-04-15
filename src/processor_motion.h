0a54a52004a1e142828bc25d37ececdd6170fbb3
Joan Sola <jsola@iri.upc.edu>
2016-04-15 12:08:54 +0200

    Remove ProcessorMotionBase

--- src/processor_motion.h
+++ src/processor_motion.h
@@ -9,7 +9,7 @@
 #define PROCESSOR_MOTION_H_
 
 // Wolf
-#include "processor_motion_base.h"
+#include "processor_motion.h"
 #include "capture_motion2.h"
 #include "motion_buffer.h"
 #include "time_stamp.h"
