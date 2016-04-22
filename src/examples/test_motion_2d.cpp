94d85fc64541bad4101aa5907ffa7c70408a2b56
joanvallve <jvallve@iri.upc.edu>
2016-04-22 16:50:32 +0200

    test motion 2d changed to new api

--- src/examples/test_motion_2d.cpp
+++ src/examples/test_motion_2d.cpp
@@ -35,8 +35,8 @@ int main()
     Scalar dt = .01;
 
     // Origin frame:
-    Eigen::Vector2s p0 = Eigen::Vector2s::Zero();
-    Eigen::Vector1s o0 = Eigen::Vector1s::Zero();
+    Eigen::Vector2s p0(0.5, -0.5-sqrt(0.5));
+    Eigen::Vector1s o0(Eigen::Vector1s::Constant(Constants::PI/4));
     Eigen::Vector3s x0;
     x0 << p0, o0;
 
@@ -46,7 +46,7 @@ int main()
 
     // motion data
     Eigen::VectorXs data(2);
-    data << 1, 0.1;  // advance 1m turn 0.1 rad (aprox 6 deg)
+    data << 1, Constants::PI/4;  // advance 1m turn 0.1 rad (aprox 6 deg)
     Eigen::MatrixXs data_cov = Eigen::MatrixXs::Identity(2,2) * 0.01;
 
     // Create Wolf tree nodes
@@ -60,40 +60,92 @@ int main()
     // Initialize
     odom2d_ptr->setOrigin(x0, t0);
 
-//    FrameBase* frm_ptr = new FrameBase(t, &robot_p_, &robot_o_);
-//    std::cout << "frame created" << std::endl;
-//    CaptureMotion2* cap_ptr = new CaptureMotion2(t, sensor_ptr, data, data_cov);
-//    std::cout << "capture created" << std::endl;
-//
-//    frm_ptr->addCapture(cap_ptr);
-//    std::cout << "capture added" << std::endl;
-
     std::cout << "Initial pose : " << problem_ptr->getLastFramePtr()->getState().transpose() << std::endl;
     std::cout << "Motion data  : " << data.transpose() << std::endl;
 
     std::cout << "\nIntegrating states at synchronous time values..." << std::endl;
 
     std::cout << "State(" << (t-t0) << ") : " << odom2d_ptr->getState().transpose() << std::endl;
-//    for (int i = 1; i <= 5; i++)
-//    {
-//        t += dt;
-//        cap_ptr->setTimeStamp(t);
-//        odom2d_ptr->process(cap_ptr);
-//        std::cout << "State(" << (t-t0) << ") : " << odom2d_ptr->getState().transpose() << std::endl;
-//    }
-//
-//    std::cout << "\nQuery states at asynchronous time values..." << std::endl;
-//
-//    t = t0;
-//    Scalar dt_2 = dt/2;
-//    dt = 0.0045; // new dt
-//    for (int i = 1; i <= 20; i++)
-//    {
-//        std::cout << "State(" << (t-t0) << ") = " << odom2d_ptr->getState(t+dt_2).transpose() << std::endl;
-//        t += dt;
-//    }
-//    std::cout << "       ^^^^^^^   After the last time-stamp the buffer keeps returning the last member." << std::endl;
-//
-//
-//    return 0;
+    // Capture to use as container for all incoming data
+    t += dt;
+    CaptureMotion2* cap_ptr = new CaptureMotion2(t, sensor_ptr, data, data_cov);
+
+    for (int i = 0; i <= 8; i++)
+    {
+        odom2d_ptr->process(cap_ptr);
+        std::cout << "State(" << (t-t0) << ") : " << odom2d_ptr->getState().transpose() << std::endl;
+
+        t += dt;
+        cap_ptr->setTimeStamp(t);
+    }
+
+    std::cout << "\nQuery states at asynchronous time values..." << std::endl;
+
+    t = t0;
+    dt = 0.0045; // new dt
+    for (int i = 1; i <= 25; i++)
+    {
+        std::cout << "State(" << (t-t0) << ") = " << odom2d_ptr->getState(t/*+dt/2*/).transpose() << std::endl;
+        t += dt;
+    }
+    std::cout << "       ^^^^^^^   After the last time-stamp the buffer keeps returning the last member." << std::endl;
+
+
+
+    // Split the buffer
+
+    std::cout << "\nSplitting the buffer!\n---------------------" << std::endl;
+    std::cout << "Original buffer:           < ";
+    for (const auto &s : odom2d_ptr->getBufferPtr()->get() )
+        std::cout << s.ts_ - t0 << ' ';
+    std::cout << ">" << std::endl;
+
+    // first split at non-exact timestamp
+    TimeStamp t_split = t0 + 0.033;
+    std::cout << "Split time:                  " << t_split - t0 << std::endl;
+
+    FrameBase* new_keyframe_ptr = problem_ptr->createFrame(KEY_FRAME,odom2d_ptr->getState(t_split), t_split);
+
+    odom2d_ptr->keyFrameCallback(new_keyframe_ptr);
+
+
+    std::cout << "New buffer: oldest part:   < ";
+    for (const auto &s : ((CaptureMotion2*)(new_keyframe_ptr->getCaptureListPtr()->front() ))->getBufferPtr()->get() )
+        std::cout << s.ts_ - t0 << ' ';
+    std::cout << ">" << std::endl;
+
+    std::cout << "Original keeps the newest: < ";
+    for (const auto &s : odom2d_ptr->getBufferPtr()->get() )
+        std::cout << s.ts_ - t0 << ' ';
+    std::cout << ">" << std::endl;
+
+    std::cout << "All in one row:            < ";
+    for (const auto &s : ((CaptureMotion2*)(new_keyframe_ptr->getCaptureListPtr()->front() ))->getBufferPtr()->get() )
+        std::cout << s.ts_ - t0 << ' ';
+    std::cout << "> " << t_split - t0 <<   " < ";
+    for (const auto &s : odom2d_ptr->getBufferPtr()->get() )
+        std::cout << s.ts_ - t0 << ' ';
+    std::cout << ">" << std::endl;
+
+    // second split as exact timestamp
+
+    t_split = t0 + 0.07;
+    std::cout << "New split time:              " << t_split - t0 << std::endl;
+
+    new_keyframe_ptr = problem_ptr->createFrame(KEY_FRAME,odom2d_ptr->getState(t_split), t_split);
+    odom2d_ptr->keyFrameCallback(new_keyframe_ptr);
+
+    std::cout << "All in one row:            < ";
+    for (const auto &s : ((CaptureMotion2*)(new_keyframe_ptr->getCaptureListPtr()->front() ))->getBufferPtr()->get() )
+        std::cout << s.ts_ - t0 << ' ';
+    std::cout << "> " << t_split - t0 <<   " < ";
+    for (const auto &s : odom2d_ptr->getBufferPtr()->get() )
+        std::cout << s.ts_ - t0 << ' ';
+    std::cout << ">" << std::endl;
+
+
+    // Free allocated memory
+    cap_ptr->destruct();
+
+    return 0;
 }
