15102401b507b4fbafb84d82ebede0fbb076c143
joanvallve <jvallve@iri.upc.edu>
2016-04-27 17:59:31 +0200

    Changed to premultiplying by square root information matrix instead of dividing each residual by the sqrt of the diagonal element

--- src/constraint_corner_2D.h
+++ src/constraint_corner_2D.h
@@ -53,69 +53,66 @@ template<typename T>
 inline bool ConstraintCorner2D::operator ()(const T* const _robotP, const T* const _robotO, const T* const _landmarkP,
                                             const T* const _landmarkO, T* _residuals) const
 {
-    //TODO: Not computing the transformations each iteration
     // Mapping
-    Eigen::Map<const Eigen::Matrix<T, 2, 1> > landmark_position(_landmarkP);
-    Eigen::Map<const Eigen::Matrix<T, 2, 1> > robot_position(_robotP);
-    //			std::cout << "getSensorPosition: " << std::endl;
-    //			std::cout << getCapturePtr()->getSensorPtr()->getSensorPosition()->head(2).transpose() << std::endl;
-    //			std::cout << "getSensorRotation: " << std::endl;
-    //			std::cout << getCapturePtr()->getSensorPtr()->getSensorRotation()->topLeftCorner<2,2>() << std::endl;
-    //			std::cout << "atan2: " << atan2(getCapturePtr()->getSensorPtr()->getSensorRotation()->transpose()(0,1),getCapturePtr()->getSensorPtr()->getSensorRotation()->transpose()(0,0)) << std::endl;
+    Eigen::Map<const Eigen::Matrix<T,2,1>> landmark_position_map(_landmarkP);
+    Eigen::Map<const Eigen::Matrix<T,2,1>> robot_position_map(_robotP);
+    Eigen::Map<Eigen::Matrix<T,3,1>> residuals_map(_residuals);
+
+    //std::cout << "getSensorPosition: " << std::endl;
+    //std::cout << getCapturePtr()->getSensorPtr()->getSensorPosition()->head(2).transpose() << std::endl;
+    //std::cout << "getSensorRotation: " << std::endl;
+    //std::cout << getCapturePtr()->getSensorPtr()->getSensorRotation()->topLeftCorner<2,2>() << std::endl;
+    //std::cout << "atan2: " << atan2(getCapturePtr()->getSensorPtr()->getSensorRotation()->transpose()(0,1),getCapturePtr()->getSensorPtr()->getSensorRotation()->transpose()(0,0)) << std::endl;
+
     // sensor transformation
     Eigen::Matrix<T, 2, 1> sensor_position = getCapturePtr()->getSensorPtr()->getPPtr()->getVector().head(2).cast<T>();
-    //                        Eigen::Matrix<T,2,2> inverse_R_sensor = (getCapturePtr()->getSensorPtr()->getRotationMatrix2D().transpose()).cast<T>();
-    Eigen::Rotation2D<Scalar> S_R(getCapturePtr()->getSensorOPtr()->getVector()(0));
-    Eigen::Matrix<T, 2, 2> inverse_R_sensor = (S_R.matrix().transpose()).cast<T>();
-    // robot information
-    Eigen::Matrix<T, 2, 2> inverse_R_robot;
-    inverse_R_robot << cos(*_robotO), sin(*_robotO), -sin(*_robotO), cos(*_robotO);
+    Eigen::Matrix<T,2,2> inverse_R_sensor = Eigen::Rotation2D<T>(T(-getCapturePtr()->getSensorOPtr()->getVector()(0))).matrix();
+    // robot transformation
+    Eigen::Matrix<T,2,2> inverse_R_robot = Eigen::Rotation2D<T>(-_robotO[0]).matrix();
+
     // Expected measurement
-    Eigen::Matrix<T, 2, 1> expected_landmark_relative_position = inverse_R_sensor
-            * (inverse_R_robot * (landmark_position - robot_position) - sensor_position);
-    T expected_landmark_relative_orientation = (*_landmarkO) - (*_robotO)
-            - atan2(inverse_R_sensor(0, 1), inverse_R_sensor(0, 0));
-    while (expected_landmark_relative_orientation > T(M_PI))
-        expected_landmark_relative_orientation = expected_landmark_relative_orientation - T(2 * M_PI);
-    while (expected_landmark_relative_orientation <= T(-M_PI))
-        expected_landmark_relative_orientation = expected_landmark_relative_orientation + T(2 * M_PI);
-    // Residuals
-    _residuals[0] = (expected_landmark_relative_position(0) - T(getMeasurement()(0)))
-            / T(sqrt(getMeasurementCovariance()(0, 0)));
-    _residuals[1] = (expected_landmark_relative_position(1) - T(getMeasurement()(1)))
-            / T(sqrt(getMeasurementCovariance()(1, 1)));
-    _residuals[2] = expected_landmark_relative_orientation - T(getMeasurement()(2));
+    Eigen::Matrix<T, 2, 1> expected_measurement_position = inverse_R_sensor * (inverse_R_robot * (landmark_position_map - robot_position_map) - sensor_position);
+    T expected_measurement_orientation = _landmarkO[0] - _robotO[0] - T(getCapturePtr()->getSensorPtr()->getOPtr()->getVector()(0));
+
+    // Error
+    residuals_map.head(2) = expected_measurement_position - getMeasurement().head<2>().cast<T>();
+    residuals_map(2) = expected_measurement_orientation - T(getMeasurement()(2));
+
+    // pi 2 pi
     while (_residuals[2] > T(M_PI))
-        _residuals[2] = _residuals[2] - T(2 * M_PI);
+        _residuals[2] = _residuals[2] - T(2*M_PI);
     while (_residuals[2] <= T(-M_PI))
-        _residuals[2] = _residuals[2] + T(2 * M_PI);
-    _residuals[2] = _residuals[2] / T(sqrt(getMeasurementCovariance()(2, 2)));
-    //			std::cout << "\nCONSTRAINT: " << nodeId() << std::endl;
-    //			std::cout << "Feature: " << getFeaturePtr()->nodeId() << std::endl;
-    //			std::cout << "Landmark: " << lmk_ptr_->nodeId() << std::endl;
-    //			std::cout << "measurement:\n\t" << getMeasurement().transpose() << std::endl;
+        _residuals[2] = _residuals[2] + T(2*M_PI);
+
+    // Residuals
+    residuals_map = getMeasurementSquareRootInformation().cast<T>() * residuals_map;
+
+    //std::cout << "\nCONSTRAINT: " << nodeId() << std::endl;
+    //std::cout << "Feature: " << getFeaturePtr()->nodeId() << std::endl;
+    //std::cout << "Landmark: " << lmk_ptr_->nodeId() << std::endl;
+    //std::cout << "measurement:\n\t" << getMeasurement().transpose() << std::endl;
     //
-    //			std::cout << "robot pose:";
-    //			for (int i=0; i < 2; i++)
-    //			   std::cout  << "\n\t" << _robotP[i];
-    //			std::cout  << "\n\t" << _robotO[0];
-    //			std::cout << std::endl;
+    //std::cout << "robot pose:";
+    //for (int i=0; i < 2; i++)
+    //   std::cout  << "\n\t" << _robotP[i];
+    //std::cout  << "\n\t" << _robotO[0];
+    //std::cout << std::endl;
     //
-    //			std::cout << "landmark pose:";
-    //			for (int i=0; i < 2; i++)
-    //			   std::cout  << "\n\t" << _landmarkP[i];
-    //			std::cout  << "\n\t" << _landmarkO[0];
-    //			std::cout << std::endl;
+    //std::cout << "landmark pose:";
+    //for (int i=0; i < 2; i++)
+    //   std::cout  << "\n\t" << _landmarkP[i];
+    //std::cout  << "\n\t" << _landmarkO[0];
+    //std::cout << std::endl;
     //
-    //			std::cout << "expected_measurement: ";
-    //			for (int i=0; i < 2; i++)
-    //				std::cout << "\n\t" << expected_landmark_relative_position.data()[i];
-    //			std::cout << std::endl;
+    //std::cout << "expected_measurement: ";
+    //for (int i=0; i < 2; i++)
+    //    std::cout << "\n\t" << expected_measurement_position.data()[i];
+    //std::cout << std::endl;
     //
-    //			std::cout << "_residuals: "<< std::endl;
-    //			for (int i=0; i < 3; i++)
-    //				std::cout  << "\n\t" << _residuals[i] << " ";
-    //			std::cout << std::endl;
+    //std::cout << "_residuals: "<< std::endl;
+    //for (int i=0; i < 3; i++)
+    //    std::cout  << "\n\t" << _residuals[i] << " ";
+    //std::cout << std::endl;
     return true;
 }
 
