bbd754f6541026c0679b828b7498d239eb3c2b29
Joan Solà <jsola@iri.upc.edu>
2016-05-01 22:34:27 +0200

    Some improvements

--- src/examples/test_yaml.cpp
+++ src/examples/test_yaml.cpp
@@ -15,9 +15,11 @@
 int main(){
 
     YAML::Node camera_config = YAML::LoadFile("/Users/jsola/dev/wolf/src/examples/camera.yaml");
-    std::string sensor_type = "NULL";
+
     if (camera_config["sensor type"]) {
-        sensor_type = camera_config["sensor type"].as<std::string>();
+        std::string sensor_type = camera_config["sensor type"].as<std::string>();
+
+        std::string sensor_name = camera_config["sensor name"].as<std::string>();
 
         YAML::Node params = camera_config["parameters"];
 
@@ -25,20 +27,27 @@ int main(){
         std::vector<double> p = params["position"].as<std::vector<double>>();
         std::vector<double> o = params["orientation"].as<std::vector<double>>();
         std::vector<double> k = params["intrinsic"].as<std::vector<double>>();
+        std::vector<double> d = params["distortion"].as<std::vector<double>>();
+        std::vector<double> c = params["correction"].as<std::vector<double>>();
 
         using namespace Eigen;
 
-        Map<Vector2d> size(s.data());
-        Map<Vector3d> pos(p.data());
-        Map<Vector3d> ori(o.data());
-        ori *= 3.141592653/180;
-        Map<Vector4d> intrinsic(k.data());
+        // Using Eigen vector constructors from data pionters. Mind the vector sizes!
+        Vector2d size(s.data());
+        Vector3d pos(p.data());
+        Vector3d ori(o.data());
+        ori *= 3.1415926536/180;
+        Vector4d intrinsic(k.data());
+        Map<VectorXd> distortion(d.data(), d.size());
+        Map<VectorXd> correction(c.data(), c.size());
 
         std::cout << "sensor type: " << sensor_type << std::endl;
-        std::cout << "position   : " << pos.transpose() << std::endl;
-        std::cout << "orientation: " << ori.transpose() << std::endl;
-        std::cout << "intrinsic  : " << intrinsic.transpose() << std::endl;
-        std::cout << "image size : " << size.transpose() << std::endl;
+        std::cout << "sensor name: " << sensor_name << std::endl;
+        std::cout << "sensor parameters: " << std::endl;
+        std::cout << "\tposition    : " << pos.transpose() << std::endl;
+        std::cout << "\torientation : " << ori.transpose() << std::endl;
+        std::cout << "\tintrinsic   : " << intrinsic.transpose() << std::endl;
+        std::cout << "\timage size  : " << size.transpose() << std::endl;
     }
     else
         std::cout << "Bad configuration file. No sensor type found." << std::endl;
