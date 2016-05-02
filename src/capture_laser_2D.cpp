762baa8c8399e78d57aa9a2ddf5248edc4a9ef7b
joanvallve <jvallve@iri.upc.edu>
2016-05-02 15:26:25 +0200

    CaptureLaser2D: initialization of scan_ in constructor

--- src/capture_laser_2D.cpp
+++ src/capture_laser_2D.cpp
@@ -3,7 +3,7 @@
 namespace wolf {
 
 CaptureLaser2D::CaptureLaser2D(const TimeStamp& _ts, SensorBase* _sensor_ptr, const std::vector<float>& _ranges) :
-        CaptureBase(_ts, _sensor_ptr), laser_ptr_((SensorLaser2D*)(sensor_ptr_))
+        CaptureBase(_ts, _sensor_ptr), laser_ptr_((SensorLaser2D*)(sensor_ptr_)), scan_(_ranges)
 {
     //
 }
