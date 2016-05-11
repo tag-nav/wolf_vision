53aa5088ee47709a5b2e19e1625496e4824ba52c
Joan Sola <jsola@iri.upc.edu>
2016-05-11 19:24:17 +0200

    Remove unnecessary wolf:: appearances

--- src/sensor_factory.cpp
+++ src/sensor_factory.cpp
@@ -16,9 +16,9 @@ bool SensorFactory::registerCreator(const std::string& _sensor_type, CreateSenso
 {
     bool reg = callbacks_.insert(CallbackMap::value_type(_sensor_type, createFn)).second;
     if (reg)
-        std::cout << "SeosorFactory     : registered " << _sensor_type << std::endl;
+        std::cout << "SensorFactory     : registered " << _sensor_type << std::endl;
     else
-        std::cout << "SeosorFactory     : sensor " << _sensor_type << " already registered. Skipping. " << std::endl;
+        std::cout << "SensorFactory     : sensor " << _sensor_type << " already registered. Skipping. " << std::endl;
     return reg;
 }
 
@@ -27,7 +27,7 @@ bool SensorFactory::unregisterCreator(const std::string& _sensor_type)
     return callbacks_.erase(_sensor_type) == 1;
 }
 
-wolf::SensorBase* SensorFactory::create(const std::string& _sensor_type, const std::string& _name, const Eigen::VectorXs& _extrinsics, const IntrinsicsBase* _intrinsics)
+SensorBase* SensorFactory::create(const std::string& _sensor_type, const std::string& _name, const Eigen::VectorXs& _extrinsics, const IntrinsicsBase* _intrinsics)
 {
     CallbackMap::const_iterator i = callbacks_.find(_sensor_type);
     if (i == callbacks_.end())
@@ -42,7 +42,7 @@ wolf::SensorBase* SensorFactory::create(const std::string& _sensor_type, const s
 // Singleton ---------------------------------------------------
 // This class is a singleton. The code below guarantees this.
 
-wolf::SensorFactory* SensorFactory::get() // Unique point of access;
+SensorFactory* SensorFactory::get() // Unique point of access;
 {
     static SensorFactory instance_;
     return &instance_;
