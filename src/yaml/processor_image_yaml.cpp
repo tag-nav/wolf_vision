6880fe93f0d70ff43bfa3f93638571228a5f12c0
Joan Solà <jsola@iri.upc.edu>
2016-05-21 16:04:17 +0200

    Start YAML helper for ProcessorImage
    YAML file done
    Registerer and creator not done
    Need to see how to read enums by text!

---
+++ src/yaml/processor_image_yaml.cpp
@@ -0,0 +1,24 @@
+/**
+ * \file processor_image_yaml.cpp
+ *
+ *  Created on: May 21, 2016
+ *      \author: jsola
+ */
+
+// wolf yaml
+#include "yaml_conversion.h"
+
+// wolf
+#include "../processor_image.h"
+#include "../factory.h"
+
+// yaml-cpp library
+#include <yaml-cpp/yaml.h>
+
+namespace wolf
+{
+
+
+
+}
+
