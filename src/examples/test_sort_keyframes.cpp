8b85325425324b19e76464ef5c3bd06d3dbe5262
joanvallve <jvallve@iri.upc.edu>
2016-05-25 10:44:46 +0200

    test_sort_keyframes: OK

--- src/examples/test_sort_keyframes.cpp
+++ src/examples/test_sort_keyframes.cpp
@@ -6,35 +6,74 @@
  */
 
 // Wolf includes
-#include "capture_fix.h"
-#include "state_block.h"
-#include "wolf.h"
-#include "ceres_wrapper/ceres_manager.h"
+#include "../wolf.h"
+#include "../problem.h"
+#include "../frame_base.h"
 
 // STL includes
-#include <map>
 #include <list>
-#include <algorithm>
-#include <iterator>
 
 // General includes
 #include <iostream>
-#include <iomanip>      // std::setprecision
+
+using namespace wolf;
+
+void printFrames(Problem* _problem_ptr)
+{
+    std::cout << "TRAJECTORY:" << std::endl;
+    for (auto frm : *(_problem_ptr->getTrajectoryPtr()->getFrameListPtr()))
+        std::cout << "\t " << (frm->isKey() ? "KEY FRAME: " : "FRAME: ") << frm->id() << " - TS: " << frm->getTimeStamp().getSeconds() << "." << frm->getTimeStamp().getNanoSeconds() << std::endl;
+}
 
 int main()
 {
-    std::cout << std::setprecision(3);
+    Problem problem(FRM_PO_2D);
 
-    using namespace wolf;
+    FrameBase* frm1 = problem.createFrame(NON_KEY_FRAME, Eigen::VectorXs::Zero(3), TimeStamp(0.1));
+    FrameBase* frm2 = problem.createFrame(NON_KEY_FRAME, Eigen::VectorXs::Zero(3), TimeStamp(0.2));
+    FrameBase* frm3 = problem.createFrame(NON_KEY_FRAME, Eigen::VectorXs::Zero(3), TimeStamp(0.3));
+    FrameBase* frm4 = problem.createFrame(NON_KEY_FRAME, Eigen::VectorXs::Zero(3), TimeStamp(0.4));
+    FrameBase* frm5 = problem.createFrame(NON_KEY_FRAME, Eigen::VectorXs::Zero(3), TimeStamp(0.5));
+    FrameBase* frm6 = problem.createFrame(NON_KEY_FRAME, Eigen::VectorXs::Zero(3), TimeStamp(0.6));
 
-    Problem problem = new Problem(FRM_PO_2D);
+    printFrames(&problem);
 
-    problem.createFrame(NON_KEY_FRAME, Eigen::VectorXs::Zero(3), TimeStamp(0.1));
-    problem.createFrame(NON_KEY_FRAME, Eigen::VectorXs::Zero(3), TimeStamp(0.2));
-    problem.createFrame(NON_KEY_FRAME, Eigen::VectorXs::Zero(3), TimeStamp(0.3));
-    problem.createFrame(NON_KEY_FRAME, Eigen::VectorXs::Zero(3), TimeStamp(0.4));
-    problem.createFrame(NON_KEY_FRAME, Eigen::VectorXs::Zero(3), TimeStamp(0.5));
-    problem.createFrame(NON_KEY_FRAME, Eigen::VectorXs::Zero(3), TimeStamp(0.6));
+    std::cout << std::endl << "Frame " << frm5->id() << " set KEY" << std::endl;
+    frm5->setKey();
+
+    printFrames(&problem);
+
+    std::cout << std::endl << "Frame " << frm2->id() << " set KEY" << std::endl;
+    frm2->setKey();
+
+    printFrames(&problem);
+
+    std::cout << std::endl << "Frame " << frm3->id() << " new TimeStamp:" << 0.45 << std::endl;
+    frm3->setTimeStamp(TimeStamp(0.45));
+
+    printFrames(&problem);
+
+    std::cout << std::endl << "Frame " << frm3->id() << " set KEY" << std::endl;
+    frm3->setKey();
+
+    printFrames(&problem);
+
+    std::cout << std::endl << "Frame " << frm6->id() << " set KEY" << std::endl;
+    frm6->setKey();
+
+    printFrames(&problem);
+
+    FrameBase* frm7 = problem.createFrame(KEY_FRAME, Eigen::VectorXs::Zero(3), TimeStamp(0.7));
+    std::cout << std::endl << "created Key Frame " << frm7->id() << " TS: " << 0.7 << std::endl;
+
+    printFrames(&problem);
+
+    FrameBase* frm8 = problem.createFrame(KEY_FRAME, Eigen::VectorXs::Zero(3), TimeStamp(0.35));
+    std::cout << std::endl << "created Key Frame " << frm8->id() << " TS: " << 0.35 << std::endl;
+
+    printFrames(&problem);
+
+
 
     return 0;
 }
