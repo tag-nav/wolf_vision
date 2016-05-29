4c3bab7b205c13ec864741efb6c3050b877ad301
Joan Solà <jsola@iri.upc.edu>
2016-05-29 21:16:12 +0200

    Test also rotations q and R

--- src/examples/test_matrix_prod.cpp
+++ src/examples/test_matrix_prod.cpp
@@ -20,9 +20,9 @@ int main()
 {
     using namespace Eigen;
 
-    int N = 100000;
+    int N = 1000000;
     const int S = 6;
-    Matrix<double, 16, S-3+1> results;
+    Matrix<double, 16, S - 3 + 1> results;
     clock_t t0;
 
     {
@@ -40,237 +40,382 @@ int main()
             std::cout << "Timings for dynamic matrix product. R: row major matrix. C: column major matrix. " << s << "x"
                     << s << " matrices." << std::endl;
 
-
             t0 = clock();
             for (int i = 0; i < N; i++)
+            {
                 Ro = R1 * R2;
-            std::cout << "Time Ro = R * R: " << ((double)clock() - t0) / CLOCKS_PER_SEC / N * 1e6 << "us" << std::endl;
+                R2.setRandom();
+            }
+            std::cout << "TimeRo = R * R: " << ((double)clock() - t0) / CLOCKS_PER_SEC / N * 1e6 << "us" << std::endl;
 
             t0 = clock();
             for (int i = 0; i < N; i++)
+            {
                 Ro = R1 * C2;
-            std::cout << "Time Ro = R * C: " << ((double)clock() - t0) / CLOCKS_PER_SEC / N * 1e6 << "us" << std::endl;
+                C2.setRandom();
+            }
+            std::cout << "TimeRo = R * C: " << ((double)clock() - t0) / CLOCKS_PER_SEC / N * 1e6 << "us" << std::endl;
 
             t0 = clock();
             for (int i = 0; i < N; i++)
+            {
                 Ro = C1 * R2;
-            std::cout << "Time Ro = C * R: " << ((double)clock() - t0) / CLOCKS_PER_SEC / N * 1e6 << "us" << std::endl;
+                R2.setRandom();
+            }
+            std::cout << "TimeRo = C * R: " << ((double)clock() - t0) / CLOCKS_PER_SEC / N * 1e6 << "us" << std::endl;
 
             t0 = clock();
             for (int i = 0; i < N; i++)
+            {
                 Ro = C1 * C2;
-            std::cout << "Time Ro = C * C: " << ((double)clock() - t0) / CLOCKS_PER_SEC / N * 1e6 << "us" << std::endl;
+                C2.setRandom();
+            }
+            std::cout << "TimeRo = C * C: " << ((double)clock() - t0) / CLOCKS_PER_SEC / N * 1e6 << "us" << std::endl;
 
             t0 = clock();
             for (int i = 0; i < N; i++)
+            {
                 Co = R1 * R2;
+                R2.setRandom();
+            }
             std::cout << "Time Co = R * R: " << ((double)clock() - t0) / CLOCKS_PER_SEC / N * 1e6 << "us" << std::endl;
 
             t0 = clock();
             for (int i = 0; i < N; i++)
+            {
                 Co = R1 * C2;
+                C2.setRandom();
+            }
             std::cout << "Time Co = R * C: " << ((double)clock() - t0) / CLOCKS_PER_SEC / N * 1e6 << "us" << std::endl;
 
             t0 = clock();
             for (int i = 0; i < N; i++)
+            {
                 Co = C1 * R2;
+                R2.setRandom();
+            }
             std::cout << "Time Co = C * R: " << ((double)clock() - t0) / CLOCKS_PER_SEC / N * 1e6 << "us" << std::endl;
 
             t0 = clock();
             for (int i = 0; i < N; i++)
+            {
                 Co = C1 * C2;
+                C2.setRandom();
+            }
             std::cout << "Time Co = C * C: " << ((double)clock() - t0) / CLOCKS_PER_SEC / N * 1e6
                     << "us <-- this is the Eigen default!" << std::endl;
         }
     }
+    N *= 10;
     {
         const int s = 3;
-        std::cout << "Timings for static matrix product. R: row major matrix. C: column major matrix. " << s << "x"
-                << s << " matrices." << std::endl;
+        std::cout << "Timings for static matrix product. R: row major matrix. C: column major matrix. " << s << "x" << s
+                << " matrices." << std::endl;
         Matrix<double, s, s, RowMajor> R1, R2, Ro;
         Matrix<double, s, s, ColMajor> C1, C2, Co;
         t0 = clock();
         for (int i = 0; i < N; i++)
+        {
             Ro = R1 * R2;
-        std::cout << "Time Ro = R * R: " << ((double)clock() - t0) / CLOCKS_PER_SEC / N * 1e6 << "us" << std::endl;
+            R2.setRandom();
+        }
+        std::cout << "TimeRo = R * R: " << ((double)clock() - t0) / CLOCKS_PER_SEC / N * 1e6 << "us" << std::endl;
 
         t0 = clock();
         for (int i = 0; i < N; i++)
+        {
             Ro = R1 * C2;
-        std::cout << "Time Ro = R * C: " << ((double)clock() - t0) / CLOCKS_PER_SEC / N * 1e6 << "us" << std::endl;
+            C2.setRandom();
+        }
+        std::cout << "TimeRo = R * C: " << ((double)clock() - t0) / CLOCKS_PER_SEC / N * 1e6 << "us" << std::endl;
 
         t0 = clock();
         for (int i = 0; i < N; i++)
+        {
             Ro = C1 * R2;
-        std::cout << "Time Ro = C * R: " << ((double)clock() - t0) / CLOCKS_PER_SEC / N * 1e6 << "us" << std::endl;
+            R2.setRandom();
+        }
+        std::cout << "TimeRo = C * R: " << ((double)clock() - t0) / CLOCKS_PER_SEC / N * 1e6 << "us" << std::endl;
 
         t0 = clock();
         for (int i = 0; i < N; i++)
+        {
             Ro = C1 * C2;
-        std::cout << "Time Ro = C * C: " << ((double)clock() - t0) / CLOCKS_PER_SEC / N * 1e6 << "us" << std::endl;
+            C2.setRandom();
+        }
+        std::cout << "TimeRo = C * C: " << ((double)clock() - t0) / CLOCKS_PER_SEC / N * 1e6 << "us" << std::endl;
 
         t0 = clock();
         for (int i = 0; i < N; i++)
+        {
             Co = R1 * R2;
+            R2.setRandom();
+        }
         std::cout << "Time Co = R * R: " << ((double)clock() - t0) / CLOCKS_PER_SEC / N * 1e6 << "us" << std::endl;
 
         t0 = clock();
         for (int i = 0; i < N; i++)
+        {
             Co = R1 * C2;
+            C2.setRandom();
+        }
         std::cout << "Time Co = R * C: " << ((double)clock() - t0) / CLOCKS_PER_SEC / N * 1e6 << "us" << std::endl;
 
         t0 = clock();
         for (int i = 0; i < N; i++)
+        {
             Co = C1 * R2;
+            R2.setRandom();
+        }
         std::cout << "Time Co = C * R: " << ((double)clock() - t0) / CLOCKS_PER_SEC / N * 1e6 << "us" << std::endl;
 
         t0 = clock();
         for (int i = 0; i < N; i++)
+        {
             Co = C1 * C2;
+            C2.setRandom();
+        }
         std::cout << "Time Co = C * C: " << ((double)clock() - t0) / CLOCKS_PER_SEC / N * 1e6
                 << "us <-- this is the Eigen default!" << std::endl;
     }
     {
         const int s = 4;
-        std::cout << "Timings for static matrix product. R: row major matrix. C: column major matrix. " << s << "x"
-                << s << " matrices." << std::endl;
+        std::cout << "Timings for static matrix product. R: row major matrix. C: column major matrix. " << s << "x" << s
+                << " matrices." << std::endl;
         Matrix<double, s, s, RowMajor> R1, R2, Ro;
         Matrix<double, s, s, ColMajor> C1, C2, Co;
         t0 = clock();
         for (int i = 0; i < N; i++)
+        {
             Ro = R1 * R2;
-        std::cout << "Time Ro = R * R: " << ((double)clock() - t0) / CLOCKS_PER_SEC / N * 1e6 << "us" << std::endl;
+            R2.setRandom();
+        }
+        std::cout << "TimeRo = R * R: " << ((double)clock() - t0) / CLOCKS_PER_SEC / N * 1e6 << "us" << std::endl;
 
         t0 = clock();
         for (int i = 0; i < N; i++)
+        {
             Ro = R1 * C2;
-        std::cout << "Time Ro = R * C: " << ((double)clock() - t0) / CLOCKS_PER_SEC / N * 1e6 << "us" << std::endl;
+            C2.setRandom();
+        }
+        std::cout << "TimeRo = R * C: " << ((double)clock() - t0) / CLOCKS_PER_SEC / N * 1e6 << "us" << std::endl;
 
         t0 = clock();
         for (int i = 0; i < N; i++)
+        {
             Ro = C1 * R2;
-        std::cout << "Time Ro = C * R: " << ((double)clock() - t0) / CLOCKS_PER_SEC / N * 1e6 << "us" << std::endl;
+            R2.setRandom();
+        }
+        std::cout << "TimeRo = C * R: " << ((double)clock() - t0) / CLOCKS_PER_SEC / N * 1e6 << "us" << std::endl;
 
         t0 = clock();
         for (int i = 0; i < N; i++)
+        {
             Ro = C1 * C2;
-        std::cout << "Time Ro = C * C: " << ((double)clock() - t0) / CLOCKS_PER_SEC / N * 1e6 << "us" << std::endl;
+            C2.setRandom();
+        }
+        std::cout << "TimeRo = C * C: " << ((double)clock() - t0) / CLOCKS_PER_SEC / N * 1e6 << "us" << std::endl;
 
         t0 = clock();
         for (int i = 0; i < N; i++)
+        {
             Co = R1 * R2;
+            R2.setRandom();
+        }
         std::cout << "Time Co = R * R: " << ((double)clock() - t0) / CLOCKS_PER_SEC / N * 1e6 << "us" << std::endl;
 
         t0 = clock();
         for (int i = 0; i < N; i++)
+        {
             Co = R1 * C2;
+            C2.setRandom();
+        }
         std::cout << "Time Co = R * C: " << ((double)clock() - t0) / CLOCKS_PER_SEC / N * 1e6 << "us" << std::endl;
 
         t0 = clock();
         for (int i = 0; i < N; i++)
+        {
             Co = C1 * R2;
+            R2.setRandom();
+        }
         std::cout << "Time Co = C * R: " << ((double)clock() - t0) / CLOCKS_PER_SEC / N * 1e6 << "us" << std::endl;
 
         t0 = clock();
         for (int i = 0; i < N; i++)
+        {
             Co = C1 * C2;
+            C2.setRandom();
+        }
         std::cout << "Time Co = C * C: " << ((double)clock() - t0) / CLOCKS_PER_SEC / N * 1e6
                 << "us <-- this is the Eigen default!" << std::endl;
     }
     {
         const int s = 5;
-        std::cout << "Timings for static matrix product. R: row major matrix. C: column major matrix. " << s << "x"
-                << s << " matrices." << std::endl;
+        std::cout << "Timings for static matrix product. R: row major matrix. C: column major matrix. " << s << "x" << s
+                << " matrices." << std::endl;
         Matrix<double, s, s, RowMajor> R1, R2, Ro;
         Matrix<double, s, s, ColMajor> C1, C2, Co;
         t0 = clock();
         for (int i = 0; i < N; i++)
+        {
             Ro = R1 * R2;
-        std::cout << "Time Ro = R * R: " << ((double)clock() - t0) / CLOCKS_PER_SEC / N * 1e6 << "us" << std::endl;
+            R2.setRandom();
+        }
+        std::cout << "TimeRo = R * R: " << ((double)clock() - t0) / CLOCKS_PER_SEC / N * 1e6 << "us" << std::endl;
 
         t0 = clock();
         for (int i = 0; i < N; i++)
+        {
             Ro = R1 * C2;
-        std::cout << "Time Ro = R * C: " << ((double)clock() - t0) / CLOCKS_PER_SEC / N * 1e6 << "us" << std::endl;
+            C2.setRandom();
+        }
+        std::cout << "TimeRo = R * C: " << ((double)clock() - t0) / CLOCKS_PER_SEC / N * 1e6 << "us" << std::endl;
 
         t0 = clock();
         for (int i = 0; i < N; i++)
+        {
             Ro = C1 * R2;
-        std::cout << "Time Ro = C * R: " << ((double)clock() - t0) / CLOCKS_PER_SEC / N * 1e6 << "us" << std::endl;
+            R2.setRandom();
+        }
+        std::cout << "TimeRo = C * R: " << ((double)clock() - t0) / CLOCKS_PER_SEC / N * 1e6 << "us" << std::endl;
 
         t0 = clock();
         for (int i = 0; i < N; i++)
+        {
             Ro = C1 * C2;
-        std::cout << "Time Ro = C * C: " << ((double)clock() - t0) / CLOCKS_PER_SEC / N * 1e6 << "us" << std::endl;
+            C2.setRandom();
+        }
+        std::cout << "TimeRo = C * C: " << ((double)clock() - t0) / CLOCKS_PER_SEC / N * 1e6 << "us" << std::endl;
 
         t0 = clock();
         for (int i = 0; i < N; i++)
+        {
             Co = R1 * R2;
+            R2.setRandom();
+        }
         std::cout << "Time Co = R * R: " << ((double)clock() - t0) / CLOCKS_PER_SEC / N * 1e6 << "us" << std::endl;
 
         t0 = clock();
         for (int i = 0; i < N; i++)
+        {
             Co = R1 * C2;
+            C2.setRandom();
+        }
         std::cout << "Time Co = R * C: " << ((double)clock() - t0) / CLOCKS_PER_SEC / N * 1e6 << "us" << std::endl;
 
         t0 = clock();
         for (int i = 0; i < N; i++)
+        {
             Co = C1 * R2;
+            R2.setRandom();
+        }
         std::cout << "Time Co = C * R: " << ((double)clock() - t0) / CLOCKS_PER_SEC / N * 1e6 << "us" << std::endl;
 
         t0 = clock();
         for (int i = 0; i < N; i++)
+        {
             Co = C1 * C2;
+            C2.setRandom();
+        }
         std::cout << "Time Co = C * C: " << ((double)clock() - t0) / CLOCKS_PER_SEC / N * 1e6
                 << "us <-- this is the Eigen default!" << std::endl;
     }
     {
         const int s = 6;
-        std::cout << "Timings for static matrix product. R: row major matrix. C: column major matrix. " << s << "x"
-                << s << " matrices." << std::endl;
+        std::cout << "Timings for static matrix product. R: row major matrix. C: column major matrix. " << s << "x" << s
+                << " matrices." << std::endl;
         Matrix<double, s, s, RowMajor> R1, R2, Ro;
         Matrix<double, s, s, ColMajor> C1, C2, Co;
         t0 = clock();
         for (int i = 0; i < N; i++)
+        {
             Ro = R1 * R2;
-        std::cout << "Time Ro = R * R: " << ((double)clock() - t0) / CLOCKS_PER_SEC / N * 1e6 << "us" << std::endl;
+            R2.setRandom();
+        }
+        std::cout << "TimeRo = R * R: " << ((double)clock() - t0) / CLOCKS_PER_SEC / N * 1e6 << "us" << std::endl;
 
         t0 = clock();
         for (int i = 0; i < N; i++)
+        {
             Ro = R1 * C2;
-        std::cout << "Time Ro = R * C: " << ((double)clock() - t0) / CLOCKS_PER_SEC / N * 1e6 << "us" << std::endl;
+            C2.setRandom();
+        }
+        std::cout << "TimeRo = R * C: " << ((double)clock() - t0) / CLOCKS_PER_SEC / N * 1e6 << "us" << std::endl;
 
         t0 = clock();
         for (int i = 0; i < N; i++)
+        {
             Ro = C1 * R2;
-        std::cout << "Time Ro = C * R: " << ((double)clock() - t0) / CLOCKS_PER_SEC / N * 1e6 << "us" << std::endl;
+            R2.setRandom();
+        }
+        std::cout << "TimeRo = C * R: " << ((double)clock() - t0) / CLOCKS_PER_SEC / N * 1e6 << "us" << std::endl;
 
         t0 = clock();
         for (int i = 0; i < N; i++)
+        {
             Ro = C1 * C2;
-        std::cout << "Time Ro = C * C: " << ((double)clock() - t0) / CLOCKS_PER_SEC / N * 1e6 << "us" << std::endl;
+            C2.setRandom();
+        }
+        std::cout << "TimeRo = C * C: " << ((double)clock() - t0) / CLOCKS_PER_SEC / N * 1e6 << "us" << std::endl;
 
         t0 = clock();
         for (int i = 0; i < N; i++)
+        {
             Co = R1 * R2;
+            R2.setRandom();
+        }
         std::cout << "Time Co = R * R: " << ((double)clock() - t0) / CLOCKS_PER_SEC / N * 1e6 << "us" << std::endl;
 
         t0 = clock();
         for (int i = 0; i < N; i++)
+        {
             Co = R1 * C2;
+            C2.setRandom();
+        }
         std::cout << "Time Co = R * C: " << ((double)clock() - t0) / CLOCKS_PER_SEC / N * 1e6 << "us" << std::endl;
 
         t0 = clock();
         for (int i = 0; i < N; i++)
+        {
             Co = C1 * R2;
+            R2.setRandom();
+        }
         std::cout << "Time Co = C * R: " << ((double)clock() - t0) / CLOCKS_PER_SEC / N * 1e6 << "us" << std::endl;
 
         t0 = clock();
         for (int i = 0; i < N; i++)
+        {
             Co = C1 * C2;
+            C2.setRandom();
+        }
         std::cout << "Time Co = C * C: " << ((double)clock() - t0) / CLOCKS_PER_SEC / N * 1e6
                 << "us <-- this is the Eigen default!" << std::endl;
     }
+
+    std::cout << "Test q and R rotations" << std::endl;
+    Eigen::Quaterniond q(Eigen::Vector4d::Random().normalized());
+    Eigen::Matrix3d R = q.matrix();
+    Eigen::Vector3d v((Eigen::Vector3d() << 1, 2, 3).finished());
+    Eigen::Vector3d w;
+
+    N *= 10;
+
+    t0 = clock();
+    for (int i = 0; i < N; i++)
+    {
+        w = R * v;
+        v.setRandom();
+    }
+
+    std::cout << "Time w = R * v: " << ((double)clock() - t0) / CLOCKS_PER_SEC / N * 1e6 << "us" << std::endl;
+
+    t0 = clock();
+    for (int i = 0; i < N; i++)
+    {
+        w = q * v;
+        v.setRandom();
+    }
+    std::cout << "Time w = q * v: " << ((double)clock() - t0) / CLOCKS_PER_SEC / N * 1e6 << "us" << std::endl;
 
     return 0;
 }
