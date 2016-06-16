874d0e79ccc96bd400c5ec8a4152c5b4c76b3d7b
joanvallve <jvallve@iri.upc.edu>
2016-06-16 14:08:42 +0200

    removed cout

--- src/constraint_point_to_line_2D.h
+++ src/constraint_point_to_line_2D.h
@@ -25,7 +25,6 @@ class ConstraintPointToLine2D: public ConstraintSparse<1,2,1,2,2>
 			ConstraintSparse<1,2,1,2,2>(CTR_POINT_TO_LINE_2D, _lmk_ptr, _apply_loss_function, _status, _ftr_ptr->getFramePtr()->getPPtr(), _ftr_ptr->getFramePtr()->getOPtr(), _lmk_ptr->getPointStateBlockPtr(_lmk_point_id), _lmk_ptr->getPointStateBlockPtr(_lmk_point_aux_id)),
 			point_id_(_lmk_point_id), point_aux_id_(_lmk_point_aux_id), point_state_ptr_(_lmk_ptr->getPointStateBlockPtr(_lmk_point_id)), point_aux_state_ptr_(_lmk_ptr->getPointStateBlockPtr(_lmk_point_aux_id)), measurement_(_ftr_ptr->getPoints().col(_ftr_point_id)), measurement_covariance_(_ftr_ptr->getPointsCov().middleCols(_ftr_point_id*2,2))
 		{
-			std::cout << "ConstraintPointToLine2D" << std::endl;
             setType("CORNER 2D");
             Eigen::LLT<Eigen::MatrixXs> lltOfA(measurement_covariance_); // compute the Cholesky decomposition of A
             Eigen::MatrixXs measurement_sqrt_covariance = lltOfA.matrixU();
