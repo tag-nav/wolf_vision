4a47d83cc1d2d20db73884cd2754b9de82488085
Joan Sola <jsola@iri.upc.edu>
2016-07-22 12:36:29 +0200

    test matrix products

--- src/examples/test_matrix_prod.cpp
+++ src/examples/test_matrix_prod.cpp
@@ -12,6 +12,38 @@
 #include <iostream>
 #include <iomanip>
 
+#include <eigen3/Eigen/StdVector>
+using namespace Eigen;
+
+EIGEN_DEFINE_STL_VECTOR_SPECIALIZATION(Eigen::Matrix<double,1,1,RowMajor>)
+EIGEN_DEFINE_STL_VECTOR_SPECIALIZATION(Eigen::Matrix<double,2,2,RowMajor>)
+EIGEN_DEFINE_STL_VECTOR_SPECIALIZATION(Eigen::Matrix<double,3,3,RowMajor>)
+EIGEN_DEFINE_STL_VECTOR_SPECIALIZATION(Eigen::Matrix<double,4,4,RowMajor>)
+EIGEN_DEFINE_STL_VECTOR_SPECIALIZATION(Eigen::Matrix<double,5,5,RowMajor>)
+EIGEN_DEFINE_STL_VECTOR_SPECIALIZATION(Eigen::Matrix<double,6,6,RowMajor>)
+EIGEN_DEFINE_STL_VECTOR_SPECIALIZATION(Eigen::Matrix<double,7,7,RowMajor>)
+EIGEN_DEFINE_STL_VECTOR_SPECIALIZATION(Eigen::Matrix<double,8,8,RowMajor>)
+EIGEN_DEFINE_STL_VECTOR_SPECIALIZATION(Eigen::Matrix<double,9,9,RowMajor>)
+
+EIGEN_DEFINE_STL_VECTOR_SPECIALIZATION(Eigen::Matrix<double,1,1,ColMajor>)
+EIGEN_DEFINE_STL_VECTOR_SPECIALIZATION(Eigen::Matrix<double,2,2,ColMajor>)
+EIGEN_DEFINE_STL_VECTOR_SPECIALIZATION(Eigen::Matrix<double,3,3,ColMajor>)
+EIGEN_DEFINE_STL_VECTOR_SPECIALIZATION(Eigen::Matrix<double,4,4,ColMajor>)
+EIGEN_DEFINE_STL_VECTOR_SPECIALIZATION(Eigen::Matrix<double,5,5,ColMajor>)
+EIGEN_DEFINE_STL_VECTOR_SPECIALIZATION(Eigen::Matrix<double,6,6,ColMajor>)
+EIGEN_DEFINE_STL_VECTOR_SPECIALIZATION(Eigen::Matrix<double,7,7,ColMajor>)
+EIGEN_DEFINE_STL_VECTOR_SPECIALIZATION(Eigen::Matrix<double,8,8,ColMajor>)
+EIGEN_DEFINE_STL_VECTOR_SPECIALIZATION(Eigen::Matrix<double,9,9,ColMajor>)
+
+EIGEN_DEFINE_STL_VECTOR_SPECIALIZATION(Eigen::Matrix<double,2,1>)
+EIGEN_DEFINE_STL_VECTOR_SPECIALIZATION(Eigen::Matrix<double,3,1>)
+EIGEN_DEFINE_STL_VECTOR_SPECIALIZATION(Eigen::Matrix<double,4,1>)
+EIGEN_DEFINE_STL_VECTOR_SPECIALIZATION(Eigen::Matrix<double,5,1>)
+EIGEN_DEFINE_STL_VECTOR_SPECIALIZATION(Eigen::Matrix<double,6,1>)
+EIGEN_DEFINE_STL_VECTOR_SPECIALIZATION(Eigen::Matrix<double,7,1>)
+EIGEN_DEFINE_STL_VECTOR_SPECIALIZATION(Eigen::Matrix<double,8,1>)
+EIGEN_DEFINE_STL_VECTOR_SPECIALIZATION(Eigen::Matrix<double,9,1>)
+
 #define DECLARE_MATRICES(s) \
         Matrix<double, s, s, RowMajor> R1, R2, Ro; \
         Matrix<double, s, s, ColMajor> C1, C2, Co;
@@ -23,7 +55,6 @@
         C2.setRandom(s, s);\
         Ro.setRandom(s, s);\
         Co.setRandom(s, s);
-
 
 #define LOOP_MATRIX(N,Mo,M1,M2) \
         for (int i = 0; i < N; i++) \
@@ -68,7 +99,7 @@ int main()
 {
     using namespace Eigen;
 
-    int N = 100000;
+    int N = 100*1000;
     const int S = 6;
     Matrix<double, 16, S - 3 + 1> results;
     clock_t t0, t1;
@@ -129,10 +160,12 @@ int main()
     std::cout << "Test q and R rotations" << std::endl;
     Eigen::Quaterniond q(Eigen::Vector4d::Random().normalized());
     Eigen::Matrix3d R = q.matrix();
-    Eigen::Vector3d v; v << 1,2,3; double vn = v.norm();
+    Eigen::Vector3d v0; v0.setRandom(); v0.normalize(); double v0n = v0.norm();
+    Eigen::Vector3d v;
 
     N *= 100;
 
+    v = v0;
     t0 = clock();
     for (int i = 0; i < N; i++)
     {
@@ -140,9 +173,9 @@ int main()
     }
     t1 = clock();
     std::cout << "Time w = R * v: " << (double)(t1 - t0) * 1e9 / CLOCKS_PER_SEC / N << "ns" << std::endl;
-    std::cout << "v norm change: " << 10*logl((long double)v.norm()/(long double)vn) << " dB" << std::endl;
+    std::cout << "v norm change: " << 10*logl((long double)v.norm()/(long double)v0n) << " dB" << std::endl;
 
-    v << 1,2,3;
+    v = v0;
     t0 = clock();
     for (int i = 0; i < N; i++)
     {
@@ -150,7 +183,7 @@ int main()
     }
     t1 = clock();
     std::cout << "Time w = q * v: " << (double)(t1 - t0) * 1e9 / CLOCKS_PER_SEC / N << "ns" << std::endl;
-    std::cout << "v norm change: " << 10*logl((long double)v.norm()/(long double)vn) << " dB" << std::endl;
+    std::cout << "v norm change: " << 10*logl((long double)v.norm()/(long double)v0n) << " dB" << std::endl;
     return 0;
 }
 
