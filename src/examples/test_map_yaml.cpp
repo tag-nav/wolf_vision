158157304564ecf8a18f30ed6b55fd2a3055b04c
Joan Sola <jsola@iri.upc.edu>
2016-07-28 16:07:05 +0200

    Improve test with save() map method.
    The test correctly:
      - Loads a YAML file with a hand-written map.
      - Prints it on screen
      - Re-writes it on a new file
      - Re-loads it onto a new map
      - Re-prints the new map --> it is exactly equal to the original, as it must be.

--- src/examples/test_map_yaml.cpp
+++ src/examples/test_map_yaml.cpp
@@ -20,6 +20,8 @@ int main()
 {
     using namespace wolf;
 
+    std::string filename;
+
     char* w = std::getenv("WOLF_ROOT");
     if (w == NULL)
         throw std::runtime_error("Environment variable WOLF_ROOT not found");
@@ -29,8 +31,9 @@ int main()
     std::cout << "\nWolf directory for configuration files: " << WOLF_CONFIG << std::endl;
 
     Problem problem(FRM_PO_2D);
-    std::cout << "Reading map from file: " << WOLF_CONFIG + "/map_polyline_example.yaml" << std::endl;
-    problem.getMapPtr()->load(WOLF_CONFIG + "/map_polyline_example.yaml");
+    filename = WOLF_CONFIG + "/map_polyline_example.yaml";
+    std::cout << "Reading map from file: " << filename << std::endl;
+    problem.getMapPtr()->load(filename);
 
     std::cout << "printing map..." << std::endl;
 
@@ -47,6 +50,30 @@ int main()
         std::cout << std::endl;
     }
 
+    filename = WOLF_CONFIG + "/map_polyline_example_write.yaml";
+    std::cout << "Writing map to file: " << filename << std::endl;
+    problem.getMapPtr()->save(filename);
+
+
+    std::cout << "Clearing map... " << std::endl;
+    problem.getMapPtr()->getLandmarkListPtr()->clear();
+
+    std::cout << "Re-reading map from file: " << filename << std::endl;
+    problem.getMapPtr()->load(filename);
+
+    std::cout << "printing map..." << std::endl;
+    for (auto lmk_ptr : *(problem.getMapPtr()->getLandmarkListPtr()))
+    {
+        std::cout << "Lmk ID:    " << lmk_ptr->id();
+        LandmarkPolyline2D* poly_ptr = (LandmarkPolyline2D*)lmk_ptr;
+        std::cout << "\nn points:  " << poly_ptr->getNPoints();
+        std::cout << "\nFirst idx: " << poly_ptr->getFirstId();
+        std::cout << "\nFirst def: " << poly_ptr->isFirstDefined();
+        std::cout << "\nLast  def: " << poly_ptr->isLastDefined();
+        for (int idx = poly_ptr->getFirstId(); idx <= poly_ptr->getLastId(); idx++)
+            std::cout << "\n  point: " << idx << ": " << poly_ptr->getPointStateBlockPtr(idx)->getVector().transpose();
+        std::cout << std::endl;
+    }
 
     return 0;
 }
