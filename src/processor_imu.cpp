5873dec36e6828369319e2d607ff73d1567a3cfe
Dylan Bourgeois <dylan.bourgeois@epfl.ch>
2016-07-28 17:50:45 +0200

    init map

--- src/processor_imu.cpp
+++ src/processor_imu.cpp
@@ -3,7 +3,23 @@
 namespace wolf {
 
 ProcessorIMU::ProcessorIMU() :
-        ProcessorMotion(PRC_IMU, 16, 10, 6)
+        ProcessorMotion(PRC_IMU, 16, 16, 6),
+        q1_(nullptr),
+        q2_(nullptr),
+        q_out_(nullptr),
+        v1_(nullptr),
+        v2_(nullptr),
+        v_out_(nullptr),
+        p1_(nullptr),
+        p2_(nullptr),
+        p_out_(nullptr),
+        bias_acc1_(nullptr),
+        bias_acc2_(nullptr),
+        bias_acc_out_(nullptr),
+        bias_gyro1_(nullptr),
+        bias_gyro2_(nullptr),
+        bias_gyro_out_(nullptr)
+
 //        sensor_imu_ptr_(nullptr),
 //        capture_imu_ptr_(nullptr)
 {
