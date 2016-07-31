46b03668e46ace2541b01c18c0dec7bc3ee0efbc
Joan Solà <jsola@iri.upc.edu>
2016-07-31 04:48:09 +0200

    Add YAML creator

--- src/landmark_AHP.cpp
+++ src/landmark_AHP.cpp
@@ -2,13 +2,19 @@
 
 #include "state_homogeneous_3D.h"
 #include "factory.h"
+#include "yaml/yaml_conversion.h"
 
 namespace wolf {
 
 /* Landmark - Anchored Homogeneous Point*/
-LandmarkAHP::LandmarkAHP(Eigen::Vector4s _position, FrameBase* _anchor_frame, SensorBase* _anchor_sensor, cv::Mat _2D_descriptor) :
-    LandmarkBase(LANDMARK_AHP, new StateHomogeneous3D(_position)),
-    descriptor_(_2D_descriptor), anchor_frame_(_anchor_frame), anchor_sensor_(_anchor_sensor)
+LandmarkAHP::LandmarkAHP(Eigen::Vector4s _position_homogeneous,
+                         FrameBase* _anchor_frame,
+                         SensorBase* _anchor_sensor,
+                         cv::Mat _2D_descriptor) :
+    LandmarkBase(LANDMARK_AHP, new StateHomogeneous3D(_position_homogeneous)),
+    descriptor_(_2D_descriptor),
+    anchor_frame_(_anchor_frame),
+    anchor_sensor_(_anchor_sensor)
 {
 //    std::cout << "LandmarkAHP p_ptr" << p_ptr_->getVector().transpose() << std::endl;
     setType("AHP");
@@ -19,11 +25,18 @@ LandmarkAHP::~LandmarkAHP()
     //
 }
 
+
+wolf::LandmarkBase* LandmarkAHP::create(const YAML::Node& _node)
+{
+    Eigen::VectorXs pos_homog = _node["position"].as<Eigen::VectorXs>();
+    cv::Mat desc(_node["descriptor"].as<std::vector<unsigned int> >());
+    return new LandmarkAHP(pos_homog, nullptr, nullptr, desc);
+}
+
 // Register landmark creator
 namespace
 {
 const bool registered_lmk_ahp = LandmarkFactory::get().registerCreator("AHP", LandmarkAHP::create);
 }
-
 
 } // namespace wolf
--- src/landmark_AHP.h
+++ src/landmark_AHP.h
@@ -3,7 +3,9 @@
 
 //Wolf includes
 #include "landmark_base.h"
-#include "yaml/yaml_conversion.h"
+
+// yaml
+#include <yaml-cpp/yaml.h>
 
 //OpenCV includes
 #include "opencv2/features2d/features2d.hpp"
@@ -19,7 +21,7 @@ class LandmarkAHP : public LandmarkBase
         SensorBase* anchor_sensor_;
 
     public:
-        LandmarkAHP(Eigen::Vector4s _position, FrameBase* _frame, SensorBase* _anchor_sensor, cv::Mat _2D_descriptor);
+        LandmarkAHP(Eigen::Vector4s _position_homogeneous, FrameBase* _anchor_frame, SensorBase* _anchor_sensor, cv::Mat _2D_descriptor);
 
         virtual ~LandmarkAHP();
 
@@ -74,13 +76,6 @@ inline void LandmarkAHP::setAnchor(FrameBase* _anchor_frame, SensorBase* _anchor
 {
     anchor_frame_  = _anchor_frame;
     anchor_sensor_ = _anchor_sensor;
-}
-
-inline wolf::LandmarkBase* LandmarkAHP::create(const YAML::Node& _node)
-{
-    Eigen::VectorXs pos = _node["position"].as<Eigen::VectorXs>();
-    cv::Mat desc(_node["descriptor"].as<std::vector<unsigned int> >());
-    return new LandmarkAHP(pos, nullptr, nullptr, desc);
 }
 
 } // namespace wolf
