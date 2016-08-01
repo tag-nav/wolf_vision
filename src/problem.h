5a88d6a02d748485ea06ebe7051f4c41744735a7
Joan Sola <jsola@iri.upc.edu>
2016-08-01 10:51:19 +0200

    Revert "change some string pass-by-refs to pass-by-copy"
    This reverts commit 5ccb11d77ca7f091f2588b4a20625a7e48347c23.

--- src/problem.h
+++ src/problem.h
@@ -108,8 +108,8 @@ class Problem : public NodeBase
          * \param _extrinsics a vector of extrinsic parameters: size 2 for 2D position, 3 for 2D pose, 3 for 3D position, 7 for 3D pose.
          * \param _intrinsics a base-pointer to a derived struct defining the intrinsic parameters.
          */
-        SensorBase* installSensor(const std::string _sen_type, //
-                                  const std::string _unique_sensor_name, //
+        SensorBase* installSensor(const std::string& _sen_type, //
+                                  const std::string& _unique_sensor_name, //
                                   const Eigen::VectorXs& _extrinsics, //
                                   IntrinsicsBase* _intrinsics = nullptr);
 
@@ -119,10 +119,10 @@ class Problem : public NodeBase
          * \param _extrinsics a vector of extrinsic parameters: size 2 for 2D position, 3 for 2D pose, 3 for 3D position, 7 for 3D pose.
          * \param _intrinsics_filename the name of a file containing the intrinsic parameters in a format compatible with the intrinsics creator registered in IntrinsicsFactory under the key _sen_type.
          */
-        SensorBase* installSensor(const std::string _sen_type, //
-                                  const std::string _unique_sensor_name, //
+        SensorBase* installSensor(const std::string& _sen_type, //
+                                  const std::string& _unique_sensor_name, //
                                   const Eigen::VectorXs& _extrinsics, //
-                                  const std::string _intrinsics_filename);
+                                  const std::string& _intrinsics_filename);
 
         /** \brief Factory method to install (create, and add to sensor) processors only from its properties
          *
@@ -132,8 +132,8 @@ class Problem : public NodeBase
          * \param _corresponding_sensor_ptr pointer to the sensor where the processor will be installed.
          * \param _prc_params a base-pointer to a derived struct defining the processor parameters.
          */
-        ProcessorBase* installProcessor(const std::string _prc_type, //
-                                        const std::string _unique_processor_name, //
+        ProcessorBase* installProcessor(const std::string& _prc_type, //
+                                        const std::string& _unique_processor_name, //
                                         SensorBase* _corresponding_sensor_ptr, //
                                         ProcessorParamsBase* _prc_params = nullptr);
 
@@ -147,10 +147,10 @@ class Problem : public NodeBase
          * \param _corresponding_sensor_name corresponding sensor name, used to bind the processor to the particular instance of the sensor
          * \param _params_filename name of formatted file (xml, yaml, etc) defining the processor parameters.
          */
-        void installProcessor(const std::string _prc_type, //
-                              const std::string _unique_processor_name, //
-                              const std::string _corresponding_sensor_name, //
-                              const std::string _params_filename = "");
+        void installProcessor(const std::string& _prc_type, //
+                              const std::string& _unique_processor_name, //
+                              const std::string& _corresponding_sensor_name, //
+                              const std::string& _params_filename = "");
 
         /** \brief Set the processor motion
          *
