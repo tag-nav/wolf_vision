cef5233b6b62c2b3c06c3bbda39102239d19af19
Dinesh Atchuthan <datchuth@laas.fr>
2016-08-04 12:52:56 +0200

    input simulated data in test_processor_imu.cpp

--- src/examples/test_processor_imu.cpp
+++ src/examples/test_processor_imu.cpp
@@ -7,20 +7,47 @@
 
 //std
 #include <iostream>
+#include <fstream>
 
 //Wolf
 #include "wolf.h"
 #include "problem.h"
 #include "sensor_imu.h"
+#include "capture_imu.h"
 #include "state_block.h"
 #include "state_quaternion.h"
 #include "processor_imu.h"
 
-int main()
+int main(int argc, char** argv)
 {
     using namespace wolf;
 
     std::cout << std::endl << "==================== processor IMU test ======================" << std::endl;
+
+    std::ifstream data_file_acc;
+    std::ifstream data_file_gyro;
+    //load files containing accelerometer and gyroscope data
+    const char * filename_acc;
+    const char * filename_gyro;
+    if (argc < 3)
+    {
+        std::cout << "missing input argument : needs 2 arguments (path to accelerometer file and path to gyroscope data)." << std::endl;
+    }
+    else
+    {
+        filename_acc = argv[1];
+        filename_gyro = argv[2];
+        data_file_acc.open(filename_acc);
+        data_file_gyro.open(filename_gyro);
+        if(!data_file_acc.is_open() || !data_file_gyro.is_open()){
+            std::cerr << "Failed to open data files... Exiting" << std::endl;
+            return 1;
+        }
+    }
+
+     TimeStamp t;
+     float mti_clock, tmp;
+     Eigen::Vector6s data_;
 
     // Wolf problem
     Problem* wolf_problem_ptr_ = new Problem(FRM_PQVBB_3D);
@@ -32,6 +59,18 @@ int main()
 
     wolf_problem_ptr_->addSensor(sensor_ptr_);
     sensor_ptr_->addProcessor(processor_ptr_);
+
+    CaptureIMU* imu_ptr;
+
+    while(!data_file_acc.eof()){
+        data_file_acc >> mti_clock >> data_[0] >> data_[1] >> data_[2];
+        data_file_gyro >> tmp >> data_[3] >> data_[4] >> data_[5];
+        t.set(mti_clock);
+
+        imu_ptr = new CaptureIMU(t, sensor_ptr_, data_);
+
+        imu_ptr ->process();
+    }
 
     std::cout << "sensor & processor created and added to wolf problem" << std::endl;
 
