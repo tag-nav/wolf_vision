f1a936f18809e723cbe5afe9e1167a754da24954
Joan Solà <jsola@iri.upc.edu>
2016-08-24 00:12:25 +0200

    typo in comment

--- src/processor_tracker.h
+++ src/processor_tracker.h
@@ -71,7 +71,7 @@ class ProcessorTracker : public ProcessorBase
         CaptureBase* incoming_ptr_;  ///< Pointer to the incoming capture being processed.
         FeatureBaseList new_features_last_; ///< List of new features in \b last for landmark initialization and new key-frame creation.
         FeatureBaseList new_features_incoming_; ///< list of the new features of \b last successfully tracked in \b incoming
-        unsigned int max_new_features_; ///< max features alowed to detect in one iteration. 0 = no limit
+        unsigned int max_new_features_; ///< max features allowed to detect in one iteration. 0 = no limit
 
     public:
         ProcessorTracker(ProcessorType _tp, const std::string& _type, const unsigned int _max_new_features = 0, const Scalar& _time_tolerance = 0.1);
