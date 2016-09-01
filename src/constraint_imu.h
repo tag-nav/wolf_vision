a733d0704d328af6716c54cbca64bb7014f9462a
Joan Sola <jsola@iri.upc.edu>
2016-09-01 14:51:22 +0200

    Move definitions after the class declaration.

--- src/constraint_imu.h
+++ src/constraint_imu.h
@@ -13,23 +13,15 @@ class ConstraintIMU : public ConstraintSparse<9, 3, 4, 3, 3, 3, 3, 4, 3>
     public:
         static const unsigned int N_BLOCKS = 8;
 
-        ConstraintIMU(FeatureIMU* _ftr_ptr, FrameIMU* _frame_ptr, bool _apply_loss_function = false, ConstraintStatus _status = CTR_ACTIVE) :
-                ConstraintSparse<9, 3, 4, 3, 3, 3, 3, 4, 3>(CTR_IMU, _frame_ptr, _apply_loss_function, _status,
-                     _frame_ptr->getPPtr(), _frame_ptr->getOPtr(), _frame_ptr->getVPtr(), _frame_ptr->getBAPtr(), _frame_ptr->getBGPtr(),
-                     _ftr_ptr->getFramePtr()->getPPtr(), _ftr_ptr->getFramePtr()->getOPtr(), _ftr_ptr->getFramePtr()->getVPtr())
-        {
-            setType("IMU");
-        }
+        ConstraintIMU(FeatureIMU* _ftr_ptr, FrameIMU* _frame_ptr, bool _apply_loss_function = false,
+                      ConstraintStatus _status = CTR_ACTIVE);
 
         /** \brief Default destructor (not recommended)
          *
          * Default destructor (please use destruct() instead of delete for guaranteeing the wolf tree integrity)
          *
          **/
-        virtual ~ConstraintIMU()
-        {
-            //
-        }
+        virtual ~ConstraintIMU();
 
         template<typename T>
         bool operator ()(const T* const _p1, const T* const _o1, const T* const _v1, const T* const _b_a1, const T* _b_g1,
@@ -38,19 +30,29 @@ class ConstraintIMU : public ConstraintSparse<9, 3, 4, 3, 3, 3, 3, 4, 3>
 
         /** \brief Returns the jacobians computation method
          **/
-        virtual JacobianMethod getJacobianMethod() const
-        {
-            return JAC_AUTO;
-        }
+        virtual JacobianMethod getJacobianMethod() const;
 
     public:
-        static wolf::ConstraintBase* create(FeatureIMU* _feature_ptr, //
-                                            NodeBase* _correspondant_ptr)
-        {
-            return new ConstraintIMU(_feature_ptr, (FrameIMU*)_correspondant_ptr);
-        }
+        static wolf::ConstraintBase* create(FeatureIMU* _feature_ptr, NodeBase* _correspondant_ptr);
 
 };
+
+inline ConstraintIMU::ConstraintIMU(FeatureIMU* _ftr_ptr, FrameIMU* _frame_ptr, bool _apply_loss_function,
+                                    ConstraintStatus _status) :
+        ConstraintSparse<9, 3, 4, 3, 3, 3, 3, 4, 3>(CTR_IMU, _frame_ptr, _apply_loss_function, _status,
+                                                    _frame_ptr->getPPtr(), _frame_ptr->getOPtr(), _frame_ptr->getVPtr(),
+                                                    _frame_ptr->getBAPtr(), _frame_ptr->getBGPtr(),
+                                                    _ftr_ptr->getFramePtr()->getPPtr(),
+                                                    _ftr_ptr->getFramePtr()->getOPtr(),
+                                                    _ftr_ptr->getFramePtr()->getVPtr())
+{
+    setType("IMU");
+}
+
+inline ConstraintIMU::~ConstraintIMU()
+{
+    //
+}
 
 template<typename T>
 inline bool ConstraintIMU::operator ()(const T* const _p1, const T* const _o1, const T* const _v1, const T* const _ba, const T* _bg,
@@ -75,10 +77,20 @@ inline bool ConstraintIMU::operator ()(const T* const _p1, const T* const _o1, c
     // Expected measurement
     
 
-    // Error
+    // Residual
     residuals_map = expected_measurement - getMeasurement().cast<T>();
 
     return true;
+}
+
+inline JacobianMethod ConstraintIMU::getJacobianMethod() const
+{
+    return JAC_AUTO;
+}
+
+inline wolf::ConstraintBase* ConstraintIMU::create(FeatureIMU* _feature_ptr, NodeBase* _correspondant_ptr)
+{
+    return new ConstraintIMU(_feature_ptr, (FrameIMU*)(_correspondant_ptr));
 }
 
 } // namespace wolf
