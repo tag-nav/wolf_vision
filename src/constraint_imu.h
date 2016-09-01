c79236918b7245e3b6af5095aac4fbe0290f9e85
Dylan Bourgeois <dylan.bourgeois@epfl.ch>
2016-09-01 10:57:12 +0200

    get frame pointers

--- src/constraint_imu.h
+++ src/constraint_imu.h
@@ -3,6 +3,8 @@
 
 //Wolf includes
 #include "constraint_sparse.h"
+#include "feature_imu.h"
+#include "frame_imu.h"
 
 namespace wolf {
 
@@ -11,8 +13,10 @@ class ConstraintIMU : public ConstraintSparse<9, 3, 4, 3, 3, 3, 3, 4, 3>
     public:
         static const unsigned int N_BLOCKS = 8;
 
-        ConstraintIMU(FeatureBase* _ftr_ptr, FrameBase* _frame_ptr, bool _apply_loss_function = false, ConstraintStatus _status = CTR_ACTIVE) :
-                ConstraintSparse<9, 3, 4, 3, 3, 3, 3, 4, 3>(CTR_IMU, _frame_ptr, _apply_loss_function, _status, _frame_ptr->getPPtr(), _frame_ptr->getOPtr(), _ftr_ptr->getFramePtr()->getPPtr(), _ftr_ptr->getFramePtr()->getOPtr())
+        ConstraintIMU(FeatureIMU* _ftr_ptr, FrameIMU* _frame_ptr, bool _apply_loss_function = false, ConstraintStatus _status = CTR_ACTIVE) :
+                ConstraintSparse<9, 3, 4, 3, 3, 3, 3, 4, 3>(CTR_IMU, _frame_ptr, _apply_loss_function, _status,
+                     _frame_ptr->getPPtr(), _frame_ptr->getOPtr(), _frame_ptr->getVPtr(), _frame_ptr->getBAPtr(), _frame_ptr->getBGPtr(),
+                     _ftr_ptr->getFramePtr()->getPPtr(), _ftr_ptr->getFramePtr()->getOPtr(), _ftr_ptr->getFramePtr()->getVPtr())
         {
             setType("IMU");
         }
@@ -40,10 +44,10 @@ class ConstraintIMU : public ConstraintSparse<9, 3, 4, 3, 3, 3, 3, 4, 3>
         }
 
     public:
-        static wolf::ConstraintBase* create(FeatureBase* _feature_ptr, //
+        static wolf::ConstraintBase* create(FeatureIMU* _feature_ptr, //
                                             NodeBase* _correspondant_ptr)
         {
-            return new ConstraintIMU(_feature_ptr, (FrameBase*)_correspondant_ptr);
+            return new ConstraintIMU(_feature_ptr, (FrameIMU*)_correspondant_ptr);
         }
 
 };
