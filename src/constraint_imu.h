6f5e8e102feefba4ddf200044dbea158a5c23dd6
Dylan Bourgeois <dylan.bourgeois@epfl.ch>
2016-09-01 12:57:20 +0200

    constraint operator maps

--- src/constraint_imu.h
+++ src/constraint_imu.h
@@ -53,10 +53,31 @@ class ConstraintIMU : public ConstraintSparse<9, 3, 4, 3, 3, 3, 3, 4, 3>
 };
 
 template<typename T>
-inline bool ConstraintIMU::operator ()(const T* const _p1, const T* const _o1, const T* const _v1, const T* const _b_a1, const T* _b_g1,
+inline bool ConstraintIMU::operator ()(const T* const _p1, const T* const _o1, const T* const _v1, const T* const _ba, const T* _bg,
                                        const T* const _p2, const T* const _o2, const T* const _v2,
                                        T* _residuals) const
 {
+    // MAPS
+    Eigen::Map<Eigen::Matrix<T,9,1> > residuals_map(_residuals);
+
+    Eigen::Map<const Eigen::Matrix<T,3,1> > p1_map(_p1);
+    Eigen::Map<const Eigen::Quaternions > q1_map(_o1);
+    Eigen::Map<const Eigen::Matrix<T,3,1> > v1_map(_v1);
+    Eigen::Map<const Eigen::Matrix<T,3,1> > ba_map(_ba);
+    Eigen::Map<const Eigen::Matrix<T,3,1> > bg_map(_bg);
+
+    Eigen::Map<const Eigen::Matrix<T,3,1> > p2_map(_p2);
+    Eigen::Map<const Eigen::Quaternions > q2_map(_o2);
+    Eigen::Map<const Eigen::Matrix<T,3,1> > v2_map(_v2);
+
+    Eigen::Matrix<T, 9, 1> expected_measurement;
+
+    // Expected measurement
+    
+
+    // Error
+    residuals_map = expected_measurement - getMeasurement().cast<T>();
+
     return true;
 }
 
