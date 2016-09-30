fdecc9d21f511c0af4e35474cf670df07a7d7cb3
Joan Solà <jsola@iri.upc.edu>
2016-09-30 02:13:45 +0200

    New test

--- src/examples/test_clean.cpp
+++ src/examples/test_clean.cpp
@@ -13,6 +13,8 @@
 int main()
 {
     using namespace wolf;
+    using std::cout;
+    using std::endl;
 
 
     Problem problem(FRM_PVQBB_3D);
@@ -20,7 +22,8 @@ int main()
     Eigen::VectorXs extrinsics(7);
     extrinsics << 0,0,0, 0,0,0,1; // IMU pose in the robot
     SensorBasePtr sensor_ptr = problem.installSensor("IMU", "Main IMU", extrinsics, nullptr);
-    problem.installProcessor("IMU", "IMU pre-integrator", "Main IMU", "");
+    ProcessorBasePtr processor_ptr = problem.installProcessor("IMU", "IMU pre-integrator", "Main IMU", "");
+    ProcessorIMU* imu_ptr = (ProcessorIMU*)processor_ptr;
 
     // Time and data variables
     TimeStamp t;
@@ -34,6 +37,8 @@ int main()
     Eigen::VectorXs x0(16);
     x0 << 0,1,0,  1,0,0,  0,0,0,1,  0,0,.001,  0,0,.002; // Try some non-zero biases
     problem.getProcessorMotionPtr()->setOrigin(x0, t);
+
+    cout << imu_ptr->getCurrentState().transpose() << endl;
 
 
 
