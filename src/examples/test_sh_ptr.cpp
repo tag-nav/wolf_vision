c5e05a8a23a6b9101f23f7a8d6abbb11cd0a640e
Joan Solà <jsola@iri.upc.edu>
2016-10-05 00:30:28 +0200

    removeL() and remove_c_by()
    remove() from a list of weak_ptr is difficult. I used remove_if(), and
    made a lambda function.

--- src/examples/test_sh_ptr.cpp
+++ src/examples/test_sh_ptr.cpp
@@ -43,6 +43,7 @@ class L; // landmark
 
 class P : public enable_shared_from_this<P>
 {
+    private:
         shared_ptr<H> H_ptr_;
         shared_ptr<T> T_ptr_;
         shared_ptr<M> M_ptr_;
@@ -58,28 +59,31 @@ class P : public enable_shared_from_this<P>
 
 class H : public enable_shared_from_this<H>
 {
+    private:
         weak_ptr<P> P_ptr_;
         list<shared_ptr<S>> S_list_;
+
     public:
         H(){cout << "construct H" << endl;}
         ~H(){cout << "destruct H" << endl;}
         shared_ptr<P> getP(){
             shared_ptr<P> P_sh = P_ptr_.lock();
-            // if (P_sh) // check will be made by caller after all
             return P_sh;
         }
         void setP(const shared_ptr<P> _P){P_ptr_ = _P;}
         list<shared_ptr<S>>& getSlist() {return S_list_;}
-        shared_ptr<S> addS(shared_ptr<S> _S);
+        shared_ptr<S> add_S(shared_ptr<S> _S);
 };
 
 class S : public enable_shared_from_this<S>
 {
+    private:
         weak_ptr<P> P_ptr_;
         weak_ptr<H> H_ptr_;
         list<shared_ptr<p>> p_list_;
 
         list<weak_ptr<C>> C_list_; // List of all captures
+
     public:
         S(){cout << "construct S" << endl;}
         ~S(){cout << "destruct S" << endl;}
@@ -92,18 +96,19 @@ class S : public enable_shared_from_this<S>
         void setP(const shared_ptr<P>& _P){P_ptr_ = _P;}
         shared_ptr<H> getH(){
             shared_ptr<H> H_sh = H_ptr_.lock();
-            // if (H_sh) // check will be made by caller after all
             return H_sh;
         }
         void setH(const shared_ptr<H> _H){H_ptr_ = _H;}
         list<shared_ptr<p>>& getplist() {return p_list_;}
-        shared_ptr<p> addp(shared_ptr<p> _p);
+        shared_ptr<p> add_p(shared_ptr<p> _p);
 };
 
 class p : public enable_shared_from_this<p>
 {
+    private:
         weak_ptr<P> P_ptr_;
         weak_ptr<S> S_ptr_;
+
     public:
         p(){cout << "construct p" << endl;}
         ~p(){cout << "destruct p" << endl;}
@@ -116,7 +121,6 @@ class p : public enable_shared_from_this<p>
         void setP(const shared_ptr<P>& _P){P_ptr_ = _P;}
         shared_ptr<S> getS(){
             shared_ptr<S> S_sh = S_ptr_.lock();
-            // if (S_sh) // check will be made by caller after all
             return S_sh;
         }
         void setS(const shared_ptr<S> _S){S_ptr_ = _S;}
@@ -124,28 +128,31 @@ class p : public enable_shared_from_this<p>
 
 class T : public enable_shared_from_this<T>
 {
+    private:
         weak_ptr<P> P_ptr_;
         list<shared_ptr<F>> F_list_;
+
     public:
         T(){cout << "construct T" << endl;}
         ~T(){cout << "destruct T" << endl;}
         shared_ptr<P> getP(){
             shared_ptr<P> P_sh = P_ptr_.lock();
-            // if (P_sh) // check will be made by caller after all
             return P_sh;
         }
         void setP(const shared_ptr<P> _P){P_ptr_ = _P;}
         list<shared_ptr<F>>& getFlist() {return F_list_;}
-        shared_ptr<F> addF(shared_ptr<F> _F);
+        shared_ptr<F> add_F(shared_ptr<F> _F);
 };
 
 class F : public enable_shared_from_this<F>
 {
+    private:
         weak_ptr<P> P_ptr_;
         weak_ptr<T> T_ptr_;
         list<shared_ptr<C>> C_list_;
 
         list<weak_ptr<c>> c_by_list; // list of constraints to this frame
+
     public:
         F(){cout << "construct F" << endl;}
         ~F(){cout << "destruct F" << endl;}
@@ -158,13 +165,12 @@ class F : public enable_shared_from_this<F>
         void setP(const shared_ptr<P>& _P){P_ptr_ = _P;}
         shared_ptr<T> getT(){
             shared_ptr<T> T_sh = T_ptr_.lock();
-            // if (T_sh) // check will be made by caller after all
             return T_sh;
         }
         void setT(const shared_ptr<T> _T){T_ptr_ = _T;}
         list<shared_ptr<C>>& getClist() {return C_list_;}
-        shared_ptr<C> addC(shared_ptr<C> _C);
-        void addcby(shared_ptr<c> _cby)
+        shared_ptr<C> add_C(shared_ptr<C> _C);
+        void add_c_by(shared_ptr<c> _cby)
         {
             c_by_list.push_back(_cby);
         }
@@ -172,11 +178,13 @@ class F : public enable_shared_from_this<F>
 
 class C : public enable_shared_from_this<C>
 {
+    private:
         weak_ptr<P> P_ptr_;
         weak_ptr<F> F_ptr_;
         list<shared_ptr<f>> f_list_;
 
         weak_ptr<S> S_ptr_; // sensor
+
     public:
         C(){cout << "construct C" << endl;}
         ~C(){cout << "destruct C" << endl;}
@@ -189,16 +197,16 @@ class C : public enable_shared_from_this<C>
         void setP(const shared_ptr<P>& _P){P_ptr_ = _P;}
         shared_ptr<F> getF(){
             shared_ptr<F> F_sh = F_ptr_.lock();
-            // if (F_sh) // check will be made by caller after all
             return F_sh;
         }
         void setF(const shared_ptr<F> _F){F_ptr_ = _F;}
         list<shared_ptr<f>>& getflist() {return f_list_;}
-        shared_ptr<f> addf(shared_ptr<f> _f);
+        shared_ptr<f> add_f(shared_ptr<f> _f);
 };
 
 class f : public enable_shared_from_this<f>
 {
+    private:
         weak_ptr<P> P_ptr_;
         weak_ptr<C> C_ptr_;
         list<shared_ptr<c>> c_list_; // change to list<weak_ptr<c>> ???
@@ -217,13 +225,12 @@ class f : public enable_shared_from_this<f>
         void setP(const shared_ptr<P>& _P){P_ptr_ = _P;}
         shared_ptr<C> getC(){
             shared_ptr<C> C_sh = C_ptr_.lock();
-            // if (C_sh) // check will be made by caller after all
             return C_sh;
         }
         void setC(const shared_ptr<C> _C){C_ptr_ = _C;}
         list<shared_ptr<c>>& getclist() {return c_list_;} // Make a list of shareds from weaks? NO! then what?
-        shared_ptr<c> addc(shared_ptr<c> _c); // use the same 'add' API, or transfer this to be mastered by the 'c' class?
-        void addcby(shared_ptr<c> _cby)
+        shared_ptr<c> add_c(shared_ptr<c> _c); // use the same 'add' API, or transfer this to be mastered by the 'c' class?
+        void add_c_by(shared_ptr<c> _cby)
         {
             c_by_list.push_back(_cby);
         }
@@ -231,6 +238,7 @@ class f : public enable_shared_from_this<f>
 
 class c : public enable_shared_from_this<c>
 {
+    private:
         weak_ptr<P> P_ptr_;
         weak_ptr<f> f_ptr_; // change this to shared??
 
@@ -238,6 +246,7 @@ class c : public enable_shared_from_this<c>
         weak_ptr<F> F_other_ptr_; // change this to shared?
         weak_ptr<f> f_other_ptr_; // change this to shared?
         weak_ptr<L> L_other_ptr_; // change this to shared?
+
     public:
         c(){cout << "construct c" << endl;}
         c(shared_ptr<F> _F_other);
@@ -253,7 +262,6 @@ class c : public enable_shared_from_this<c>
         void setP(const shared_ptr<P>& _P){P_ptr_ = _P;}
         shared_ptr<f> getf(){
             shared_ptr<f> f_sh = f_ptr_.lock();
-            // if (f_sh) // check will be made by caller after all
             return f_sh;
         }
         void setf(const shared_ptr<f> _f){f_ptr_ = _f;}
@@ -261,25 +269,31 @@ class c : public enable_shared_from_this<c>
 
 class M : public enable_shared_from_this<M>
 {
+    private:
         weak_ptr<P> P_ptr_;
         list<shared_ptr<L>> L_list_;
+
     public:
         M(){cout << "construct M" << endl;}
         ~M(){cout << "destruct M" << endl;}
         shared_ptr<P> getP(){
             shared_ptr<P> P_sh = P_ptr_.lock();
-            // if (P_sh) // check will be made by caller after all
             return P_sh;
         }
         void setP(const shared_ptr<P> _P){P_ptr_ = _P;}
         list<shared_ptr<L>>& getLlist() {return L_list_;}
-        shared_ptr<L> addL(shared_ptr<L> _L);
+        shared_ptr<L> add_L(shared_ptr<L> _L);
+        void removeL(shared_ptr<L> _L)
+        {
+            L_list_.remove(_L);
+        }
 };
 
 
 
 class L : public enable_shared_from_this<L>
 {
+    private:
         weak_ptr<P> P_ptr_;
         weak_ptr<M> M_ptr_;
 
@@ -297,13 +311,23 @@ class L : public enable_shared_from_this<L>
         void setP(const shared_ptr<P>& _P){P_ptr_ = _P;}
         shared_ptr<M> getM(){
             shared_ptr<M> M_sh = M_ptr_.lock();
-            // if (M_sh) // check will be made by caller after all
             return M_sh;
         }
         void setM(const shared_ptr<M> _M){M_ptr_ = _M;}
-        void addcby(shared_ptr<c> _cby)
+        void add_c_by(shared_ptr<c> _cby)
         {
             c_by_list.push_back(_cby);
+        }
+        void remove_c_by(shared_ptr<c> _cby)
+        {
+            c_by_list.remove_if( // remove _cby
+                    [_cby](std::weak_ptr<c> p){
+                        std::shared_ptr<c> sp = p.lock();
+                        if(_cby && sp)
+                            return _cby == sp;
+                        return false;});
+            if (c_by_list.empty())
+                getM()->removeL(shared_from_this());
         }
 };
 
@@ -327,7 +351,7 @@ void P::setup()
     cout << "P is set up" << endl;
 }
 
-shared_ptr<S> H::addS(shared_ptr<S> _S)
+shared_ptr<S> H::add_S(shared_ptr<S> _S)
 {
     S_list_.push_back(_S);
     _S->setH(shared_from_this());
@@ -335,7 +359,7 @@ shared_ptr<S> H::addS(shared_ptr<S> _S)
     return _S;
 }
 
-shared_ptr<p> S::addp(shared_ptr<p> _p)
+shared_ptr<p> S::add_p(shared_ptr<p> _p)
 {
     p_list_.push_back(_p);
     _p->setS(shared_from_this());
@@ -343,7 +367,7 @@ shared_ptr<p> S::addp(shared_ptr<p> _p)
     return _p;
 }
 
-shared_ptr<F> T::addF(shared_ptr<F> _F)
+shared_ptr<F> T::add_F(shared_ptr<F> _F)
 {
     F_list_.push_back(_F);
     _F->setT(shared_from_this());
@@ -351,7 +375,7 @@ shared_ptr<F> T::addF(shared_ptr<F> _F)
     return _F;
 }
 
-shared_ptr<C> F::addC(shared_ptr<C> _C)
+shared_ptr<C> F::add_C(shared_ptr<C> _C)
 {
     C_list_.push_back(_C);
     _C->setF(shared_from_this());
@@ -359,7 +383,7 @@ shared_ptr<C> F::addC(shared_ptr<C> _C)
     return _C;
 }
 
-shared_ptr<f> C::addf(shared_ptr<f> _f)
+shared_ptr<f> C::add_f(shared_ptr<f> _f)
 {
     f_list_.push_back(_f);
     _f->setC(shared_from_this());
@@ -367,7 +391,7 @@ shared_ptr<f> C::addf(shared_ptr<f> _f)
     return _f;
 }
 
-shared_ptr<c> f::addc(shared_ptr<c> _c)
+shared_ptr<c> f::add_c(shared_ptr<c> _c)
 {
     c_list_.push_back(_c);
     _c->setf(shared_from_this());
@@ -390,7 +414,7 @@ c::c(shared_ptr<L> _L_other)
     L_other_ptr_ = _L_other;
 }
 
-shared_ptr<L> M::addL(shared_ptr<L> _L)
+shared_ptr<L> M::add_L(shared_ptr<L> _L)
 {
     L_list_.push_back(_L);
     _L->setM(shared_from_this());
@@ -413,35 +437,37 @@ int main()
     // H
     for (int i = 0; i < 2; i++)
     {
-        shared_ptr<S> Sp = Pp->getH()->addS(make_shared<S>());
+        shared_ptr<S> Sp = Pp->getH()->add_S(make_shared<S>());
         for (int i = 0; i < 2; i++)
         {
-            shared_ptr<p> pp = Sp->addp(make_shared<p>());
+            shared_ptr<p> pp = Sp->add_p(make_shared<p>());
         }
     }
 
     // M
     for (int i = 0; i < 2; i++)
     {
-        shared_ptr<L> Lp = Pp->getM()->addL(make_shared<L>());
+        shared_ptr<L> Lp = Pp->getM()->add_L(make_shared<L>());
     }
 
     // T
     for (int i = 0; i < 2; i++)
     {
-        shared_ptr<F> Fp = Pp->getT()->addF(make_shared<F>());
+        shared_ptr<F> Fp = Pp->getT()->add_F(make_shared<F>());
         for (int i = 0; i < 2; i++)
         {
-            shared_ptr<C> Cp = Fp->addC(make_shared<C>());
+            shared_ptr<C> Cp = Fp->add_C(make_shared<C>());
             for (int i = 0; i < 2; i++)
             {
-                shared_ptr<f> fp = Cp->addf(make_shared<f>());
+                shared_ptr<f> fp = Cp->add_f(make_shared<f>());
                 list<shared_ptr<L>>::iterator Lit = Pp->getM()->getLlist().begin();
                 for (int i = 0; i < 2; i++)
                 {
-                    shared_ptr<c> cp = fp->addc(make_shared<c>(*Lit));
                     if(*Lit)
-                        (*Lit)->addcby(cp);
+                    {
+                        shared_ptr<c> cp = fp->add_c(make_shared<c>(*Lit));
+                        (*Lit)->add_c_by(cp);
+                    }
                     else
                         cout << "Could not constrain lmk" << endl;
 
