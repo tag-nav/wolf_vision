7987adb52385b486645f81be2f1df1c2f7bde3e5
Joan Sola <jsola@iri.upc.edu>
2016-10-07 18:07:17 +0200

    [WIP] Improve test. It compiles and executes.

--- src/examples/test_processor_odom_3D.cpp
+++ src/examples/test_processor_odom_3D.cpp
@@ -9,11 +9,13 @@
 #include "problem.h"
 #include "sensor_odom_2D.h"
 #include "processor_odom_3D.h"
+#include "capture_imu.h"
 
 using namespace wolf;
 using std::cout;
 using std::endl;
 using Eigen::Vector3s;
+using Eigen::Vector6s;
 using Eigen::Vector7s;
 using Eigen::Quaternions;
 using Eigen::VectorXs;
@@ -23,17 +25,26 @@ int main ()
 
     Problem* problem = new Problem(FRM_PO_3D);
 
-    SensorBase* sen = problem->installSensor("ODOM 2D", "odom", (Vector7s()<<0,0,0,0,0,0,1).finished(),"");
-    ProcessorBase* prc = problem->installProcessor("ODOM 3D", "odometry integrator", "odom", "");
+    SensorBase* sen = problem->installSensor("ODOM 3D", "odom", (Vector7s()<<0,0,0,0,0,0,1).finished(),"");
+    problem->installProcessor("ODOM 3D", "odometry integrator", "odom", "");
+    problem->getProcessorMotionPtr()->setOrigin((Vector7s()<<0,0,0,0,0,0,1).finished(), TimeStamp(0));
 
-    Vector3s d_pos   ((Vector3s() << 0,0,0).finished());
-    Vector3s d_theta ((Vector3s() << 0,0,0).finished());
+    Vector3s d_pos   ((Vector3s() << 0.1, 0.2, 0.3).finished());
+    Vector3s d_theta ((Vector3s() << 0.01,0.02,0.03).finished());
+
+    Vector6s data((Vector6s() << d_pos , d_theta).finished());
 
     Scalar dt = 0.001;
 
     for (TimeStamp t = 0; t < 5; t += dt)
     {
-        cout << t.get() << endl;
+        cout << "t: " << t.get();
+
+        CaptureIMU* cap_odo = new CaptureIMU(t, sen, data);
+
+        cap_odo->process();
+
+        cout << "   x: " << problem->getCurrentState().transpose() << endl;
 
     }
 
