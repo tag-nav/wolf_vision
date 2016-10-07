a0cd2d9381c1335f8fac7898f9d11982a6c5d90c
Joan Sola <jsola@iri.upc.edu>
2016-10-07 18:06:55 +0200

    Add sensor_odom_3d

---
+++ src/sensor_odom_3D.cpp
@@ -0,0 +1,67 @@
+/**
+ * \file sensor_odom_3D.cpp
+ *
+ *  Created on: Oct 7, 2016
+ *      \author: jsola
+ */
+
+#include "sensor_odom_3D.h"
+
+#include "state_block.h"
+#include "state_quaternion.h"
+
+namespace wolf {
+
+SensorOdom3D::SensorOdom3D(StateBlock* _p_ptr, StateQuaternion* _o_ptr, const Scalar& _k_disp_to_disp, const Scalar& _k_disp_to_rot, const Scalar&  _k_rot_to_rot) :
+        SensorBase(SEN_ODOM_3D, "ODOM 3D", _p_ptr, _o_ptr, nullptr, 6), k_disp_to_disp_(_k_disp_to_disp), k_disp_to_rot_(_k_disp_to_rot), k_rot_to_rot_(_k_rot_to_rot)
+{
+    //
+}
+
+SensorOdom3D::~SensorOdom3D()
+{
+    //
+}
+
+Scalar SensorOdom3D::getDispVarToDispNoiseFactor() const
+{
+    return k_disp_to_disp_;
+}
+
+Scalar SensorOdom3D::getDispVarToRotNoiseFactor() const
+{
+    return k_disp_to_rot_;
+}
+
+Scalar SensorOdom3D::getRotVarToRotNoiseFactor() const
+{
+    return k_rot_to_rot_;
+}
+
+// Define the factory method
+SensorBasePtr SensorOdom3D::create(const std::string& _unique_name, const Eigen::VectorXs& _extrinsics_po,
+                                 const IntrinsicsBasePtr _intrinsics)
+{
+    // decode extrinsics vector
+    assert(_extrinsics_po.size() == 7 && "Bad extrinsics vector length. Should be 7 for 3D.");
+    StateBlock* pos_ptr = new StateBlock(_extrinsics_po.head(3), true);
+    StateQuaternion* ori_ptr = new StateQuaternion(_extrinsics_po.tail(4), true);
+    // cast intrinsics into derived type
+//    IntrinsicsOdom3D* params = (IntrinsicsOdom3D*)(_intrinsics);
+//    params->k_disp_to_disp = 1.0;
+//    params->k_disp_to_rot = 0.0;
+//    params->k_rot_to_rot = 1.0;
+//    SensorBasePtr odo = new SensorOdom3D(pos_ptr, ori_ptr, params->k_disp_to_disp, params->k_disp_to_rot, params->k_rot_to_rot);
+    SensorBasePtr odo = new SensorOdom3D(pos_ptr, ori_ptr, 1.0, 0.0, 1.0);
+    odo->setName(_unique_name);
+    return odo;
+}
+
+} // namespace wolf
+
+
+// Register in the SensorFactory
+#include "sensor_factory.h"
+namespace wolf {
+WOLF_REGISTER_SENSOR("ODOM 3D", SensorOdom3D)
+}
---
+++ src/sensor_odom_3D.h
@@ -0,0 +1,80 @@
+/**
+ * \file sensor_odom_3D.h
+ *
+ *  Created on: Oct 7, 2016
+ *      \author: jsola
+ */
+
+#ifndef SRC_SENSOR_ODOM_3D_H_
+#define SRC_SENSOR_ODOM_3D_H_
+
+//wolf includes
+#include "sensor_base.h"
+
+namespace wolf {
+
+struct IntrinsicsOdom3D : public IntrinsicsBase
+{
+        Scalar k_disp_to_disp; ///< ratio of displacement variance to displacement, for odometry noise calculation
+        Scalar k_disp_to_rot; ///< ratio of displacement variance to rotation, for odometry noise calculation
+        Scalar k_rot_to_rot; ///< ratio of rotation variance to rotation, for odometry noise calculation
+};
+
+class SensorOdom3D : public SensorBase
+{
+
+    protected:
+        Scalar k_disp_to_disp_; ///< ratio of displacement variance to displacement, for odometry noise calculation
+        Scalar k_disp_to_rot_; ///< ratio of displacement variance to rotation, for odometry noise calculation
+        Scalar k_rot_to_rot_; ///< ratio of rotation variance to rotation, for odometry noise calculation
+
+    public:
+        /** \brief Constructor with arguments
+         *
+         * Constructor with arguments
+         * \param _p_ptr StateBlock pointer to the sensor position wrt vehicle base
+         * \param _o_ptr StateBlock pointer to the sensor orientation wrt vehicle base
+         * \param _disp_noise_factor displacement noise factor
+         * \param _rot_noise_factor rotation noise factor
+         *
+         **/
+        SensorOdom3D(StateBlock* _p_ptr, StateQuaternion* _q_ptr, const Scalar& _k_disp_to_disp, const Scalar& _k_disp_to_rot, const Scalar&  _k_rot_to_rot);
+
+        /** \brief Default destructor (not recommended)
+         *
+         * Default destructor (please use destruct() instead of delete for guaranteeing the wolf tree integrity)
+         *
+         **/
+        virtual ~SensorOdom3D();
+
+        /** \brief Returns displacement noise factor
+         *
+         * Returns displacement noise factor
+         *
+         **/
+        Scalar getDispVarToDispNoiseFactor() const;
+
+        /** \brief Returns displacement noise factor
+         *
+         * Returns displacement noise factor
+         *
+         **/
+        Scalar getDispVarToRotNoiseFactor() const;
+
+        /** \brief Returns rotation noise factor
+         *
+         * Returns rotation noise factor
+         *
+         **/
+        Scalar getRotVarToRotNoiseFactor() const;
+
+
+    public:
+        static SensorBasePtr create(const std::string& _unique_name, const Eigen::VectorXs& _extrinsics_pq, const IntrinsicsBasePtr _intrinsics);
+
+
+};
+
+} /* namespace wolf */
+
+#endif /* SRC_SENSOR_ODOM_3D_H_ */
