cabc5c4f3bcc894fed12de883a2953322185d5af
Dinesh Atchuthan <datchuth@laas.fr>
2016-10-13 10:41:58 +0200

    adding unitary test file for rotation.h

---
+++ src/examples/test_rotation.cpp
@@ -0,0 +1,22 @@
+/**
+ * \file test_rotation.cpp
+ *
+ *  Created on: Oct 13, 2016
+ *      \author: AtDinesh
+ */
+
+//std
+#include <iostream>
+
+//Eigen
+#include <eigen3/Eigen/Geometry>
+
+ //Wolf
+#include "wolf.h"
+#include "rotation.h"
+
+int main()
+{
+    using namespace wolf;
+                        // THESE ARE UNITARY TESTS FOR METHODS IN ROTATION.H
+}
