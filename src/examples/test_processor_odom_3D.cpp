e189c6201265c50b4652e690e2c0934113a04950
Joan Sola <jsola@iri.upc.edu>
2016-10-14 14:29:49 +0200

    remove delete problem

--- src/examples/test_processor_odom_3D.cpp
+++ src/examples/test_processor_odom_3D.cpp
@@ -130,7 +130,7 @@ int main (int argc, char** argv)
 
     }
 
-        delete problem; // XXX Why is this throwing segfault?
+        //delete problem; // XXX Why is this throwing segfault?
 
     return 0;
 }
