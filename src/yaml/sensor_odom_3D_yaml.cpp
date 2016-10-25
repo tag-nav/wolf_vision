1af29d3a87fd6af35a177587015749b62c547a91
Joan Sola <jsola@iri.upc.edu>
2016-10-25 12:44:18 +0200

    Add yaml file and parser for sensor odom 3D parameters

---
+++ src/yaml/sensor_odom_3D_yaml.cpp
@@ -0,0 +1,67 @@
+/**
+ * \file sensor_odom_3D_yaml.cpp
+ *
+ *  Created on: Oct 25, 2016
+ *      \author: jsola
+ */
+
+// wolf yaml
+#include "yaml_conversion.h"
+
+// wolf
+#include "../sensor_odom_3D.h"
+#include "../factory.h"
+
+// yaml-cpp library
+#include <yaml-cpp/yaml.h>
+
+namespace wolf
+{
+
+namespace
+{
+static IntrinsicsBasePtr createIntrinsicsOdom3D(const std::string & _filename_dot_yaml)
+{
+    YAML::Node config = YAML::LoadFile(_filename_dot_yaml);
+
+    if (config["sensor type"].as<std::string>() == "ODOM 3D")
+    {
+
+        // YAML:: to Eigen::
+        using namespace Eigen;
+        std::string sensor_type = config["sensor type"]     .as<std::string>();
+        std::string sensor_name = config["sensor name"]     .as<std::string>();
+
+        YAML::Node variances = config["motion variances"];
+
+        IntrinsicsOdom3D::Ptr params = std::make_shared<IntrinsicsOdom3D>();
+
+        params->k_disp_to_disp   = variances["disp_to_disp"] .as<Scalar>();
+        params->k_disp_to_rot    = variances["disp_to_rot"]  .as<Scalar>();
+        params->k_rot_to_rot     = variances["rot_to_rot"]   .as<Scalar>();
+        params->min_disp_var     = variances["min_disp_var"] .as<Scalar>();
+        params->min_rot_var      = variances["min_rot_var"]  .as<Scalar>();
+
+        //=========================================
+        // ===== this part for debugging only =====
+        //=========================================
+        std::cout << "\n--- Parameters Parsed from YAML file ---" << std::endl;
+        std::cout << "sensor type: " << sensor_type << std::endl;
+        std::cout << "sensor name: " << sensor_name << std::endl;
+
+        //=========================================
+        //=========================================
+        return params;
+    }
+
+    std::cout << "Bad configuration file. No sensor type found." << std::endl;
+    return nullptr;
+}
+
+// Register in the SensorFactory
+const bool registered_odom_3D_intr = IntrinsicsFactory::get().registerCreator("ODOM 3D", createIntrinsicsOdom3D);
+
+} // namespace [unnamed]
+
+} // namespace wolf
+
