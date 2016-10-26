c4bd2e5ef1cac23d34834579a10ab2c64cb24366
Jaime Tarrasó Martinez <jtarraso@iri.upc.edu>
2016-10-26 18:28:22 +0200

    Modify the test to do projection and backprojection, while also calling the expectation function of the constraintAHP

--- src/examples/test_constraint_AHP.cpp
+++ src/examples/test_constraint_AHP.cpp
@@ -4,6 +4,7 @@
 #include "state_quaternion.h"
 #include "sensor_camera.h"
 #include "capture_image.h"
+#include "pinholeTools.h"
 
 int main()
 {
@@ -26,13 +27,15 @@ int main()
 
     /* Do this while there aren't extrinsic parameters on the yaml */
     Eigen::Vector7s extrinsic_cam;
-    extrinsic_cam[0] = 0; //px
-    extrinsic_cam[1] = 3; //py
-    extrinsic_cam[2] = 2; //pz
-    extrinsic_cam[3] = 0; //qx
-    extrinsic_cam[4] = 0; //qy
-    extrinsic_cam[5] = 0; //qz
-    extrinsic_cam[6] = 1; //qw
+    extrinsic_cam.setRandom();
+//    extrinsic_cam[0] = 0; //px
+//    extrinsic_cam[1] = 0; //py
+//    extrinsic_cam[2] = 0; //pz
+//    extrinsic_cam[3] = 0; //qx
+//    extrinsic_cam[4] = 0; //qy
+//    extrinsic_cam[5] = 0; //qz
+//    extrinsic_cam[6] = 1; //qw
+    extrinsic_cam.tail<4>().normalize();
     std::cout << "========extrinsic_cam: " << extrinsic_cam.transpose() << std::endl;
     const Eigen::VectorXs extr = extrinsic_cam;
     /* Do this while there aren't extrinsic parameters on the yaml */
@@ -47,13 +50,15 @@ int main()
 
     // create the current frame
     Eigen::Vector7s frame_pos_ori;
-    frame_pos_ori[0] = 0; //px
-    frame_pos_ori[1] = 4; //py
-    frame_pos_ori[2] = 2; //pz
-    frame_pos_ori[3] = 0.2; //qx
-    frame_pos_ori[4] = 0.5; //qy
-    frame_pos_ori[5] = 0.1; //qz
-    frame_pos_ori[6] = 0.83666; //qw
+    frame_pos_ori.setRandom();
+//    frame_pos_ori[0] = 0; //px
+//    frame_pos_ori[1] = 0; //py
+//    frame_pos_ori[2] = 0; //pz
+//    frame_pos_ori[3] = 0; //qx
+//    frame_pos_ori[4] = 0; //qy
+//    frame_pos_ori[5] = 0; //qz
+//    frame_pos_ori[6] = 1; //qw
+    frame_pos_ori.tail<4>().normalize();
     const Eigen::VectorXs frame_val = frame_pos_ori;
 
     FrameBasePtr last_frame = std::make_shared<FrameBase>(t,std::make_shared<StateBlock>(frame_val.head(3)), std::make_shared<StateQuaternion>(frame_val.tail(4)));
@@ -70,7 +75,7 @@ int main()
 
 
     // create the feature
-    cv::KeyPoint kp; kp.pt = {40,40};
+    cv::KeyPoint kp; kp.pt = {10.2,20.4};
     cv::Mat desc;
 
     std::shared_ptr<FeaturePointImage> feat_point_image_ptr = std::make_shared<FeaturePointImage>(kp, desc, Eigen::Matrix2s::Identity());
@@ -92,6 +97,7 @@ int main()
 
     Eigen::VectorXs correction_vec = (std::static_pointer_cast<SensorCamera>(image_ptr->getSensorPtr()))->getCorrectionVector();
     std::cout << "correction vector: " << correction_vec << std::endl;
+    std::cout << "distortion vector: " << (std::static_pointer_cast<SensorCamera>(image_ptr->getSensorPtr()))->getDistortionVector() << std::endl;
     point2D = pinhole::depixellizePoint(image_ptr->getSensorPtr()->getIntrinsicPtr()->getVector(),point2D);
     std::cout << "point2D depixellized: " << point2D.transpose() << std::endl;
     point2D = pinhole::undistortPoint((std::static_pointer_cast<SensorCamera>(image_ptr->getSensorPtr()))->getCorrectionVector(),point2D);
@@ -119,23 +125,32 @@ int main()
     feat_point_image_ptr->addConstraint(constraint_ptr);
     std::cout << "Constraint AHP created" << std::endl;
 
+    Eigen::Vector2s point2D_proj = point3D.head<2>()/point3D(2);
+    std::cout << "point2D projected: " << point2D_proj.transpose() << std::endl;
 
+    Eigen::Vector2s point2D_dist;
+    point2D_dist =  pinhole::distortPoint((std::static_pointer_cast<SensorCamera>(image_ptr->getSensorPtr()))->getDistortionVector(),point2D_proj);
+    std::cout << "point2D distorted: " << point2D_dist.transpose() << std::endl;
 
-    Eigen::Vector2s residuals;
+    Eigen::Vector2s point2D_pix;
+    point2D_pix = pinhole::pixellizePoint(image_ptr->getSensorPtr()->getIntrinsicPtr()->getVector(),point2D_dist);
+    std::cout << "point2D pixellized: " << point2D_pix.transpose() << std::endl;
+
+    Eigen::Vector2s expectation;
     Eigen::Vector3s current_frame_p = last_frame->getPPtr()->getVector();
     Eigen::Vector4s current_frame_o = last_frame->getOPtr()->getVector();
     Eigen::Vector3s anchor_frame_p = landmark->getAnchorFrame()->getPPtr()->getVector();
     Eigen::Vector4s anchor_frame_o = landmark->getAnchorFrame()->getOPtr()->getVector();
     Eigen::Vector4s landmark_ = landmark->getPPtr()->getVector();
 
-    ( * constraint_ptr ) (current_frame_p.data(), current_frame_o.data(),
+    constraint_ptr ->expectation(current_frame_p.data(), current_frame_o.data(),
             anchor_frame_p.data(), anchor_frame_o.data(),
-            landmark_.data(), residuals.data());
+            landmark_.data(), expectation.data());
 //    current_frame p; current_frame o; anchor_frame p; anchor_frame o; homogeneous_vector landmark, residual
 
 
-    std::cout << "Residual computed" << std::endl;
-    std::cout << "Residual = " << residuals[0] << "   " << residuals[1] << std::endl;
+    std::cout << "expectation computed" << std::endl;
+    std::cout << "expectation = " << expectation[0] << "   " << expectation[1] << std::endl;
 
     return 0;
 
