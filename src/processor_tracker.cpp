d508b70a0dd37025326dda72496d56c2f45c693f
Joan Solà <jsola@iri.upc.edu>
2016-10-27 22:16:36 +0200

    [WIP] debugging processor tracker

--- src/processor_tracker.cpp
+++ src/processor_tracker.cpp
@@ -120,20 +120,15 @@ void ProcessorTracker::process(CaptureBasePtr const _incoming_ptr)
 
         // 2. Then we see if we want and we are allowed to create a KeyFrame
         FrameBasePtr last_key_frm = last_ptr_->getFramePtr();
-        if (!last_key_frm || !last_key_frm->isKey())
-        {
-            std::cout << __FILE__ << ":" << __FUNCTION__ << "():" << __LINE__ << std::endl;
+        if (!last_key_frm || !last_key_frm->isKey()) // Last frame is missing, or is not key
             last_key_frm = getProblem()->getTrajectoryPtr()->closestKeyFrameToTimeStamp(last_ptr_->getTimeStamp());
-        }
+
         if (last_key_frm && abs(last_key_frm->getTimeStamp() - last_ptr_->getTimeStamp()) > time_tolerance_)
-        {
-            std::cout << __FILE__ << ":" << __FUNCTION__ << "():" << __LINE__ << std::endl;
             last_key_frm = nullptr;
-        }
-        std::cout << __FILE__ << ":" << __FUNCTION__ << "():" << __LINE__ << ": Last key frame " << last_key_frm << std::endl;
+
+
         if (voteForKeyFrame() && permittedKeyFrame() )//&& last_key_frm == nullptr ) // FIXME: This seems not to work!
         {
-            std::cout << __FILE__ << ":" << __FUNCTION__ << "():" << __LINE__ << std::endl;
             // 2.a. We create a keyframe
 
             // Detect new Features, initialize Landmarks, create Constraints, ...
@@ -143,12 +138,10 @@ void ProcessorTracker::process(CaptureBasePtr const _incoming_ptr)
             FrameBasePtr closest_key_frm = getProblem()->getTrajectoryPtr()->closestKeyFrameToTimeStamp(incoming_ptr_->getTimeStamp());
             if (closest_key_frm)
             {
-                std::cout << __FILE__ << ":" << __FUNCTION__ << "():" << __LINE__ << std::endl;
                 closest_key_frm->addCapture(incoming_ptr_);
             }
             else
             {
-                std::cout << __FILE__ << ":" << __FUNCTION__ << "():" << __LINE__ << std::endl;
                 makeFrame(incoming_ptr_);
             }
 
@@ -168,24 +161,21 @@ void ProcessorTracker::process(CaptureBasePtr const _incoming_ptr)
         }
         else
         {
-            std::cout << __FILE__ << ":" << __FUNCTION__ << "():" << __LINE__ << std::endl;
             // 2.b. We did not create a KeyFrame:
 
             // advance the derived tracker
             advance();
 
             // Advance this
-            last_ptr_->getFramePtr()->addCapture(incoming_ptr_); // Add incoming Capture to the tracker's Frame
+            last_ptr_       ->getFramePtr()->addCapture(incoming_ptr_); // Add incoming Capture to the tracker's Frame
 
-            std::cout << __FILE__ << ":" << __FUNCTION__ << "():" << __LINE__ << std::endl;
-            last_ptr_->remove();
+            last_ptr_       ->remove();
 
-            incoming_ptr_->getFramePtr()->setTimeStamp(incoming_ptr_->getTimeStamp());
+            incoming_ptr_   ->getFramePtr()->setTimeStamp(incoming_ptr_->getTimeStamp());
 
-            std::cout << __FILE__ << ":" << __FUNCTION__ << "():" << __LINE__ << std::endl;
-            last_ptr_ = incoming_ptr_; // Incoming Capture takes the place of last Capture
+            last_ptr_       = incoming_ptr_; // Incoming Capture takes the place of last Capture
 
-            incoming_ptr_ = nullptr; // This line is not really needed, but it makes things clearer.
+            incoming_ptr_   = nullptr; // This line is not really needed, but it makes things clearer.
         }
 
 
