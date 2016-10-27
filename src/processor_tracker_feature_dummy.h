b38e1bdbb790538bf644c44978fa9d8994d0bbb8
Joan Solà <jsola@iri.upc.edu>
2016-10-27 23:52:20 +0200

    Add some options in constructor

--- src/processor_tracker_feature_dummy.h
+++ src/processor_tracker_feature_dummy.h
@@ -19,12 +19,12 @@ class ProcessorTrackerFeatureDummy : public ProcessorTrackerFeature
 {
 
     public:
-        ProcessorTrackerFeatureDummy();
+        ProcessorTrackerFeatureDummy(const unsigned int _max_new_features, const unsigned int _min_feat_for_keyframe);
         virtual ~ProcessorTrackerFeatureDummy();
 
     protected:
 
-        unsigned int n_feature_;
+        unsigned int n_feature_, min_feat_for_keyframe_;
 
 //        virtual void preProcess() { }
 //        virtual void postProcess() { }
@@ -79,9 +79,9 @@ class ProcessorTrackerFeatureDummy : public ProcessorTrackerFeature
 
 };
 
-inline ProcessorTrackerFeatureDummy::ProcessorTrackerFeatureDummy() :
-        ProcessorTrackerFeature(PRC_TRACKER_DUMMY, "TRACKER FEATURE DUMMY"),
-        n_feature_(0)
+inline ProcessorTrackerFeatureDummy::ProcessorTrackerFeatureDummy(const unsigned int _max_new_features, const unsigned int _min_feat_for_keyframe) :
+        ProcessorTrackerFeature(PRC_TRACKER_DUMMY, "TRACKER FEATURE DUMMY", _max_new_features),
+        n_feature_(0), min_feat_for_keyframe_(_min_feat_for_keyframe)
 {
     //
 }
