589d2a757e9424b0d0f19819a812a1e8f9f755dd
Jaime Tarrasó Martinez <jtarraso@iri.upc.edu>
2016-11-02 18:55:36 +0100

    Create new test simple_AHP to test the solver

---
+++ src/examples/test_simple_AHP.cpp
@@ -0,0 +1,28 @@
+/**
+ * \file test_simple_AHP.cpp
+ *
+ *  Created on: 2 nov. 2016
+ *      \author: jtarraso
+ */
+
+
+
+
+int main()
+{
+    /* 1. crear 2 kf, fixed
+     * 2. crear 1 sensor
+     * 3. crear 1 lmk1
+     * 4. projectar lmk sobre sensor a fk1
+     * 5. projectar lmk sobre sensor a kf2
+     * 6. // esborrar lmk lmk_ptr->remove() no cal
+     * 7. crear nous kf
+     * 8. crear captures
+     * 9. crear features amb les mesures de 4 i 5
+     * 10. crear lmk2 des de kf1
+     * 11. crear constraint des del kf2 a lmk2, amb ancora al kf1
+     * 12. solve
+     * 13. lmk1 == lmk2 ?
+     */
+
+}
