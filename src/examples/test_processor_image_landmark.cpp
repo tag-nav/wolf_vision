1c95b612ea1ff190d24472ce93df6ba9f258f0a5
Joan Sola <jsola@iri.upc.edu>
2016-11-03 12:47:04 +0100

    Fix initial time stamp

--- src/examples/test_processor_image_landmark.cpp
+++ src/examples/test_processor_image_landmark.cpp
@@ -126,7 +126,7 @@ int main(int argc, char** argv)
     // running CAPTURES preallocated
     CaptureImage::Ptr image_ptr;
     Vector6s data(Vector6s::Zero()); // will integrate this data repeatedly
-    CaptureMotion::Ptr cap_odo = std::make_shared<CaptureMotion>(TimeStamp(0), sen_odo_ptr, data);
+    CaptureMotion::Ptr cap_odo = std::make_shared<CaptureMotion>(TimeStamp(t), sen_odo_ptr, data);
     //=====================================================
 
 
