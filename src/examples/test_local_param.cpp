bda19ff178f2e93290abeb3dab5975df78b5f849
Joan Solà <jsola@iri.upc.edu>
2016-11-05 10:14:33 +0100

    Fix local Jacobian
    All tests agains numerical Jac passed

--- src/examples/test_local_param.cpp
+++ src/examples/test_local_param.cpp
@@ -49,6 +49,7 @@ int main(){
     da /= 10.0;
     Map<VectorXs> qo(&x(7),4);
     Map<MatrixXs> J(&M(0,0),4,3);
+    MatrixXs J_num(4,3);
 
     cout << "Initial values:" << endl;
     cout << "q  = " << q.transpose() << "   with norm = " << q.norm() << "\nda = " << da.transpose() << endl;
@@ -58,7 +59,7 @@ int main(){
     LocalParametrizationQuaternion<DQ_LOCAL> Qpar_loc;
     bool pass;
 
-    cout << "\nGLOBAL D_QUAT plus()" << endl;
+    cout << "\nGLOBAL D_QUAT plus() -------------------- " << endl;
     Map<const VectorXs> q_m(q.data(),4);
     Map<const VectorXs> da_m(da.data(),3);
     Qpar.plus(q_m,da_m,qo);
@@ -67,29 +68,28 @@ int main(){
     Qpar.computeJacobian(q_m,J);
     cout << " J = \n" << J << endl << endl;
 
-    MatrixXs J_num(4,3);
     JAC_NUMERIC(Qpar, q_m, J_num, 1e-9)
-    cout << " J_num = \n" << J_num;
+    cout << " J_num = \n" << J_num << std::endl;
 
     pass = (J-J_num).isMuchSmallerThan(1,1e-6);
-    std::cout << "Jacobians test " << (pass ? "PASSED" : "FAIL") << std::endl;
+    std::cout << "-------------------- Jacobians test " << (pass ? "PASSED" : "FAIL") << std::endl;
 
-    cout << "\nLOCAL D_QUAT plus()" << endl;
+    cout << "\nLOCAL D_QUAT plus() -------------------- " << endl;
     Qpar_loc.plus(q_m,da_m,qo);
     cout << "qo = " << qo.transpose() << "   with norm = " << qo.norm() << endl;
 
     Qpar_loc.computeJacobian(q_m,J);
-    cout << " J = " << J << endl;
+    cout << " J = \n" << J << endl;
 
     JAC_NUMERIC(Qpar_loc, q_m, J_num, 1e-9)
     cout << " J_num = \n" << J_num << endl;
 
     pass = (J-J_num).isMuchSmallerThan(1,1e-6);
-    std::cout << "Jacobians test " << (pass ? "PASSED" : "FAIL") << std::endl;
+    std::cout << "-------------------- Jacobians test " << (pass ? "PASSED" : "FAIL") << std::endl;
 
 
     // HOMOGENEOUS ----------------------------------------
-    cout << "\nHOMOGENEOUS plus()" << endl;
+    cout << "\nHOMOGENEOUS plus() -------------------- " << endl;
     Map<VectorXs> h(&x(11),4);
     h.setRandom();
     Map<VectorXs> d(&x(15),3);
@@ -101,20 +101,20 @@ int main(){
     cout << "d  = " << d.transpose() << endl;
 
     LocalParametrizationHomogeneous Hpar;
-    Map<const VectorXs> h_const(h.data(),4);
-    Map<const VectorXs> d_const(d.data(),3);
+    Map<const VectorXs> h_m(h.data(),4);
+    Map<const VectorXs> d_m(d.data(),3);
 
-    Hpar.plus(h_const,d_const,h_out);
+    Hpar.plus(h_m,d_m,h_out);
     cout << "\nh_out = " << h_out.transpose() << "   with norm: " << h_out.norm() << endl;
 
-    Hpar.computeJacobian(h_const,J);
-    cout << " J = " << J << "\n" << endl;
+    Hpar.computeJacobian(h_m,J);
+    cout << " J = \n" << J << endl;
 
-    JAC_NUMERIC(Hpar, q_m, J_num, 1e-9)
-    cout << " J_num = \n" << J_num << endl;
+    JAC_NUMERIC(Hpar, h_m, J_num, 1e-9)
+    cout << " J_num = \n" << J_num << std::endl;
 
     pass = (J-J_num).isMuchSmallerThan(1,1e-6);
-    std::cout << "Jacobians test " << (pass ? "PASSED" : "FAIL") << std::endl;
+    std::cout << "-------------------- Jacobians test " << (pass ? "PASSED" : "FAIL") << std::endl;
 
 
     return 0;
