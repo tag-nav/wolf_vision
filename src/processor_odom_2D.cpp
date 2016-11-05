e3a5107b538e5ad81905993ce58665da6328106e
Joan Solà <jsola@iri.upc.edu>
2016-11-05 14:31:23 +0100

    Fix creators with dummy params if not provided

--- src/processor_odom_2D.cpp
+++ src/processor_odom_2D.cpp
@@ -4,8 +4,20 @@ namespace wolf
 
 ProcessorBasePtr ProcessorOdom2D::create(const std::string& _unique_name, const ProcessorParamsBasePtr _params, const SensorBasePtr)
 {
-    std::shared_ptr<ProcessorParamsOdom2D> params = std::static_pointer_cast<ProcessorParamsOdom2D>(_params);
-    std::shared_ptr<ProcessorOdom2D> prc_ptr = std::make_shared<ProcessorOdom2D>(params->dist_traveled_th_, params->cov_det_th_, params->elapsed_time_th_);
+    Scalar dist_traveled_th = 1;
+    Scalar cov_det_th       = 1;
+    Scalar elapsed_time_th  = 1;
+    if (_params)
+    {
+        std::shared_ptr<ProcessorParamsOdom2D> params = std::static_pointer_cast<ProcessorParamsOdom2D>(_params);
+        dist_traveled_th = params->dist_traveled_th_;
+        cov_det_th       = params->cov_det_th_;
+        elapsed_time_th  = params->elapsed_time_th_;
+    }
+    else
+        std::cout << __FILE__ << ":" << __FUNCTION__ << "() : No parameters provided. Using dummy set." << std::endl;
+
+    std::shared_ptr<ProcessorOdom2D> prc_ptr = std::make_shared<ProcessorOdom2D>(dist_traveled_th, cov_det_th, elapsed_time_th);
     prc_ptr->setName(_unique_name);
     return prc_ptr;
 }
