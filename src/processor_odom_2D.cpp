513332f35c883ad00927ec5622d97638c98776c4
Joan Solà <jsola@iri.upc.edu>
2016-11-06 23:05:53 +0100

    Clarify code -- no functionality change

--- src/processor_odom_2D.cpp
+++ src/processor_odom_2D.cpp
@@ -4,9 +4,9 @@ namespace wolf
 
 ProcessorBasePtr ProcessorOdom2D::create(const std::string& _unique_name, const ProcessorParamsBasePtr _params, const SensorBasePtr)
 {
-    Scalar dist_traveled_th = 1;
-    Scalar cov_det_th       = 1;
-    Scalar elapsed_time_th  = 1;
+    Scalar dist_traveled_th ;
+    Scalar cov_det_th       ;
+    Scalar elapsed_time_th  ;
     if (_params)
     {
         std::shared_ptr<ProcessorParamsOdom2D> params = std::static_pointer_cast<ProcessorParamsOdom2D>(_params);
@@ -15,8 +15,12 @@ ProcessorBasePtr ProcessorOdom2D::create(const std::string& _unique_name, const 
         elapsed_time_th  = params->elapsed_time_th_;
     }
     else
+    {
         std::cout << __FILE__ << ":" << __FUNCTION__ << "() : No parameters provided. Using dummy set." << std::endl;
-
+        dist_traveled_th = 1;
+        cov_det_th       = 1;
+        elapsed_time_th  = 1;
+    }
     std::shared_ptr<ProcessorOdom2D> prc_ptr = std::make_shared<ProcessorOdom2D>(dist_traveled_th, cov_det_th, elapsed_time_th);
     prc_ptr->setName(_unique_name);
     return prc_ptr;
