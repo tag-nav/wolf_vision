61a11e23b7d8401e749347142104672b26c96140
Joan Solà <jsola@iri.upc.edu>
2016-11-15 22:14:31 +0100

    Typedef derived shared_ptr

--- src/constraint_fix.h
+++ src/constraint_fix.h
@@ -12,6 +12,8 @@ namespace wolf {
 class ConstraintFix: public ConstraintSparse<3,2,1>
 {
     public:
+        typedef std::shared_ptr<ConstraintFix> Ptr;
+    public:
 //        static const unsigned int N_BLOCKS = 2;
 
         ConstraintFix(FeatureBasePtr _ftr_ptr, bool _apply_loss_function = false, ConstraintStatus _status = CTR_ACTIVE) :
