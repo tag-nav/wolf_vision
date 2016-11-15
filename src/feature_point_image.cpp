756c1d72a1989ba565266c83802147198d24c79f
Jaime Tarrasó Martinez <jtarraso@iri.upc.edu>
2016-11-15 15:15:05 +0100

    Remove constructors from feature_point_image

--- src/feature_point_image.cpp
+++ src/feature_point_image.cpp
@@ -3,14 +3,6 @@
 
 namespace wolf {
 
-FeaturePointImage::FeaturePointImage(const Eigen::Vector2s & _measurement) :
-    FeatureBase("POINT IMAGE", _measurement,Eigen::MatrixXs::Zero(0,0)), is_known_(false)
-{
-    keypoint_.pt.x = float(measurement_(0));
-    keypoint_.pt.y = float(measurement_(1));
-    //
-}
-
 FeaturePointImage::~FeaturePointImage()
 {
     //
