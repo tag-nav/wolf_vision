d89c39b3f344cd659a790e3b31f5109a71aa07c0
Joan Solà <jsola@iri.upc.edu>
2016-11-22 21:40:57 +0100

    Remove constructors and commented code

--- src/frame_imu.cpp
+++ src/frame_imu.cpp
@@ -7,303 +7,24 @@
 
 namespace wolf {
 
-FrameIMU::FrameIMU(const TimeStamp& _ts, StateBlockPtr _p_ptr, StateBlockPtr _v_ptr, StateQuaternionPtr _q_ptr,
-                   StateBlockPtr _ba_ptr, StateBlockPtr _bg_ptr) :
-        FrameBase(_ts, _p_ptr, _q_ptr, _v_ptr)//,
-{
-    resizeStateBlockVec(5); // could have done push_back, but prefer more explicit locations for the StateBlocks
-    setStateBlockPtr(3, std::make_shared<StateBlock>(3)); // acc bias
-    setStateBlockPtr(4, std::make_shared<StateBlock>(3)); // gyro bias
-    setType("IMU");
-}
-
-FrameIMU::FrameIMU(const FrameType& _tp, const TimeStamp& _ts, StateBlockPtr _p_ptr, StateBlockPtr _v_ptr,
-                   StateQuaternionPtr _q_ptr, StateBlockPtr _ba_ptr, StateBlockPtr _bg_ptr) :
-        FrameBase(_tp, _ts, _p_ptr, _q_ptr, _v_ptr)
-{
-    resizeStateBlockVec(5); // could have done push_back, but prefer more explicit locations for the StateBlocks
-    setStateBlockPtr(3, std::make_shared<StateBlock>(3)); // acc bias
-    setStateBlockPtr(4, std::make_shared<StateBlock>(3)); // gyro bias
-    setType("IMU");
-}
-
 FrameIMU::FrameIMU(const FrameType& _tp, const TimeStamp& _ts, const Eigen::VectorXs& _x) :
         FrameBase(_tp, _ts, std::make_shared<StateBlock>(3), std::make_shared<StateQuaternion>(), std::make_shared<StateBlock>(3))
 {
+    assert(_x.size() == 16 && "Wrong vector size! Must be 16.");
+
     resizeStateBlockVec(5); // could have done push_back, but prefer more explicit locations for the StateBlocks
     setStateBlockPtr(3, std::make_shared<StateBlock>(3)); // acc bias
     setStateBlockPtr(4, std::make_shared<StateBlock>(3)); // gyro bias
-    assert(_x.size() == 16 && "Wrong vector size! Must be 16.");
     setState(_x);
     setType("IMU");
 }
 
 
-  FrameIMU::~FrameIMU()
-  {
-//      std::cout << "destructed   -F-IMU" << id() << std::endl;
-  }
+FrameIMU::~FrameIMU()
+{
+    //      std::cout << "destructed   -F-IMU" << id() << std::endl;
+}
 
-//<<<<<<< Updated upstream
-//  void FrameIMU::setState(const Eigen::VectorXs& _st) // Order: PVQ
-//  {
-//
-//      assert(_st.size() == ((getPPtr()==nullptr ? 0 : getPPtr()->getSize())    +
-//                            (getVPtr()==nullptr ? 0 : getVPtr()->getSize())    +
-//                            (getOPtr()==nullptr ? 0 : getOPtr()->getSize())    +
-//                            (getAccBiasPtr()==nullptr ? 0 : getAccBiasPtr()->getSize())  +
-//                            (getGyroBiasPtr()==nullptr ? 0 : getGyroBiasPtr()->getSize())) &&
-//                            "In FrameBase::setState wrong state size, should be 16!");
-//
-//      unsigned int index = 0;
-//      if (getPPtr()!=nullptr)
-//      {
-//          getPPtr()->setVector(_st.head(getPPtr()->getSize()));
-//          index += getPPtr()->getSize();
-////          std::cout << "set F-pos" << std::endl;
-//      }
-//      if (getVPtr()!=nullptr)
-//      {
-//          getVPtr()->setVector(_st.segment(index, getVPtr()->getSize()));
-//          index += getVPtr()->getSize();
-////          std::cout << "set F-vel" << std::endl;
-//      }
-//      if (getOPtr()!=nullptr)
-//      {
-//          getOPtr()->setVector(_st.segment(index, getOPtr()->getSize()));
-//          index += getOPtr()->getSize();
-////          std::cout << "set F-ori" << std::endl;
-//      }
-//      if (getAccBiasPtr()!=nullptr)
-//      {
-//          getAccBiasPtr()->setVector(_st.segment(index, getAccBiasPtr()->getSize()));
-//          index += getAccBiasPtr()->getSize();
-////          std::cout << "set F-ab" << std::endl;
-//      }
-//      if (getGyroBiasPtr()!=nullptr)
-//      {
-//          getGyroBiasPtr()->setVector(_st.segment(index, getGyroBiasPtr()->getSize()));
-//          //   index += bg_ptr_->getSize();
-////          std::cout << "set F-wb" << std::endl;
-//     }
-//  }
-//
-//  Eigen::VectorXs FrameIMU::getState() const
-//  {
-//      Eigen::VectorXs state((getPPtr()==nullptr ? 0 : getPPtr()->getSize())    +
-//                            (getVPtr()==nullptr ? 0 : getVPtr()->getSize())    +
-//                            (getOPtr()==nullptr ? 0 : getOPtr()->getSize())    +
-//                            (getAccBiasPtr()==nullptr ? 0 : getAccBiasPtr()->getSize())  +
-//                            (getGyroBiasPtr()==nullptr ? 0 : getGyroBiasPtr()->getSize()));
-//
-//      getState(state);
-//
-//      return state;
-//  }
-//
-//  void FrameIMU::getState(Eigen::VectorXs& state) const // Order: PVQBB
-//  {
-//      assert(state.size() == ((getPPtr()==nullptr ? 0 : getPPtr()->getSize())    +
-//                              (getVPtr()==nullptr ? 0 : getVPtr()->getSize())    +
-//                              (getOPtr()==nullptr ? 0 : getOPtr()->getSize())    +
-//                              (getAccBiasPtr()==nullptr ? 0 : getAccBiasPtr()->getSize())  +
-//                              (getGyroBiasPtr()==nullptr ? 0 : getGyroBiasPtr()->getSize()))
-//                              && "Wrong state size! Should be 16 for an IMU with biases!");
-//
-//      unsigned int index = 0;
-//      if (getPPtr()!=nullptr)
-//      {
-//          state.head(getPPtr()->getSize()) = getPPtr()->getVector();
-//          index += getPPtr()->getSize();
-//      }
-//      if (getVPtr()!=nullptr)
-//      {
-//          state.segment(index, getVPtr()->getSize()) = getVPtr()->getVector();
-//          index += getVPtr()->getSize();
-//      }
-//      if (getOPtr()!=nullptr)
-//      {
-//          state.segment(index, getOPtr()->getSize()) = getOPtr()->getVector();
-//          index += getOPtr()->getSize();
-//      }
-//      if (getAccBiasPtr()!=nullptr)
-//      {
-//          state.segment(index, getAccBiasPtr()->getSize()) = getAccBiasPtr()->getVector();
-//          index += getAccBiasPtr()->getSize();
-//      }
-//      if (getGyroBiasPtr()!=nullptr)
-//      {
-//          state.segment(index, getGyroBiasPtr()->getSize()) = getGyroBiasPtr()->getVector();
-//        //  index += bg_ptr_->getSize();
-//      }
-//  }
-//=======
-//  void FrameIMU::setState(const Eigen::VectorXs& _st) // Order: PVQ
-//  {
-//
-//      assert(_st.size() == ((getPPtr()==nullptr ? 0 : getPPtr()->getSize())    +
-//                            (getVPtr()==nullptr ? 0 : getVPtr()->getSize())    +
-//                            (getOPtr()==nullptr ? 0 : getOPtr()->getSize())    +
-//                            (getAccBiasPtr()==nullptr ? 0 : getAccBiasPtr()->getSize())  +
-//                            (getGyroBiasPtr()==nullptr ? 0 : getGyroBiasPtr()->getSize())) &&
-//                            "In FrameBase::setState wrong state size, should be 16!");
-//
-//      unsigned int index = 0;
-//      if (getPPtr()!=nullptr)
-//      {
-//          getPPtr()->setVector(_st.head(getPPtr()->getSize()));
-//          index += getPPtr()->getSize();
-////          std::cout << "set F-pos" << std::endl;
-//      }
-//      if (getVPtr()!=nullptr)
-//      {
-//          getVPtr()->setVector(_st.segment(index, getVPtr()->getSize()));
-//          index += getVPtr()->getSize();
-////          std::cout << "set F-vel" << std::endl;
-//      }
-//      if (getOPtr()!=nullptr)
-//      {
-//          getOPtr()->setVector(_st.segment(index, getOPtr()->getSize()));
-//          index += getOPtr()->getSize();
-////          std::cout << "set F-ori" << std::endl;
-//      }
-//      if (getAccBiasPtr()!=nullptr)
-//      {
-//          getAccBiasPtr()->setVector(_st.segment(index, getAccBiasPtr()->getSize()));
-//          index += getAccBiasPtr()->getSize();
-////          std::cout << "set F-ab" << std::endl;
-//      }
-//      if (getGyroBiasPtr()!=nullptr)
-//      {
-//          getGyroBiasPtr()->setVector(_st.segment(index, getGyroBiasPtr()->getSize()));
-//          //   index += bg_ptr_->getSize();
-////          std::cout << "set F-wb" << std::endl;
-//     }
-//  }
-//
-//  Eigen::VectorXs FrameIMU::getState() const
-//  {
-//      Eigen::VectorXs state((getPPtr()==nullptr ? 0 : getPPtr()->getSize())    +
-//                            (getVPtr()==nullptr ? 0 : getVPtr()->getSize())    +
-//                            (getOPtr()==nullptr ? 0 : getOPtr()->getSize())    +
-//                            (getAccBiasPtr()==nullptr ? 0 : getAccBiasPtr()->getSize())  +
-//                            (getGyroBiasPtr()==nullptr ? 0 : getGyroBiasPtr()->getSize()));
-//
-//      getState(state);
-//
-//      return state;
-//  }
-//
-//  void FrameIMU::getState(Eigen::VectorXs& state) const // Order: PVQBB
-//  {
-//      assert(state.size() == ((getPPtr()==nullptr ? 0 : getPPtr()->getSize())    +
-//                              (getVPtr()==nullptr ? 0 : getVPtr()->getSize())    +
-//                              (getOPtr()==nullptr ? 0 : getOPtr()->getSize())    +
-//                              (getAccBiasPtr()==nullptr ? 0 : getAccBiasPtr()->getSize())  +
-//                              (getGyroBiasPtr()==nullptr ? 0 : getGyroBiasPtr()->getSize())));
-//
-//      unsigned int index = 0;
-//      if (getPPtr()!=nullptr)
-//      {
-//          state.head(getPPtr()->getSize()) = getPPtr()->getVector();
-//          index += getPPtr()->getSize();
-//      }
-//      if (getVPtr()!=nullptr)
-//      {
-//          state.segment(index, getVPtr()->getSize()) = getVPtr()->getVector();
-//          index += getVPtr()->getSize();
-//      }
-//      if (getOPtr()!=nullptr)
-//      {
-//          state.segment(index, getOPtr()->getSize()) = getOPtr()->getVector();
-//          index += getOPtr()->getSize();
-//      }
-//      if (getAccBiasPtr()!=nullptr)
-//      {
-//          state.segment(index, getAccBiasPtr()->getSize()) = getAccBiasPtr()->getVector();
-//          index += getAccBiasPtr()->getSize();
-//      }
-//      if (getGyroBiasPtr()!=nullptr)
-//      {
-//          state.segment(index, getGyroBiasPtr()->getSize()) = getGyroBiasPtr()->getVector();
-//        //  index += bg_ptr_->getSize();
-//      }
-//  }
-//>>>>>>> Stashed changes
-
-//  void FrameIMU::setStatus(StateStatus _st)
-//  {
-//      // TODO: Separate the three fixes and unfixes to the wolfproblem lists
-//      // TODO: See what we want to do with globally fixing state blocks
-//      status_ = _st;
-//      // State Blocks
-//      if (status_ == ST_FIXED)
-//      {
-//          if (getPPtr() != nullptr)
-//          {
-//              getPPtr()->fix();
-//              if (getProblem() != nullptr)
-//                  getProblem()->updateStateBlockPtr(getPPtr());
-//          }
-//          if (getVPtr() != nullptr)
-//          {
-//              getVPtr()->fix();
-//              if (getProblem() != nullptr)
-//                  getProblem()->updateStateBlockPtr(getVPtr());
-//          }
-//          if (getOPtr() != nullptr)
-//          {
-//              getOPtr()->fix();
-//              if (getProblem() != nullptr)
-//                  getProblem()->updateStateBlockPtr(getOPtr());
-//          }
-//          if (getAccBiasPtr() != nullptr)
-//          {
-//              getAccBiasPtr()->fix();
-//              if (getProblem() != nullptr)
-//                  getProblem()->updateStateBlockPtr(getAccBiasPtr());
-//          }
-//          if (getGyroBiasPtr() != nullptr)
-//          {
-//              getGyroBiasPtr()->fix();
-//              if (getProblem() != nullptr)
-//                  getProblem()->updateStateBlockPtr(getGyroBiasPtr());
-//          }
-//      }
-//      else if (status_ == ST_ESTIMATED)
-//      {
-//          if (getPPtr() != nullptr)
-//          {
-//              getPPtr()->unfix();
-//              if (getProblem() != nullptr)
-//                  getProblem()->updateStateBlockPtr(getPPtr());
-//          }
-//          if (getVPtr() != nullptr)
-//          {
-//              getVPtr()->unfix();
-//              if (getProblem() != nullptr)
-//                  getProblem()->updateStateBlockPtr(getVPtr());
-//          }
-//          if (getOPtr() != nullptr)
-//          {
-//              getOPtr()->unfix();
-//              if (getProblem() != nullptr)
-//                  getProblem()->updateStateBlockPtr(getOPtr());
-//          }
-//          if (getAccBiasPtr() != nullptr)
-//          {
-//              getAccBiasPtr()->unfix();
-//              if (getProblem() != nullptr)
-//                  getProblem()->updateStateBlockPtr(getAccBiasPtr());
-//          }
-//          if (getGyroBiasPtr() != nullptr)
-//          {
-//              getGyroBiasPtr()->fix();
-//              if (getProblem() != nullptr)
-//                  getProblem()->updateStateBlockPtr(getGyroBiasPtr());
-//          }
-//      }
-//  }
 
 
 
@@ -311,16 +32,9 @@ FrameBasePtr FrameIMU::create(const FrameType & _tp,
                               const TimeStamp& _ts,
                               const Eigen::VectorXs& _x)
 {
-
     assert(_x.size() == 16 && "Wrong state vector size. Should be 16 for an IMU with biases!");
 
-    StateBlockPtr       p_ptr = std::make_shared<StateBlock>      (_x.segment<3>( 0  ));
-    StateQuaternionPtr  q_ptr = std::make_shared<StateQuaternion> (_x.segment<4>( 3  ));
-    StateBlockPtr       v_ptr = std::make_shared<StateBlock>      (_x.segment<3>( 7  ));
-    StateBlockPtr       a_ptr = std::make_shared<StateBlock>      (_x.segment<3>( 10 ));
-    StateBlockPtr       w_ptr = std::make_shared<StateBlock>      (_x.segment<3>( 13 ));
-
-    return std::make_shared<FrameIMU>(_tp, _ts, p_ptr, v_ptr, q_ptr, a_ptr, w_ptr);
+    return std::make_shared<FrameIMU>(_tp, _ts, _x);
 }
 
 } // namespace wolf
--- src/frame_imu.h
+++ src/frame_imu.h
@@ -22,72 +22,38 @@ namespace wolf {
           typedef std::weak_ptr<FrameIMU> WPtr;
 
       public:
-          /** \brief Constructor of non-key Frame with only time stamp
-           *
-           * Constructor with only time stamp
-           * \param _ts is the time stamp associated to this frame, provided in seconds
-           * \param _p_ptr StateBlock pointer to the position (default: nullptr)
-           * \param _v_ptr StateBlock pointer to the velocity (default: nullptr).
-           * \param _o_ptr StateBlock pointer to the orientation (default: nullptr). Pass a StateQuaternion if needed.
-           * \param _ba_ptr StateBlock pointer to the acceleration bias (default: nullptr).
-           * \param _bg_ptr StateBlock pointer to the gyrometer bias (default: nullptr).
-           **/
-        FrameIMU(const TimeStamp& _ts, StateBlockPtr _p_ptr, StateBlockPtr _v_ptr = nullptr, StateQuaternionPtr _q_ptr =
-                         nullptr,
-                 StateBlockPtr _ba_ptr = nullptr, StateBlockPtr _bg_ptr = nullptr);
-
-          /** \brief Constructor with type, time stamp and state pointer
-           *
-           * Constructor with type, time stamp and state pointer
-           * \param _tp indicates frame type. Generally either NON_KEY_FRAME or KEY_FRAME. (types defined at wolf.h)
-           * \param _ts is the time stamp associated to this frame, provided in seconds
-           * \param _p_ptr StateBlock pointer to the position (default: nullptr)
-           * \param _v_ptr StateBlock pointer to the velocity (default: nullptr).
-           * \param _o_ptr StateBlock pointer to the orientation (default: nullptr)
-           * \param _ba_ptr StateBlock pointer to the acceleration bias (default: nullptr).
-           * \param _bg_ptr StateBlock pointer to the gyrometer bias (default: nullptr).
-           **/
-        FrameIMU(const FrameType& _tp, const TimeStamp& _ts, StateBlockPtr _p_ptr, StateBlockPtr _v_ptr = nullptr,
-                 StateQuaternionPtr _q_ptr = nullptr, StateBlockPtr _ba_ptr = nullptr, StateBlockPtr _bg_ptr = nullptr);
 
           /** \brief Constructor with type, time stamp and state vector
-          * \param _tp indicates frame type. Generally either NON_KEY_FRAME or KEY_FRAME. (types defined at wolf.h)
+          * \param _tp indicates frame type. Either NON_KEY_FRAME or KEY_FRAME. (types defined in wolf.h)
           * \param _ts is the time stamp associated to this frame, provided in seconds
           * \param _x state vector of size 16, organized as [position, quaternion, velocity, acc_bias, gyro_bias]
           **/
          FrameIMU(const FrameType & _tp, const TimeStamp& _ts, const Eigen::VectorXs& _x);
           virtual ~FrameIMU();
 
-          // Frame values ------------------------------------------------
+          // State blocks ------------------------------------------------
 
           StateBlockPtr getAccBiasPtr() const;
           StateBlockPtr getGyroBiasPtr() const;
 
-//          void setState(const Eigen::VectorXs& _st);
-//          Eigen::VectorXs getState() const;
-//          void getState(Eigen::VectorXs& state) const;
-
-      private:
-          /** \brief Sets the Frame status (see wolf.h for Frame status)
-           **/
-//          void setStatus(StateStatus _st);
       public:
           static FrameBasePtr create(const FrameType & _tp,
                                      const TimeStamp& _ts,
                                      const Eigen::VectorXs& _x = Eigen::VectorXs::Zero(16));
 };
 
-  // IMPLEMENTATION //
+// IMPLEMENTATION //
 
-  inline StateBlockPtr FrameIMU::getAccBiasPtr() const
-  {
-      return getStateBlockPtr(3);
-  }
+inline StateBlockPtr FrameIMU::getAccBiasPtr() const
+{
+    return getStateBlockPtr(3);
+}
 
-  inline StateBlockPtr FrameIMU::getGyroBiasPtr() const
-  {
-      return getStateBlockPtr(4);
-  }
+inline StateBlockPtr FrameIMU::getGyroBiasPtr() const
+{
+    return getStateBlockPtr(4);
+}
+
 } // namespace wolf
 
 #endif
