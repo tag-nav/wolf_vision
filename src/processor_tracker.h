d6d6d02ce9f992f3b01fde8542fd74f233f4bd58
Joan Solà <jsola@iri.upc.edu>
2016-11-22 08:38:01 +0100

    Reorder members

--- src/processor_tracker.h
+++ src/processor_tracker.h
@@ -71,9 +71,9 @@ class ProcessorTracker : public ProcessorBase
         CaptureBasePtr origin_ptr_;             ///< Pointer to the origin of the tracker.
         CaptureBasePtr last_ptr_;               ///< Pointer to the last tracked capture.
         CaptureBasePtr incoming_ptr_;           ///< Pointer to the incoming capture being processed.
+        unsigned int max_new_features_;         ///< max features allowed to detect in one iteration. 0 = no limit
         FeatureBaseList new_features_last_;     ///< List of new features in \b last for landmark initialization and new key-frame creation.
         FeatureBaseList new_features_incoming_; ///< list of the new features of \b last successfully tracked in \b incoming
-        unsigned int max_new_features_;         ///< max features allowed to detect in one iteration. 0 = no limit
 
     public:
         ProcessorTracker(const std::string& _type, const unsigned int _max_new_features = 0, const Scalar& _time_tolerance = 0.1);
