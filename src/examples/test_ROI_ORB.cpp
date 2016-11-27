70dd385e02452af21d2b7f5d8ed65922f9b78680
Joan Solà <jsola@iri.upc.edu>
2016-11-27 20:26:34 +0100

    Cleanup of test

--- src/examples/test_ROI_ORB.cpp
+++ src/examples/test_ROI_ORB.cpp
@@ -1,3 +1,7 @@
+// wolf includes
+#include "wolf.h"
+#include "logging.h"
+
 //std includes
 #include <iostream>
 
@@ -15,22 +19,15 @@ int main(int argc, char** argv)
     const char * filename;
     if (argc == 1)
     {
-//        filename = "/home/jtarraso/Vídeos/gray.mp4";
         filename = "/home/jtarraso/Imágenes/Test_ORB.png";
         capture.open(filename);
-    }
-    else if (std::string(argv[1]) == "0")
-    {
-        //camera
-        filename = "0";
-        capture.open(0);
     }
     else
     {
         filename = argv[1];
         capture.open(filename);
     }
-    std::cout << "Input video file: " << filename << std::endl;
+    std::cout << "Input image file: " << filename << std::endl;
     if(!capture.isOpened()) std::cout << "failed" << std::endl; else std::cout << "succeded" << std::endl;
     capture.set(CV_CAP_PROP_POS_MSEC, 3000);
 
@@ -38,10 +35,7 @@ int main(int argc, char** argv)
     unsigned int img_height = capture.get(CV_CAP_PROP_FRAME_HEIGHT);
     std::cout << "Image size: " << img_width << "x" << img_height << std::endl;
 
-
-
     cv::Feature2D* detector_descriptor_ptr_;
-//    cv::DescriptorMatcher* matcher_ptr_;
 
     unsigned int nfeatures = 20;
     float scaleFactor = 1.2;
@@ -51,8 +45,6 @@ int main(int argc, char** argv)
     unsigned int WTA_K = 2;                  //# See: http://docs.opencv.org/trunk/db/d95/classcv_1_1ORB.html#a180ae17d3300cf2c619aa240d9b607e5
     unsigned int scoreType = 0;              //#enum { kBytes = 32, HARRIS_SCORE=0, FAST_SCORE=1 };
     unsigned int patchSize = 31;
-
-//    unsigned int fastThreshold = 20;
 
     unsigned int roi_width = 50;
     unsigned int roi_heigth = 50;
@@ -65,40 +57,12 @@ int main(int argc, char** argv)
                                            WTA_K, //
                                            scoreType, //
                                            patchSize);//,
-//                                           fastThreshold);
-
-//    unsigned int nominal_pattern_radius = 0;
-//    unsigned int pattern_radius = (unsigned int)( (nominal_pattern_radius) * pow(scaleFactor, nlevels-1));
-
-//    std::cout << "nominal pattern radius: " << _dd_params->nominal_pattern_radius << std::endl;
-//    std::cout << "scale factor: " << params_orb->scaleFactor << std::endl;
-//    std::cout << "nlevels: " << params_orb->nlevels << std::endl;
-
-//    unsigned int size_bits = detector_descriptor_ptr_->descriptorSize() * 8;
-
-//    matcher_ptr_ = new cv::BFMatcher(6);
-
-
-
-
-    unsigned int buffer_size = 20;
-    std::vector<cv::Mat> frame(buffer_size);
-    unsigned int f  = 1;
-    capture >> frame[f % buffer_size];
-
-
-//    image_ptr = new CaptureImage(t, camera_ptr_, frame[f % buffer_size]);
 
 
     std::vector<cv::KeyPoint> target_keypoints;
-//    std::vector<cv::KeyPoint> tracked_keypoints_;
-//    std::vector<cv::KeyPoint> tracked_keypoints_2;
-//    std::vector<cv::KeyPoint> current_keypoints;
     cv::Mat target_descriptors;
-//    cv::Mat tracked_descriptors;
-//    cv::Mat tracked_descriptors2;
-//    cv::Mat current_descriptors;
-    cv::Mat image_original = frame[f % buffer_size].clone();
+    cv::Mat image_original;
+    capture >> image_original;
     cv::Mat image;
     cv::Mat image_graphics = image_original;
     cv::KeyPointsFilter keypoint_filter;
@@ -149,12 +113,18 @@ int main(int argc, char** argv)
                 roi_inflate.height = roi_inflate.height+diff_height;
             }
 
-
             image = image_original.clone();
 
+            WOLF_TRACE("img size: ", image.cols , "x", image.rows);
+            WOLF_TRACE("roi     : (", roi_inflate.x , ",", roi_inflate.x , ") ", roi_inflate.width, "x", roi_inflate.height);
             cv::Mat image_roi = image(roi_inflate);
+
+            WOLF_TRACE("");
             detector_descriptor_ptr_->detect(image_roi, target_keypoints);
+
+            WOLF_TRACE("");
             detector_descriptor_ptr_->compute(image_roi, target_keypoints, target_descriptors);
+            WOLF_TRACE("");
 
             keypoint_filter.retainBest(target_keypoints,1);
             std::cout << "number of keypoints found: " << target_keypoints.size() << std::endl;
