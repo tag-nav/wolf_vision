f48070b00273ef7ff6e9f1db700d08be35af7c00
Dinesh Atchuthan <datchuth@laas.fr>
2016-12-13 15:31:32 +0100

    initialize delta vectors in featureIMU

--- src/feature_imu.cpp
+++ src/feature_imu.cpp
@@ -3,7 +3,8 @@
 namespace wolf {
 
 FeatureIMU::FeatureIMU(const Eigen::VectorXs& _delta_preintegrated, const Eigen::MatrixXs& _delta_preintegrated_covariance) :
-    FeatureBase("IMU", _delta_preintegrated, _delta_preintegrated_covariance)
+    FeatureBase("IMU", _delta_preintegrated, _delta_preintegrated_covariance),
+    dp_preint_(_delta_preintegrated.head<3>()),dv_preint_(_delta_preintegrated.segment<3>(7)), dq_preint_(_delta_preintegrated.segment<4>(3))
 {
     //std::cout << "New FeatureIMU: measurement " << _measurement.transpose() << std::endl << "covariance" << std::endl << _meas_covariance << std::endl;
 }
