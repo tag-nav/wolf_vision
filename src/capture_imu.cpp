1a7c393af69223e5760156f23919f0f933adad70
Dinesh Atchuthan <datchuth@laas.fr>
2017-01-05 15:15:55 +0100

    added constructors CaptureIMU

--- src/capture_imu.cpp
+++ src/capture_imu.cpp
@@ -3,13 +3,22 @@
 namespace wolf {
 
 
-CaptureIMU::CaptureIMU(const TimeStamp& _init_ts, SensorBasePtr _sensor_ptr,
-                             const Eigen::Vector6s& _acc_gyro_data) :
-        CaptureMotion(_init_ts, _sensor_ptr, _acc_gyro_data )
+CaptureIMU::CaptureIMU(const TimeStamp& _init_ts, SensorBasePtr _sensor_ptr, const Eigen::Vector6s& _acc_gyro_data, 
+                        FrameBasePtr _origin_frame_ptr) :
+        CaptureMotion(_init_ts, _sensor_ptr, _acc_gyro_data, _origin_frame_ptr)
 {
     setType("IMU");
 //    std::cout << "constructed    +C-IMU" << id() << std::endl;
 }
+
+CaptureIMU::CaptureIMU(const TimeStamp& _init_ts, SensorBasePtr _sensor_ptr, const Eigen::Vector6s& _acc_gyro_data, 
+                       const Eigen::MatrixXs& _data_cov, FrameBasePtr _origin_frame_ptr) :
+        CaptureMotion(_init_ts, _sensor_ptr, _acc_gyro_data, _data_cov, _origin_frame_ptr)
+{
+    setType("IMU");
+//    std::cout << "constructed    +C-IMU" << id() << std::endl;
+}
+
 
 CaptureIMU::~CaptureIMU()
 {
