ad62de84724f3c5dde3daeb88634d07e07416b6a
Dinesh Atchuthan <datchuth@laas.fr>
2017-01-06 13:54:06 +0100

    setFrame inside CaptureIMU constructor

--- src/capture_imu.cpp
+++ src/capture_imu.cpp
@@ -8,6 +8,7 @@ CaptureIMU::CaptureIMU(const TimeStamp& _init_ts, SensorBasePtr _sensor_ptr, con
         CaptureMotion(_init_ts, _sensor_ptr, _acc_gyro_data, _origin_frame_ptr)
 {
     setType("IMU");
+    this->setFramePtr(_origin_frame_ptr);
 //    std::cout << "constructed    +C-IMU" << id() << std::endl;
 }
 
@@ -16,6 +17,7 @@ CaptureIMU::CaptureIMU(const TimeStamp& _init_ts, SensorBasePtr _sensor_ptr, con
         CaptureMotion(_init_ts, _sensor_ptr, _acc_gyro_data, _data_cov, _origin_frame_ptr)
 {
     setType("IMU");
+    this->setFramePtr(_origin_frame_ptr);
 //    std::cout << "constructed    +C-IMU" << id() << std::endl;
 }
 
