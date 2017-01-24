47e668983e2a012df7595fe76dfbdc3ad134c19a
Dinesh Atchuthan <datchuth@laas.fr>
2017-01-24 10:49:13 +0100

    fixed TEST(ProcessorIMU, static_ceresOptimiszation_fixBias)

--- src/test/gtest_ceres.cpp
+++ src/test/gtest_ceres.cpp
@@ -69,7 +69,7 @@ TEST(ProcessorOdom3D, static_ceresOptimiszation)
 
                                              /************** SOLVER PART  **************/
      ceres::Solver::Summary summary = ceres_manager_wolf_diff->solve();
-     std::cout << summary.BriefReport() << std::endl;
+     std::cout << summary.FullReport() << std::endl;
 
     // COMPUTE COVARIANCES
     std::cout << "\t\t\t ______computing covariances______" << std::endl;
@@ -77,8 +77,13 @@ TEST(ProcessorOdom3D, static_ceresOptimiszation)
     std::cout << "\t\t\t ______computed!______" << std::endl;
 }
 
-TEST(ProcessorIMU, static_ceresOptimiszation)
+TEST(ProcessorIMU, static_ceresOptimiszation_fixBias)
 {
+
+    //With IMU data only, biases are not observable ! So covariance cannot be computed due to jacobian rank deficiency.
+    // We must add an odometry to make covariances observable Or... we could fix all bias stateBlocks
+    //First we will try to fix bias stateBlocks
+
     using std::shared_ptr;
     using std::make_shared;
     using std::static_pointer_cast;
@@ -89,54 +94,59 @@ TEST(ProcessorIMU, static_ceresOptimiszation)
     // Wolf problem
     ProblemPtr wolf_problem_ptr_ = Problem::create(FRM_PQVBB_3D);
 
-    SensorBasePtr sen = wolf_problem_ptr_->installSensor("IMU", "Main IMU", (Vector7s()<<0,0,0,0,0,0,1).finished(), wolf_root + "/src/examples/sensor_imu.yaml");
-    ProcessorBasePtr processor_ptr = wolf_problem_ptr_->installProcessor("IMU", "IMU pre-integrator", "Main IMU", "");
+    SensorBasePtr sen_imu = wolf_problem_ptr_->installSensor("IMU", "Main IMU", (Vector7s()<<0,0,0,0,0,0,1).finished(), wolf_root + "/src/examples/sensor_imu.yaml");
+    ProcessorBasePtr processor_ptr = wolf_problem_ptr_->installProcessor("IMU", "IMU pre-integrator", "Main IMU", wolf_root + "/src/examples/processor_imu.yaml");
 
     //setting origin
     Eigen::VectorXs x0(16);
     TimeStamp t(0);
-    x0 << 0,0,0,  0,0,0,1,  0,0,0,  0,0,.000,  0,0,.000;
+    x0 << 0,0,0,  0,0,0,1,  0,0,0,  0,0,.001,  0,0,.002;
     wolf_problem_ptr_->getProcessorMotionPtr()->setOrigin(x0, t); //this also creates a keyframe at origin
+    wolf_problem_ptr_->getTrajectoryPtr()->getFrameList().front()->fix();
 
     // Ceres wrappers
     ceres::Solver::Options ceres_options;
     ceres_options.minimizer_type = ceres::TRUST_REGION; //ceres::TRUST_REGION;ceres::LINE_SEARCH
     ceres_options.max_line_search_step_contraction = 1e-3;
-    //ceres_options.max_num_iterations = 1e4;
+    ceres_options.max_num_iterations = 1e4;
     CeresManager* ceres_manager_wolf_diff = new CeresManager(wolf_problem_ptr_, ceres_options, true);
 
                                              /************** USE IMU CLASSES  **************/
     Eigen::Vector6s data;
-    data << 0.15,0.10,9.88, 0.023,0.014,0.06;
+    //data << 0.15,0.10,9.88, 0.023,0.014,0.06;
+    data << 0.0,0.0,9.81, 0.0,0.0,0.0;
     Scalar dt = t.get();
     TimeStamp ts(0);
-    while((dt-t.get())<=std::static_pointer_cast<ProcessorIMU>(processor_ptr)->getMaxTimeSpan()+0.001){
+    while((dt-t.get())<=std::static_pointer_cast<ProcessorIMU>(processor_ptr)->getMaxTimeSpan()){
     // Time and data variables
     dt += 0.001;
     ts.set(dt);
 
-    wolf::CaptureIMUPtr imu_ptr = std::make_shared<CaptureIMU>(ts, sen, data);
-    wolf_problem_ptr_->getTrajectoryPtr()->getFrameList().front()->fix();
+    wolf::CaptureIMUPtr imu_ptr = std::make_shared<CaptureIMU>(ts, sen_imu, data);
     // process data in capture
-    sen->process(imu_ptr);
+    sen_imu->process(imu_ptr);
     }
 
-    /* We do not need to create features and frames and constraints here. Everything is done in wolf.
-    Features and constraint at created automatically when a new Keyframe is generated. Whether a new keyframe should be created or not, this is
-    handled by voteForKeyFrame() function for this processorMotion
-    */
+    //Fix all biases StateBlocks
+    for(FrameBasePtr it : wolf_problem_ptr_->getTrajectoryPtr()->getFrameList()){
+        ( std::static_pointer_cast<FrameIMU>(it) )->getAccBiasPtr()->fix();
+        ( std::static_pointer_cast<FrameIMU>(it) )->getGyroBiasPtr()->fix();
+    }
+    // Fix also sensor bias
+    sen_imu->getStateBlockPtr(2)->fix();
 
+    //Check and print wolf tree
     if(wolf_problem_ptr_->check(1)){
         wolf_problem_ptr_->print(4,1,1,1);
     }
-
+    
                                              /************** SOLVER PART  **************/
      ceres::Solver::Summary summary = ceres_manager_wolf_diff->solve();
-     std::cout << summary.BriefReport() << std::endl;
+     std::cout << summary.FullReport() << std::endl;
      
     // COMPUTE COVARIANCES
     std::cout << "\t\t\t ______computing covariances______" << std::endl;
-    ceres_manager_wolf_diff->computeCovariances(ALL_MARGINALS);//ALL_MARGINALS, ALL
+    ceres_manager_wolf_diff->computeCovariances(ALL);//ALL_MARGINALS, ALL
     std::cout << "\t\t\t ______computed!______" << std::endl;
 }
 
