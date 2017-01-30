629b1cc31c3518eb7b66b996541e999907851b03
Dinesh Atchuthan <datchuth@laas.fr>
2017-01-30 10:21:52 +0100

    [WIP] gtest using 1 processorMotion + 1 processorIMU

--- src/test/gtest_ceres.cpp
+++ src/test/gtest_ceres.cpp
@@ -17,6 +17,7 @@
 #include "problem.h"
 #include "ceres_wrapper/ceres_manager.h"
 #include "state_quaternion.h"
+#include "sensor_imu.h"
 
 #include <iostream>
 
@@ -79,7 +80,6 @@ TEST(ProcessorOdom3D, static_ceresOptimiszation)
 
 TEST(ProcessorIMU, static_ceresOptimiszation_fixBias)
 {
-
     //With IMU data only, biases are not observable ! So covariance cannot be computed due to jacobian rank deficiency.
     // We must add an odometry to make covariances observable Or... we could fix all bias stateBlocks
     //First we will try to fix bias stateBlocks
@@ -150,6 +150,142 @@ TEST(ProcessorIMU, static_ceresOptimiszation_fixBias)
     std::cout << "\t\t\t ______computed!______" << std::endl;
 }
 
+TEST(ProcessorIMU, static_ceresOptimiszation_Odom)
+{
+    //With IMU data only, biases are not observable ! So covariance cannot be computed due to jacobian rank deficiency.
+    // We must add an odometry to make covariances observable Or... we could fix all bias stateBlocks
+    //First we will try to fix bias stateBlocks
+
+    using std::shared_ptr;
+    using std::make_shared;
+    using std::static_pointer_cast;
+
+    //===================================================== SETTING PROBLEM
+    std::string wolf_root = _WOLF_ROOT_DIR;
+
+    // WOLF PROBLEM
+    ProblemPtr wolf_problem_ptr_ = Problem::create(FRM_PQVBB_3D);
+    Eigen::VectorXs x0(16);
+    x0 << 0,0,0,  0,0,0,1,  0,0,0,  0,0,.001,  0,0,.002;
+    TimeStamp t(0);
+    wolf_problem_ptr_->setOrigin(x0, Eigen::Matrix6s::Identity() * 0.001, t);
+
+    // CERES WRAPPER
+    ceres::Solver::Options ceres_options;
+    ceres_options.minimizer_type = ceres::TRUST_REGION; //ceres::TRUST_REGION;ceres::LINE_SEARCH
+    ceres_options.max_line_search_step_contraction = 1e-3;
+    ceres_options.max_num_iterations = 1e4;
+    CeresManager* ceres_manager_wolf_diff = new CeresManager(wolf_problem_ptr_, ceres_options, true);
+
+    wolf_problem_ptr_->print(4,1,1,1);
+
+    // SENSOR + PROCESSOR IMU
+    SensorBasePtr sen0_ptr = wolf_problem_ptr_->installSensor("IMU", "Main IMU", (Vector7s()<<0,0,0,0,0,0,1).finished(), wolf_root + "/src/examples/sensor_imu.yaml");
+    ProcessorBasePtr processor_ptr_ = wolf_problem_ptr_->installProcessor("IMU", "IMU pre-integrator", "Main IMU", wolf_root + "/src/examples/processor_imu.yaml");
+    SensorIMUPtr sen_imu = std::static_pointer_cast<SensorIMU>(sen0_ptr);
+    ProcessorIMUPtr processor_ptr_imu = std::static_pointer_cast<ProcessorIMU>(processor_ptr_);
+
+    // SET ORIGIN AND FIX ORIGIN KEYFRAME
+    //wolf_problem_ptr_->getProcessorMotionPtr()->setOrigin(x0, t); //this also creates a keyframe at origin
+    wolf_problem_ptr_->getTrajectoryPtr()->getFrameList().front()->fix();
+
+    wolf_problem_ptr_->print(4,1,1,1);
+
+    // SENSOR + PROCESSOR ODOM 3D
+    SensorBasePtr sen1_ptr = wolf_problem_ptr_->installSensor("ODOM 3D", "odom", (Vector7s()<<0,0,0,0,0,0,1).finished(), wolf_root + "/src/examples/sensor_odom_3D.yaml");
+    ProcessorBasePtr processor_ptr_odom = wolf_problem_ptr_->installProcessor("ODOM 3D", "odom", "odom", wolf_root + "/src/examples/processor_odom_3D.yaml");
+    SensorOdom3DPtr sen_odom3D = std::static_pointer_cast<SensorOdom3D>(sen1_ptr);
+    ProcessorOdom3DPtr processor_ptr_odom3D = std::static_pointer_cast<ProcessorOdom3D>(processor_ptr_odom);
+
+    // There should be a FrameIMU at origin as KeyFrame + 1 FrameIMU and 1 FrameOdom Non-KeyFrame
+    ASSERT_EQ(wolf_problem_ptr_->getTrajectoryPtr()->getFrameList().size(),3);
+    wolf_problem_ptr_->print(4,1,1,1);
+    //There should be 3 captures at origin_frame : CaptureOdom, captureIMU + CaptureFix due to setting problem origin before installing processors
+    EXPECT_EQ((wolf_problem_ptr_->getTrajectoryPtr()->getFrameList().front())->getCaptureList().size(),3);
+    /*for ( for CaptureBasePtr C : (wolf_problem_ptr_->getTrajectoryPtr()->getFrameList().front())->getCaptureList() )
+    {
+
+    }*/
+    ASSERT_TRUE(wolf_problem_ptr_->getTrajectoryPtr()->getFrameList().front()->isKey()) << "origin_frame is not a KeyFrame..." << std::endl;
+
+    /*wolf_problem_ptr_->setProcessorMotion(processor_ptr_odom3D);
+    wolf_problem_ptr_->getProcessorMotionPtr()->setOrigin(x0.head(7), t); //this also creates a keyframe at origin
+    wolf_problem_ptr_->getTrajectoryPtr()->getFrameList().front()->fix();*/
+
+    //===================================================== END{SETTING PROBLEM}
+
+    //===================================================== PROCESS DATA
+
+    // PROCESS IMU DATA
+                                             std::cout << " STARTING " << std::endl;
+    Eigen::Vector6s data;
+    Eigen::Vector6s data_odom3D;
+    data << 0.0019, 0.0001, 9.8122, 0.1022, 0.1171, -0.0413;
+    Scalar dt = t.get();
+    TimeStamp ts(0.001);
+    wolf::CaptureIMUPtr imu_ptr = std::make_shared<CaptureIMU>(ts, sen_imu, data);
+    //sen_imu->process(imu_ptr);
+
+    //data_odom3D << 0,0,0, 0,0,0;
+    //Add an Odom3D constraint
+    //wolf::CaptureMotionPtr mot_ptr = std::make_shared<CaptureMotion>(t, sen_odom3D, data_odom3D);
+    //sen_odom3D->process(mot_ptr);
+
+    wolf_problem_ptr_->setProcessorMotion(processor_ptr_imu);
+    WOLF_DEBUG("Processor Odom front ts : ", processor_ptr_odom3D->getBuffer().get().front().ts_);
+
+    while( (dt-t.get()) < (std::static_pointer_cast<ProcessorIMU>(processor_ptr_)->getMaxTimeSpan()) ){
+        
+        // Time and data variables
+        dt += 0.001;
+        ts.set(dt);
+        imu_ptr->setTimeStamp(ts);
+        imu_ptr->setData(data);
+
+        // process data in capture
+        imu_ptr->getTimeStamp();
+        sen_imu->process(imu_ptr);
+    }
+    WOLF_DEBUG("Processor Odom front ts : ", processor_ptr_odom3D->getBuffer().get().front().ts_);
+    wolf_problem_ptr_->setProcessorMotion(processor_ptr_odom3D);
+    // PROCESS ODOM 3D DATA
+    /*std::cout << "doing ODOM" << std::endl;
+    CaptureBasePtr origin_cap = processor_ptr_odom3D->getOriginPtr();
+    std::cout << "debug0" << std::endl;
+    FrameBasePtr origin_fr = std::static_pointer_cast<CaptureMotion>(origin_cap)->getOriginFramePtr();
+    std::cout << "debug1" << std::endl;
+    wolf::TimeStamp origin_stamp = processor_ptr_odom3D->getOriginPtr()->getTimeStamp();
+    std::cout << "debug2" << std::endl;*/
+    data_odom3D << 0,0,0, 0,0,0;
+    //Add an Odom3D constraint
+    //dt += 0.001;
+    ts.set(dt);
+    wolf::CaptureMotionPtr mot_ptr = std::make_shared<CaptureMotion>(t, sen_odom3D, data_odom3D);
+    mot_ptr = std::make_shared<CaptureMotion>(ts, sen_odom3D, data_odom3D);
+    sen_odom3D->process(mot_ptr);
+
+    //===================================================== END{PROCESS DATA}
+
+    //===================================================== SOLVER PART
+
+    //Check and print wolf tree
+    //wolf_problem_ptr_->print(4,1,1,1);
+    /*if(wolf_problem_ptr_->check(1)){
+        wolf_problem_ptr_->print(4,1,1,1);
+    }*/
+     
+    std::cout << "\t\t\t ______solving______" << std::endl;
+    ceres::Solver::Summary summary = ceres_manager_wolf_diff->solve();
+    std::cout << summary.FullReport() << std::endl;
+    std::cout << "\t\t\t ______solved______" << std::endl;
+     
+    // COMPUTE COVARIANCES
+    std::cout << "\t\t\t ______computing covariances______" << std::endl;
+    ceres_manager_wolf_diff->computeCovariances(ALL);//ALL_MARGINALS, ALL
+    std::cout << "\t\t\t ______computed!______" << std::endl;
+
+    //===================================================== END{SOLVER PART}
+}
 
 int main(int argc, char **argv)
 {
