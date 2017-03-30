7ed70420e913d8ca4fcc52b3958b6d2c780088d6
Joan Solà <jsola@iri.upc.edu>
2017-03-31 00:00:04 +0200

    Add tests for ConstraintFix and Fix3D

---
+++ src/test/gtest_constraint_fix.cpp
@@ -0,0 +1,78 @@
+/**
+ * \file gtest_constraint_fix.cpp
+ *
+ *  Created on: Mar 30, 2017
+ *      \author: jsola
+ */
+
+
+#include "utils_gtest.h"
+
+#include "constraint_fix.h"
+#include "capture_motion.h"
+
+#include "ceres_wrapper/ceres_manager.h"
+
+
+using namespace Eigen;
+using namespace wolf;
+using std::cout;
+using std::endl;
+
+// Input data and covariance
+Vector3s pose6(Vector3s::Random());
+Vector3s data_var((Vector3s() << 0.2,0.2,0.2).finished());
+Matrix3s data_cov = data_var.asDiagonal();
+
+// perturbated priors
+Vector3s x0 = pose6 + Vector3s::Random()*0.25;
+
+// Problem and solver
+ProblemPtr problem = Problem::create(FRM_PO_2D);
+CeresManager ceres_mgr(problem);
+
+// Two frames
+FrameBasePtr frm0 = problem->emplaceFrame(KEY_FRAME, problem->zeroState(), TimeStamp(0));
+
+// Capture, feature and constraint from frm1 to frm0
+CaptureBasePtr cap0 = frm0->addCapture(std::make_shared<CaptureMotion>(0, nullptr, pose6));
+FeatureBasePtr fea0 = cap0->addFeature(std::make_shared<FeatureBase>("ODOM 3D", pose6, data_cov));
+ConstraintFixPtr ctr0(std::make_shared<ConstraintFix>(fea0));
+
+
+////////////////////////////////////////////////////////
+
+TEST(ConstraintFix, check_tree)
+{
+    fea0->addConstraint(ctr0);
+    frm0->addConstrainedBy(ctr0);
+
+    ASSERT_TRUE(problem->check(0));
+}
+
+//TEST(ConstraintFix, expectation)
+//{
+//    ASSERT_EIGEN_APPROX(ctr0->expectation() , delta);
+//}
+
+TEST(ConstraintFix, solve)
+{
+
+    // Fix frame 0, perturb frm1
+    frm0->unfix();
+    frm0->setState(x0);
+
+    // solve for frm0
+    ceres::Solver::Summary summary = ceres_mgr.solve();
+
+    ASSERT_EIGEN_APPROX(frm0->getState(), pose6);
+
+}
+
+
+int main(int argc, char **argv)
+{
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
+
---
+++ src/test/gtest_constraint_fix_3D.cpp
@@ -0,0 +1,85 @@
+/**
+ * \file gtest_constraint_fix_3D.cpp
+ *
+ *  Created on: Mar 30, 2017
+ *      \author: jsola
+ */
+
+
+#include "utils_gtest.h"
+
+#include "constraint_fix_3D.h"
+#include "capture_motion.h"
+
+#include "ceres_wrapper/ceres_manager.h"
+
+
+using namespace Eigen;
+using namespace wolf;
+using std::cout;
+using std::endl;
+
+Vector7s pose6toPose7(Vector6s _pose6)
+{
+    return (Vector7s() << _pose6.head<3>() , v2q(_pose6.tail<3>()).coeffs()).finished();
+}
+
+
+// Input pose6 and covariance
+Vector6s pose6(Vector6s::Random());
+Vector7s pose7 = pose6toPose7(pose6);
+Vector6s data_var((Vector6s() << 0.2,0.2,0.2,0.2,0.2,0.2).finished());
+Matrix6s data_cov = data_var.asDiagonal();
+
+// perturbated priors
+Vector7s x0 = pose6toPose7(pose6 + Vector6s::Random()*0.25);
+
+// Problem and solver
+ProblemPtr problem = Problem::create(FRM_PO_3D);
+CeresManager ceres_mgr(problem);
+
+// Two frames
+FrameBasePtr frm0 = problem->emplaceFrame(KEY_FRAME, problem->zeroState(), TimeStamp(0));
+
+// Capture, feature and constraint
+CaptureBasePtr cap0 = frm0->addCapture(std::make_shared<CaptureMotion>(0, nullptr, pose7));
+FeatureBasePtr fea0 = cap0->addFeature(std::make_shared<FeatureBase>("ODOM 3D", pose7, data_cov));
+ConstraintFix3DPtr ctr0(std::make_shared<ConstraintFix3D>(fea0));
+
+
+////////////////////////////////////////////////////////
+
+TEST(ConstraintFix3D, check_tree)
+{
+    fea0->addConstraint(ctr0);
+    frm0->addConstrainedBy(ctr0);
+
+    ASSERT_TRUE(problem->check(0));
+}
+
+//TEST(ConstraintFix3D, expectation)
+//{
+//    ASSERT_EIGEN_APPROX(ctr0->expectation() , delta);
+//}
+
+TEST(ConstraintFix3D, solve)
+{
+
+    // Fix frame 0, perturb frm1
+    frm0->unfix();
+    frm0->setState(x0);
+
+    // solve for frm0
+    ceres::Solver::Summary summary = ceres_mgr.solve();
+
+    ASSERT_EIGEN_APPROX(frm0->getState(), pose7);
+
+}
+
+
+int main(int argc, char **argv)
+{
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
+
