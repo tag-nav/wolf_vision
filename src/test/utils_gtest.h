468422e1003efcbb3479d1afca7a8791a3c51038
Joan Sola <jsola@iri.upc.edu>
2017-03-31 13:17:03 +0200

    New macro EXPECT_EIGEN_APPROX

--- src/test/utils_gtest.h
+++ src/test/utils_gtest.h
@@ -55,6 +55,11 @@ public:
 
 #define TEST_COUT testing::internal::TestCout()
 
+#define EXPECT_EIGEN_APPROX(C_expect, C_actual) EXPECT_PRED2([](const MatrixXs lhs, const MatrixXs rhs) { \
+                  return (lhs - rhs).isMuchSmallerThan(1, wolf::Constants::EPS); \
+               }, \
+               C_expect, C_actual);
+
 #define ASSERT_EIGEN_APPROX(C_expect, C_actual) ASSERT_PRED2([](const MatrixXs lhs, const MatrixXs rhs) { \
                   return (lhs - rhs).isMuchSmallerThan(1, wolf::Constants::EPS); \
                }, \
