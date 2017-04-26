5032355c7e61217c8b3123dc1264d2a9b8548dec
Joan Sola <jsola@iri.upc.edu>
2017-04-26 17:43:58 +0200

    Use constraints with 2 state blocks in constraint_sparse tests

--- src/test/gtest_constraint_sparse.cpp
+++ src/test/gtest_constraint_sparse.cpp
@@ -14,11 +14,11 @@ using namespace wolf;
 
 // Dummy class for avoiding the pure virtual compilation error
 template <JacobianMethod J>
-class ConstraintSparseObject : public ConstraintSparse<1, 1>
+class ConstraintSparseObject : public ConstraintSparse<1, 2, 1>
 {
     public:
-        ConstraintSparseObject(ConstraintType _tp, bool _apply_loss_function, ConstraintStatus _status, StateBlockPtr _sb) :
-                ConstraintSparse<1, 1>(_tp, _apply_loss_function, _status, _sb)
+        ConstraintSparseObject(ConstraintType _tp, bool _apply_loss_function, ConstraintStatus _status, StateBlockPtr _xy, StateBlockPtr _theta) :
+                ConstraintSparse<1, 2, 1>(_tp, _apply_loss_function, _status, _xy, _theta)
         {
             //
         }
@@ -32,16 +32,16 @@ class ConstraintSparseObject : public ConstraintSparse<1, 1>
 
 TEST(ConstraintSparseAnalytic, Constructor)
 {
-    ConstraintSparseObject<JAC_ANALYTIC> ctr_analytic(CTR_AHP, false, CTR_ACTIVE, std::make_shared<StateBlock>(1));
+    ConstraintSparseObject<JAC_ANALYTIC> ctr_analytic(CTR_AHP, false, CTR_ACTIVE, std::make_shared<StateBlock>(2), std::make_shared<StateBlock>(1));
     ASSERT_EQ(ctr_analytic.getJacobianMethod(),     JAC_ANALYTIC);
     ASSERT_EQ(ctr_analytic.getApplyLossFunction(),  false);
     ASSERT_EQ(ctr_analytic.getStatus(),             CTR_ACTIVE);
     ASSERT_EQ(ctr_analytic.getSize(),               1);
 
-    ConstraintSparseObject<JAC_AUTO> ctr_auto(CTR_AHP, false, CTR_ACTIVE, std::make_shared<StateBlock>(1));
+    ConstraintSparseObject<JAC_AUTO> ctr_auto(CTR_AHP, false, CTR_ACTIVE, std::make_shared<StateBlock>(2), std::make_shared<StateBlock>(1));
     ASSERT_EQ(ctr_auto.getJacobianMethod(), JAC_AUTO);
 
-    ConstraintSparseObject<JAC_NUMERIC> ctr_numeric(CTR_AHP, false, CTR_ACTIVE, std::make_shared<StateBlock>(1));
+    ConstraintSparseObject<JAC_NUMERIC> ctr_numeric(CTR_AHP, false, CTR_ACTIVE, std::make_shared<StateBlock>(2), std::make_shared<StateBlock>(1));
     ASSERT_EQ(ctr_numeric.getJacobianMethod(), JAC_NUMERIC);
 }
 
