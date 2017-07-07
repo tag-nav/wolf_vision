66472fbd2e04f3e2591d04d233906b921ee0b992
Dinesh Atchuthan <datchuth@laas.fr>
2017-07-07 11:34:10 +0200

    remove use of data files

--- src/test/gtest_constraint_imu.cpp
+++ src/test/gtest_constraint_imu.cpp
@@ -746,6 +746,8 @@ class ConstraintIMU_biasTest_Move_NonNullBiasRotAndVCst : public testing::Test
     virtual void TearDown(){}
 };
 
+// var(b1,b2), inv(p1,q1,v1,p2,q2,v2); fac1: imu(p,q,v)+(b1=b2)
+
 class ConstraintIMU_biasTest_Move_NonNullBiasRot : public testing::Test
 {
     public:
@@ -768,24 +770,6 @@ class ConstraintIMU_biasTest_Move_NonNullBiasRot : public testing::Test
         using std::static_pointer_cast;
 
         std::string wolf_root = _WOLF_ROOT_DIR;
-
-        //===================================================== INPUT FILES
-
-        char* imu_filepath;
-
-        std::string imu_filepath_string(wolf_root + "/src/test/data/IMU/data_bias_check_Rot.txt");
-
-        imu_filepath = new char[imu_filepath_string.length() + 1];
-
-        std::strcpy(imu_filepath, imu_filepath_string.c_str());
-        std::ifstream imu_data_input;
-
-        imu_data_input.open(imu_filepath);
-        if(!imu_data_input.is_open()){
-            std::cerr << "Failed to open data files... Exiting" << std::endl;
-            ADD_FAILURE();
-        }
-        //===================================================== END{INPUT FILES}
         
         //===================================================== SETTING PROBLEM
         // WOLF PROBLEM
@@ -793,30 +777,27 @@ class ConstraintIMU_biasTest_Move_NonNullBiasRot : public testing::Test
 
         // CERES WRAPPER
         ceres::Solver::Options ceres_options;
-        ceres_options.minimizer_type = ceres::TRUST_REGION; //ceres::TRUST_REGION;ceres::LINE_SEARCH
+        ceres_options.minimizer_type = ceres::TRUST_REGION;
         ceres_options.max_line_search_step_contraction = 1e-3;
         ceres_options.max_num_iterations = 1e4;
         ceres_manager_wolf_diff = new CeresManager(wolf_problem_ptr_, ceres_options, true);
 
         // SENSOR + PROCESSOR IMU
         SensorBasePtr sen0_ptr = wolf_problem_ptr_->installSensor("IMU", "Main IMU", (Vector7s()<<0,0,0,0,0,0,1).finished(), wolf_root + "/src/examples/sensor_imu.yaml");
-        processor_ptr_ = wolf_problem_ptr_->installProcessor("IMU", "IMU pre-integrator", "Main IMU", wolf_root + "/src/examples/processor_imu_t6.yaml");
+        processor_ptr_ = wolf_problem_ptr_->installProcessor("IMU", "IMU pre-integrator", "Main IMU", wolf_root + "/src/examples/processor_imu_t1.yaml");
         sen_imu = std::static_pointer_cast<SensorIMU>(sen0_ptr);
         processor_ptr_imu = std::static_pointer_cast<ProcessorIMU>(processor_ptr_);
     
         //===================================================== END{SETTING PROBLEM}
-
         //===================================================== INITIALIZATION
 
         expected_final_state.resize(16);
         x_origin.resize(16);
         x_origin << 0,0,0, 0,0,0,1, 0,0,0, 0,0,0, 0,0,0;
+        origin_bias << 0.0015, 0.004, -0.002, 0.005, -0.0074, -0.003;
         t.set(0);
+        Eigen::Quaternions current_quatState(Eigen::Quaternions::Identity());
 
-        imu_data_input >> x_origin[0] >> x_origin[1] >> x_origin[2] >> x_origin[6] >> x_origin[3] >> x_origin[4] >> x_origin[5] >> x_origin[7] >> x_origin[8] >> x_origin[9];
-        imu_data_input >> origin_bias[0] >> origin_bias[1] >> origin_bias[2] >> origin_bias[3] >> origin_bias[4] >> origin_bias[5];
-        imu_data_input >> expected_final_state[0] >> expected_final_state[1] >> expected_final_state[2] >> expected_final_state[6] >> expected_final_state[3] >>
-                    expected_final_state[4] >> expected_final_state[5] >> expected_final_state[7] >> expected_final_state[8] >> expected_final_state[9];
         expected_final_state.tail(6) = origin_bias;
         x_origin.tail(6) = origin_bias;
 
@@ -824,47 +805,48 @@ class ConstraintIMU_biasTest_Move_NonNullBiasRot : public testing::Test
         origin_KF = std::static_pointer_cast<FrameIMU>(processor_ptr_imu->setOrigin(x_origin, t));
 
         //===================================================== END{INITIALIZATION}
-
-
         //===================================================== PROCESS DATA
         // PROCESS DATA
 
-        Eigen::Vector6s data_imu;
-        data_imu << 0,0,-wolf::gravity()(2), 0,0,0;
+        Eigen::Vector6s data_imu(Eigen::Vector6s::Zero());
+        Eigen::Vector3s rateOfTurn(Eigen::Vector3s::Zero()); //deg/s
 
-        Scalar input_clock;
+        Scalar dt(0.001);
         TimeStamp ts(0);
         wolf::CaptureIMUPtr imu_ptr = std::make_shared<CaptureIMU>(ts, sen_imu, data_imu);
 
-        while( !imu_data_input.eof())
+        while( ts.get() < 1 )
         {
             // PROCESS IMU DATA
             // Time and data variables
-            imu_data_input >> input_clock >> data_imu[0] >> data_imu[1] >> data_imu[2] >> data_imu[3] >> data_imu[4] >> data_imu[5]; //Ax, Ay, Az, Gx, Gy, Gz
+            ts.set(ts.get() + dt);
+            
+            rateOfTurn = Eigen::Vector3s::Random()*10; //to have rate of turn > 0 deg/s
+            data_imu.tail(3) = rateOfTurn* M_PI/180.0;
+            data_imu.head(3) =  current_quatState.conjugate() * (- wolf::gravity()); //gravity measured, we have no other translation movement
 
-            ts.set(input_clock);
+            //compute current orientaton taking this measure into account
+            current_quatState = current_quatState * wolf::v2q(data_imu.tail(3)*dt);
+
+            //set timestamp, add bias, set data and process
             imu_ptr->setTimeStamp(ts);
+            data_imu = data_imu + origin_bias;
             imu_ptr->setData(data_imu);
-
-            // process data in capture
             sen_imu->process(imu_ptr);
         }
 
+        expected_final_state.head(10) << 0,0,0, current_quatState.x(), current_quatState.y(), current_quatState.z(), current_quatState.w(), 0,0,0;
         last_KF = std::static_pointer_cast<FrameIMU>(wolf_problem_ptr_->getTrajectoryPtr()->closestKeyFrameToTimeStamp(ts));
         last_KF->setState(expected_final_state);
 
-        //closing file
-        imu_data_input.close();
-
-    //===================================================== END{PROCESS DATA}
-    origin_KF->unfix();
-    last_KF->unfix();
+        //===================================================== END{PROCESS DATA}
+        origin_KF->unfix();
+        last_KF->unfix();
     }
 
     virtual void TearDown(){}
 };
 
-// var(b1,b2), inv(p1,q1,v1,p2,q2,v2); fac1: imu(p,q,v)+(b1=b2)
 class ConstraintIMU_ODOM_biasTest_Move_NonNullBiasRot : public testing::Test
 {
     public:
@@ -889,30 +871,6 @@ class ConstraintIMU_ODOM_biasTest_Move_NonNullBiasRot : public testing::Test
         using std::static_pointer_cast;
 
         std::string wolf_root = _WOLF_ROOT_DIR;
-
-        //===================================================== INPUT FILES
-
-        char* imu_filepath;
-        char* odom_filepath;
-        std::string imu_filepath_string(wolf_root + "/src/test/data/IMU/imu_bias_check_Rot.txt");
-        std::string odom_filepath_string(wolf_root + "/src/test/data/IMU/odom_bias_check_Rot.txt");
-
-        imu_filepath   = new char[imu_filepath_string.length() + 1];
-        odom_filepath   = new char[odom_filepath_string.length() + 1];
-        std::strcpy(imu_filepath, imu_filepath_string.c_str());
-        std::strcpy(odom_filepath, odom_filepath_string.c_str());
-        std::ifstream imu_data_input;
-        std::ifstream odom_data_input;
-
-        imu_data_input.open(imu_filepath);
-        odom_data_input.open(odom_filepath);
-        //WOLF_INFO("imu file: ", imu_filepath)
-        if(!imu_data_input.is_open() | !odom_data_input.is_open()){
-            std::cerr << "Failed to open data files... Exiting" << std::endl;
-            ADD_FAILURE();
-        }
-
-        //===================================================== END{INPUT FILES}
         
         //===================================================== SETTING PROBLEM
         // WOLF PROBLEM
@@ -920,7 +878,7 @@ class ConstraintIMU_ODOM_biasTest_Move_NonNullBiasRot : public testing::Test
 
         // CERES WRAPPER
         ceres::Solver::Options ceres_options;
-        ceres_options.minimizer_type = ceres::TRUST_REGION; //ceres::TRUST_REGION;ceres::LINE_SEARCH
+        ceres_options.minimizer_type = ceres::TRUST_REGION;
         ceres_options.max_line_search_step_contraction = 1e-3;
         ceres_options.max_num_iterations = 1e4;
         ceres_manager_wolf_diff = new CeresManager(wolf_problem_ptr_, ceres_options, true);
@@ -944,18 +902,16 @@ class ConstraintIMU_ODOM_biasTest_Move_NonNullBiasRot : public testing::Test
         processor_ptr_odom3D = std::static_pointer_cast<ProcessorOdom3D>(processor_ptr_odom);
     
         //===================================================== END{SETTING PROBLEM}
-
         //===================================================== INITIALIZATION
 
         expected_final_state.resize(16);
         x_origin.resize(16);
         x_origin << 0,0,0, 0,0,0,1, 0,0,0, 0,0,0, 0,0,0;
+        origin_bias << 0.0015, 0.004, -0.002, 0.005, -0.0074, -0.003;
         t.set(0);
+        Eigen::Quaternions odom_quat(Eigen::Quaternions::Identity());
+        Eigen::Quaternions current_quatState(Eigen::Quaternions::Identity());
 
-        imu_data_input >> x_origin[0] >> x_origin[1] >> x_origin[2] >> x_origin[6] >> x_origin[3] >> x_origin[4] >> x_origin[5] >> x_origin[7] >> x_origin[8] >> x_origin[9];
-        imu_data_input >> origin_bias[0] >> origin_bias[1] >> origin_bias[2] >> origin_bias[3] >> origin_bias[4] >> origin_bias[5];
-        imu_data_input >> expected_final_state[0] >> expected_final_state[1] >> expected_final_state[2] >> expected_final_state[6] >> expected_final_state[3] >>
-                    expected_final_state[4] >> expected_final_state[5] >> expected_final_state[7] >> expected_final_state[8] >> expected_final_state[9];
         expected_final_state.tail(6) = origin_bias;
         x_origin.tail(6) = origin_bias;
 
@@ -964,62 +920,64 @@ class ConstraintIMU_ODOM_biasTest_Move_NonNullBiasRot : public testing::Test
         processor_ptr_odom3D->setOrigin(origin_KF);
 
         //===================================================== END{INITIALIZATION}
-
-
         //===================================================== PROCESS DATA
         // PROCESS DATA
 
-        Eigen::Vector6s data_imu, data_odom3D;
-        data_imu << 0,0,-wolf::gravity()(2), 0,0,0;
+        Eigen::Vector6s data_imu(Eigen::Vector6s::Zero()), data_odom3D(Eigen::Vector6s::Zero());
+        Eigen::Vector3s rateOfTurn(Eigen::Vector3s::Zero()); //deg/s
 
-        Scalar input_clock;
+        Scalar dt(0.001), dt_odom(0.1);
         TimeStamp ts(0);
         TimeStamp t_odom(0);
         wolf::CaptureIMUPtr imu_ptr = std::make_shared<CaptureIMU>(ts, sen_imu, data_imu);
         wolf::CaptureMotionPtr mot_ptr = std::make_shared<CaptureMotion>(t, sen_odom3D, data_odom3D, 6, 6);
-    
-        //read first odom data from file
-        odom_data_input >> input_clock >> data_odom3D[0] >> data_odom3D[1] >> data_odom3D[2] >> data_odom3D[3] >> data_odom3D[4] >> data_odom3D[5];
-        t_odom.set(input_clock);
+        
+        //first odometry data will be processed at this timestamp
+        t_odom.set(t_odom.get() + dt_odom);
         //when we find a IMU timestamp corresponding with this odometry timestamp then we process odometry measurement
 
-        while( !imu_data_input.eof() && !odom_data_input.eof() )
+        while( ts.get() < 2 )
         {
             // PROCESS IMU DATA
             // Time and data variables
-            imu_data_input >> input_clock >> data_imu[0] >> data_imu[1] >> data_imu[2] >> data_imu[3] >> data_imu[4] >> data_imu[5]; //Ax, Ay, Az, Gx, Gy, Gz
-            //std::cout << "input_clock : " << input_clock << std::endl;
-            ts.set(input_clock);
-            imu_ptr->setTimeStamp(ts);
-            imu_ptr->setData(data_imu);
+            ts.set(ts.get() + dt);
+            
+            rateOfTurn = Eigen::Vector3s::Random()*10; //to have rate of turn > 0 deg/s
+            data_imu.tail(3) = rateOfTurn* M_PI/180.0;
+            data_imu.head(3) =  current_quatState.conjugate() * (- wolf::gravity()); //gravity measured, we have no other translation movement
 
-            // process data in capture
-            imu_ptr->getTimeStamp();
+            //compute odometry + current orientaton taking this measure into account
+            odom_quat = odom_quat * wolf::v2q(data_imu.tail(3)*dt);
+            current_quatState = current_quatState * wolf::v2q(data_imu.tail(3)*dt);
+
+            //set timestamp, add bias, set data and process
+            imu_ptr->setTimeStamp(ts);
+            data_imu = data_imu + origin_bias;
+            imu_ptr->setData(data_imu);
             sen_imu->process(imu_ptr);
 
             if(ts.get() == t_odom.get()) //every 100 ms
             {
                 // PROCESS ODOM 3D DATA
+                data_odom3D.head(3) << 0,0,0;
+                data_odom3D.tail(6) = q2v(odom_quat);
                 mot_ptr->setTimeStamp(t_odom);
                 mot_ptr->setData(data_odom3D);
                 sen_odom3D->process(mot_ptr);
 
-                //prepare next odometry measurement if there is any
-                odom_data_input >> input_clock >> data_odom3D[0] >> data_odom3D[1] >> data_odom3D[2] >> data_odom3D[3] >> data_odom3D[4] >> data_odom3D[5];
-                t_odom.set(input_clock);
+                //prepare next odometry measurement
+                odom_quat = Eigen::Quaternions::Identity(); //set to identity to have next odom relative to this last KF
+                t_odom.set(t_odom.get() + dt_odom);
             }
         }
 
+        expected_final_state.head(10) << 0,0,0, current_quatState.x(), current_quatState.y(), current_quatState.z(), current_quatState.w(), 0,0,0;
         last_KF = std::static_pointer_cast<FrameIMU>(wolf_problem_ptr_->getTrajectoryPtr()->closestKeyFrameToTimeStamp(ts));
         last_KF->setState(expected_final_state);
 
-        //closing file
-        imu_data_input.close();
-        odom_data_input.close();
-
-    //===================================================== END{PROCESS DATA}
-    origin_KF->unfix();
-    last_KF->unfix();
+        //===================================================== END{PROCESS DATA}
+        origin_KF->unfix();
+        last_KF->unfix();
     }
 
     virtual void TearDown(){}
@@ -3598,6 +3556,6 @@ TEST_F(ConstraintIMU_ODOM_biasTest_Move_BiasedNoisyComplex_initOK, varB1P2Q2V2B2
 int main(int argc, char **argv)
 {
   testing::InitGoogleTest(&argc, argv);
-  ::testing::GTEST_FLAG(filter) = "ConstraintIMU_ODOM_biasTest_Move_NonNullBiasComplex.*";
+  ::testing::GTEST_FLAG(filter) = "ConstraintIMU_biasTest_Move_NonNullBiasRot.*";
   return RUN_ALL_TESTS();
 }
