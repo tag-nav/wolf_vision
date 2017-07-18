c3f09f9b28ed18966bf4436e33b13d41840e0513
Dinesh Atchuthan <datchuth@laas.fr>
2017-07-18 10:55:44 +0200

    added coding steps

--- src/examples/test_imuDock.cpp
+++ src/examples/test_imuDock.cpp
@@ -13,7 +13,11 @@
 #include "sensor_odom_3D.h"
 #include "processor_odom_3D.h"
 
-/*
+//std
+#include <iostream>
+#include <fstream>
+
+/*                              OFFLINE VERSION
     In this test, we use the experimental conditions needed for Humanoids 2017.
     IMU data are acquired using the docking station. 
 
@@ -23,12 +27,58 @@
     constraints : Odometry constraint between KeyFrames
                   IMU constraint
                   FixBias constraint --> makes the problem observable (adding a big covariance on all part but a smaller one on yaw)
-                  Fic3D constraint
+                  Fix3D constraint
 
+    What we expect  : Estimate biases (this will strongly depend on the actual trajectory of the IMU)
+                      Estimate the position and orienttion of the IMU (check with the standard deviation using covariance matrix)
 */
 int main(int argc, char** argv)
 {
     using namespace wolf;
 
+    //we expect 1 file giving imu measurements
+    std::ifstream imu_data_input;
+    const char * filename_imu;
+    if (argc < 02)embed
+    {
+        WOLF_ERROR("Missing 1 imput argument (path to imu data file).")
+        return 1; //return with error
+    }
+    else
+    {
+        filename_imu = argv[1];
 
+        imu_data_input.open(filename_imu);
+        WOLF_INFO("imu file : ", filename_imu)
+    }
+
+    // Check if the file is correctly opened
+
+    //#################################################### SETTING PROBLEM
+    std::string wolf_root = _WOLF_ROOT_DIR;
+
+    // Create the WOLF Problem + set origin of the problem
+
+    // Configure Ceres
+
+    // Create sensors + processors
+
+    //
+
+    //#################################################### PROCESS DATA
+    // process IMU and odometry
+
+    //#################################################### OPTIMIZATION PART
+    // Create needed constraints
+
+    // Fix/Unfix stateblocks
+
+    // Solve + compute covariances
+
+    // Get standard deviation from covariances
+
+    // Check results
+        // Define expected values
+
+        // Are expected values in the range of estimated +/- 2*stdev ?
 }
