7c2502f1f81ec3228503b2f7482dc15fc9ffbfb9
Joan Solà <jsola@iri.upc.edu>
2017-08-02 08:58:08 +0200

    Add constructor with dynamic extrinsics and intrinsics

--- src/capture_imu.cpp
+++ src/capture_imu.cpp
@@ -1,12 +1,15 @@
 #include "capture_imu.h"
 #include "sensor_imu.h"
+#include "state_quaternion.h"
 
 namespace wolf {
 
 
-CaptureIMU::CaptureIMU(const TimeStamp& _init_ts, SensorBasePtr _sensor_ptr, const Eigen::Vector6s& _acc_gyro_data, 
-                        FrameBasePtr _origin_frame_ptr) :
-        CaptureMotion(_init_ts, _sensor_ptr, _acc_gyro_data, 6, 10, 9, 6, _origin_frame_ptr)
+CaptureIMU::CaptureIMU(const TimeStamp& _init_ts,
+                       SensorBasePtr _sensor_ptr,
+                       const Eigen::Vector6s& _acc_gyro_data,
+                       FrameBasePtr _origin_frame_ptr) :
+                CaptureMotion(_init_ts, _sensor_ptr, _acc_gyro_data, 6, 10, 9, 6, _origin_frame_ptr)
 {
     setType("IMU");
 
@@ -26,10 +29,37 @@ CaptureIMU::CaptureIMU(const TimeStamp& _init_ts, SensorBasePtr _sensor_ptr, con
 //    std::cout << "constructed    +C-IMU" << id() << std::endl;
 }
 
-CaptureIMU::CaptureIMU(const TimeStamp& _init_ts, SensorBasePtr _sensor_ptr, const Eigen::Vector6s& _acc_gyro_data, 
-                       const Eigen::MatrixXs& _data_cov, FrameBasePtr _origin_frame_ptr) :
-        CaptureMotion(_init_ts, _sensor_ptr, _acc_gyro_data, _data_cov, 6, 10, 9, 6, _origin_frame_ptr)
+CaptureIMU::CaptureIMU(const TimeStamp& _init_ts,
+                       SensorBasePtr _sensor_ptr,
+                       const Eigen::Vector6s& _acc_gyro_data,
+                       const Eigen::MatrixXs& _data_cov,
+                       FrameBasePtr _origin_frame_ptr) :
+                CaptureMotion(_init_ts, _sensor_ptr, _acc_gyro_data, _data_cov, 6, 10, 9, 6, _origin_frame_ptr)
 {
+    setType("IMU");
+//    std::cout << "constructed    +C-IMU" << id() << std::endl;
+}
+
+
+
+CaptureIMU::CaptureIMU(const TimeStamp& _init_ts,
+                       SensorBasePtr _sensor_ptr,
+                       const Eigen::Vector6s& _acc_gyro_data,
+                       const Eigen::MatrixXs& _data_cov,
+                       const VectorXs& _extrinsics,
+                       const Vector6s& _bias,
+                       FrameBasePtr _origin_frame_ptr) :
+                CaptureMotion(_init_ts, _sensor_ptr, _acc_gyro_data, _data_cov, 6, 10, 9, 6, _origin_frame_ptr)
+{
+    if (!getSensorPtr() || getSensorPtr()->isExtrinsicDynamic())
+    {
+        setStateBlockPtr(0, std::make_shared<StateBlock>     (_extrinsics.head<3>(), false));
+        setStateBlockPtr(1, std::make_shared<StateQuaternion>(_extrinsics.tail<4>(), false));
+    }
+    if (!getSensorPtr() || getSensorPtr()->isIntrinsicDynamic())
+    {
+        setStateBlockPtr(2, std::make_shared<StateBlock>(_bias, false));
+    }
     setType("IMU");
 //    std::cout << "constructed    +C-IMU" << id() << std::endl;
 }
