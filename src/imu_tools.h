ea6e59acd4848cb205b6543b4b637c344d33721a
Joan Solà <jsola@iri.upc.edu>
2017-08-05 15:10:49 +0200

    Fix imu_tools plus() and diff() ...
    Some MatrixSizeCheck are still missing
    in plus() and diff()

--- src/imu_tools.h
+++ src/imu_tools.h
@@ -150,8 +150,8 @@ inline void compose(const MatrixBase<D1>& d1,
     MatrixSizeCheck<10, 1>::check(d1);
     MatrixSizeCheck<10, 1>::check(d2);
     MatrixSizeCheck<10, 1>::check(sum);
-    MatrixSizeCheck< 9, 1>::check(J_sum_d1);
-    MatrixSizeCheck< 9, 1>::check(J_sum_d2);
+    MatrixSizeCheck< 9, 9>::check(J_sum_d1);
+    MatrixSizeCheck< 9, 9>::check(J_sum_d2);
 
     // Maps over provided data
     Map<const Matrix<typename D1::Scalar, 3, 1> >   dp1    ( & d1( 0 ) );
@@ -364,19 +364,79 @@ Matrix<typename Derived::Scalar, 10, 1> retract(const MatrixBase<Derived>& d_in)
     return ret;
 }
 
+template<typename D1, typename D2, typename D3, typename D4, typename D5, typename D6, typename D7, typename D8, typename D9>
+inline void plus(const MatrixBase<D1>& dp1, const QuaternionBase<D2>& dq1, const MatrixBase<D3>& dv1,
+                 const MatrixBase<D4>& dp2, const MatrixBase<D5>& do2, const MatrixBase<D6>& dv2,
+                 MatrixBase<D7>& plus_p, QuaternionBase<D8>& plus_q, MatrixBase<D9>& plus_v )
+{
+        plus_p = dp1 + dp2;
+        plus_q = dq1 * exp_q(do2);
+        plus_v = dv1 + dv2;
+}
+
+template<typename D1, typename D2, typename D3>
+inline void plus(const MatrixBase<D1>& d1,
+                 const MatrixBase<D2>& d2,
+                 MatrixBase<D3>& d_pert)
+{
+    Map<const Matrix<typename D1::Scalar, 3, 1> >   dp1    ( & d1(0) );
+    Map<const Quaternion<typename D1::Scalar> >     dq1    ( & d1(3) );
+    Map<const Matrix<typename D1::Scalar, 3, 1> >   dv1    ( & d1(7) );
+    Map<const Matrix<typename D2::Scalar, 3, 1> >   dp2    ( & d2(0) );
+    Map<const Matrix<typename D2::Scalar, 3, 1> >   do2    ( & d2(3) );
+    Map<const Matrix<typename D2::Scalar, 3, 1> >   dv2    ( & d2(6) );
+    Map<Matrix<typename D3::Scalar, 3, 1> >         dp_p ( & d_pert(0) );
+    Map<Quaternion<typename D3::Scalar> >           dq_p ( & d_pert(3) );
+    Map<Matrix<typename D3::Scalar, 3, 1> >         dv_p ( & d_pert(7) );
+
+    plus(dp1, dq1, dv1, dp2, do2, dv2, dp_p, dq_p, dv_p);
+}
+
+template<typename D1, typename D2>
+inline Matrix<typename D1::Scalar, 10, 1> plus(const MatrixBase<D1>& d1,
+                                               const MatrixBase<D2>& d2)
+{
+    Matrix<typename D1::Scalar, 10, 1> ret;
+    plus(d1, d2, ret);
+    return ret;
+}
+
+template<typename D1, typename D2, typename D3, typename D4, typename D5, typename D6, typename D7, typename D8, typename D9>
+inline void diff(const MatrixBase<D1>& dp1, const QuaternionBase<D2>& dq1, const MatrixBase<D3>& dv1,
+                 const MatrixBase<D4>& dp2, const QuaternionBase<D5>& dq2, const MatrixBase<D6>& dv2,
+                 MatrixBase<D7>& diff_p, MatrixBase<D8>& diff_o, MatrixBase<D9>& diff_v )
+{
+        diff_p = dp2 - dp1;
+        diff_o = log_q(dq1.conjugate() * dq2);
+        diff_v = dv2 - dv1;
+}
+
+
 template<typename D1, typename D2, typename D3>
 inline void diff(const MatrixBase<D1>& d1,
                  const MatrixBase<D2>& d2,
                  MatrixBase<D3>& err)
 {
-    err = lift( between(d1, d2, 0.0) );
+    Map<const Matrix<typename D1::Scalar, 3, 1> >   dp1    ( & d1(0) );
+    Map<const Quaternion<typename D1::Scalar> >     dq1    ( & d1(3) );
+    Map<const Matrix<typename D1::Scalar, 3, 1> >   dv1    ( & d1(7) );
+    Map<const Matrix<typename D2::Scalar, 3, 1> >   dp2    ( & d2(0) );
+    Map<const Quaternion<typename D2::Scalar> >     dq2    ( & d2(3) );
+    Map<const Matrix<typename D2::Scalar, 3, 1> >   dv2    ( & d2(7) );
+    Map<Matrix<typename D3::Scalar, 3, 1> >         diff_p ( & err(0) );
+    Map<Matrix<typename D3::Scalar, 3, 1> >         diff_o ( & err(3) );
+    Map<Matrix<typename D3::Scalar, 3, 1> >         diff_v ( & err(6) );
+
+    diff(dp1, dq1, dv1, dp2, dq2, dv2, diff_p, diff_o, diff_v);
 }
 
 template<typename D1, typename D2>
 inline Matrix<typename D1::Scalar, 9, 1> diff(const MatrixBase<D1>& d1,
                                               const MatrixBase<D2>& d2)
 {
-    return lift( between(d1, d2, 0.0) );
+    Matrix<typename D1::Scalar, 9, 1> ret;
+    diff(d1, d2, ret);
+    return ret;
 }
 
 
