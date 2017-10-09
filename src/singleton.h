b0caf3fb4eea553d0767aaea79c221be63f59d1b
Jeremie Deray <jeremie.deray@pal-robotics.com>
2017-10-09 18:43:50 +0200

    enhance logging

--- src/singleton.h
+++ src/singleton.h
@@ -14,49 +14,34 @@ namespace wolf {
 namespace internal {
 
 /**
- * \brief A thread-safer Singleton implementation with
+ * \brief A thread-safer (?) Singleton implementation with
  * argument forwarding.
  **/
 template <class T>
 class Singleton
 {
-  /**
-   * \brief Custom deleter to by-pass private destructor issue.
-   **/
-  struct Deleter;
-
-  using SingletonOPtr = std::unique_ptr<T, Deleter>;
+  using SingletonOPtr = std::unique_ptr<T>;
 
 public:
 
   template <typename... Args>
   static T& get(Args&&... args)
   {
-    static SingletonOPtr instance_(new T(args...));
+    static SingletonOPtr instance_(new T(std::forward<Args>(args)...));
 
     return *instance_;
   }
 
-  constexpr Singleton(const Singleton&)       = delete;
-  //constexpr Singleton(const Singleton&&)      = delete;
+  constexpr Singleton(const Singleton&)  = delete;
+  constexpr Singleton(const Singleton&&) = delete;
 
   constexpr Singleton& operator=(const Singleton&) const = delete;
-  //constexpr Singleton& operator=(const Singleton&&) = delete;
+  constexpr Singleton& operator=(const Singleton&&) = delete;
 
 protected:
 
-  Singleton() = default;
-
-  virtual ~Singleton() = default;
-};
-
-template <class T>
-struct Singleton<T>::Deleter
-{
-  void operator()( const T* const p )
-  {
-    delete p;
-  }
+  constexpr Singleton() = default;
+  virtual ~Singleton()  = default;
 };
 
 } // namespace internal
