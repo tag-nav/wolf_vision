065060fd9f108c4541e5df774be216a627680341
Jeremie Deray <jeremie.deray@pal-robotics.com>
2017-10-27 16:57:51 +0200

    add ConstraintDiffDrive

--- src/wolf.h
+++ src/wolf.h
@@ -226,7 +226,8 @@ typedef enum
     CTR_EPIPOLAR,               ///< Epipolar constraint
     CTR_AHP,                    ///< Anchored Homogeneous Point constraint
     CTR_AHP_NL,                 ///< Anchored Homogeneous Point constraint (temporal, to be removed)
-    CTR_IMU                     ///< IMU constraint
+    CTR_IMU,                    ///< IMU constraint
+    CTR_DIFF_DRIVE              ///< Diff-drive constraint
 
 } ConstraintType;
 
