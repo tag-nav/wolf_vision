dc128dd05fc6dd327d8a648bbb8a3d710a4f3cef
Joan Solà <jsola@iri.upc.edu>
2017-12-01 21:58:39 +0100

    Start application demo hello_wolf.cpp

---
+++ src/hello_wolf/hello_wolf.cpp
@@ -0,0 +1,22 @@
+/**
+ * \file hello_wolf.cpp
+ *
+ *  Created on: Dec 1, 2017
+ *      \author: jsola
+ */
+
+
+#include "wolf.h"
+#include "sensor_range_bearing.h"
+#include "processor_range_bearing.h"
+#include "capture_range_bearing.h"
+#include "feature_range_bearing.h"
+#include "constraint_range_bearing.h"
+//#include "landmark_point_2D.h" // TODO this class does not exist yet
+
+int main()
+{
+
+
+    return 0;
+}
