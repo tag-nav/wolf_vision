1d3744e1abf919aa26169c7a1830c7854ecb486f
Joan Solà <jsola@iri.upc.edu>
2017-12-12 11:17:22 +0100

    Add more test cases

--- src/test/gtest_processor_motion.cpp
+++ src/test/gtest_processor_motion.cpp
@@ -19,7 +19,7 @@ using namespace Eigen;
 using namespace wolf;
 using std::static_pointer_cast;
 
-class ProcessorMotion_test : public testing::Test{
+class ProcessorMotion_test : public ProcessorOdom2D, public testing::Test{
     public:
         Scalar              dt;
         ProblemPtr          problem;
@@ -31,7 +31,7 @@ class ProcessorMotion_test : public testing::Test{
 
         virtual void SetUp()
         {
-            dt = 0.1;
+            dt                          = 1.0;
             problem = Problem::create("PO 2D");
             ProcessorParamsOdom2DPtr params(std::make_shared<ProcessorParamsOdom2D>());
             params->dist_traveled_th_   = 100;
@@ -49,16 +49,31 @@ class ProcessorMotion_test : public testing::Test{
 
         virtual void TearDown(){}
 
+        virtual Motion interpolate(const Motion& _ref,
+                                   Motion& _second,
+                                   TimeStamp& _ts)
+        {
+            return ProcessorMotion::interpolate(_ref, _second, _ts);
+        }
+
+        virtual Motion motionZero(TimeStamp& t)
+        {
+            return ProcessorOdom2D::motionZero(t);
+        }
+
+
 };
 
 
-TEST_F(ProcessorMotion_test, IntegrateDummy)
+TEST_F(ProcessorMotion_test, IntegrateStraight)
 {
     data << 1, 0; // advance straight
     data_cov.setIdentity();
+    TimeStamp t(0.0);
 
-    for (TimeStamp t = dt; t<0.9; t = t + dt)
+    for (int i = 0; i<9; i++)
     {
+        t += dt;
         capture->setTimeStamp(t);
         capture->setData(data);
         capture->setDataCovariance(data_cov);
@@ -67,6 +82,63 @@ TEST_F(ProcessorMotion_test, IntegrateDummy)
     }
 
     ASSERT_MATRIX_APPROX(problem->getCurrentState(), (Vector3s()<<9,0,0).finished(), 1e-8);
+}
+
+TEST_F(ProcessorMotion_test, IntegrateCircle)
+{
+    data << 1, 2*M_PI/10; // advance in circle
+    data_cov.setIdentity();
+    TimeStamp t(0.0);
+
+    for (int i = 0; i<10; i++) // one full turn
+    {
+        t += dt;
+        capture->setTimeStamp(t);
+        capture->setData(data);
+        capture->setDataCovariance(data_cov);
+        processor->process(capture);
+        WOLF_DEBUG("t: ", t, "  x: ", problem->getCurrentState().transpose());
+    }
+
+    ASSERT_MATRIX_APPROX(problem->getCurrentState(), (Vector3s()<<0,0,0).finished(), 1e-8);
+}
+
+TEST_F(ProcessorMotion_test, Interpolate)
+{
+    data << 1, 2*M_PI/10; // advance straight
+    data_cov.setIdentity();
+    TimeStamp t(0.0);
+    std::vector<Motion> motions;
+    motions.push_back(motionZero(t));
+
+    for (int i = 0; i<10; i++)
+    {
+        t += dt;
+        capture->setTimeStamp(t);
+        capture->setData(data);
+        capture->setDataCovariance(data_cov);
+        processor->process(capture);
+        motions.push_back(processor->getMotion(t));
+        WOLF_DEBUG("t: ", t, "  x: ", problem->getCurrentState().transpose());
+    }
+
+    TimeStamp tt    = 2.2;
+    Motion ref      = motions[2];
+    Motion second   = motions[3];
+    Motion interp   = interpolate(ref, second, tt);
+
+    ASSERT_NEAR(         interp.ts_.get()       , 2.2                       , 1e-8);
+    ASSERT_MATRIX_APPROX(interp.data_           , VectorXs::Zero(2)         , 1e-8);
+    ASSERT_MATRIX_APPROX(interp.delta_          , VectorXs::Zero(3)         , 1e-8);
+    ASSERT_MATRIX_APPROX(interp.delta_integr_   , motions[2].delta_integr_  , 1e-8);
+
+    tt      = 2.6;
+    interp  = interpolate(ref, second, tt);
+
+    ASSERT_NEAR(         interp.ts_.get()       , 2.6                       , 1e-8);
+    ASSERT_MATRIX_APPROX(interp.data_           , motions[3].data_          , 1e-8);
+    ASSERT_MATRIX_APPROX(interp.delta_          , motions[3].delta_         , 1e-8);
+    ASSERT_MATRIX_APPROX(interp.delta_integr_   , motions[3].delta_integr_  , 1e-8);
 }
 
 int main(int argc, char **argv)
