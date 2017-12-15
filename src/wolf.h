5cd24c494e62a3b50146d92819542e5e2d1154dd
Dinesh Atchuthan <datchuth@laas.fr>
2017-12-15 22:59:43 +0100

    remove old constraint typedefs from wolf.h

--- src/wolf.h
+++ src/wolf.h
@@ -228,10 +228,7 @@ typedef enum
     CTR_AHP_NL,                 ///< Anchored Homogeneous Point constraint (temporal, to be removed)
     CTR_IMU,                    ///< IMU constraint
     CTR_DIFF_DRIVE,             ///< Diff-drive constraint
-    CTR_BEARING_2D,             ///< 2D bearing
-    CTR_ABS_P,                  ///< absolute position constraint (for priors) 
-    CTR_ABS_O,                  ///< absolute orientation constraint (for priors) 
-    CTR_ABS_V,                  ///< absolute velocity constraint (for priors) 
+    CTR_BEARING_2D,             ///< 2D bearing 
     CTR_BLOCK_ABS               ///< absolute constraint to Poisition or Velocity depending on argument StateBlockPtr (for priors) 
 } ConstraintType;
 
