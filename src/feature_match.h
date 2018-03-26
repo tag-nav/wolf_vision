4a41858c6525a70a668a4790ec72e0170822b8cf
Joan Sola <jsola@iri.upc.edu>
2018-03-26 16:05:40 +0200

    Documentation

--- src/feature_match.h
+++ src/feature_match.h
@@ -9,17 +9,25 @@ namespace wolf {
     
 //forward declaration to typedef class pointers
 WOLF_STRUCT_PTR_TYPEDEFS(FeatureMatch);
+
+/** \brief Map of feature matches
+ *
+ *   corr feature  <--------------  actual feature
+ *
+ * map<FeatureBasePtr actual_feature, FeatureMatchPtr corresponding_feature_match>
+ *
+ */
 typedef std::map<FeatureBasePtr, FeatureMatchPtr> FeatureMatchMap; //a map is also typedefined  
     
 /** \brief Match between a feature and a feature
  *
  * Match between a feature and a feature (feature-feature correspondence)
  *
- **/
+ */
 struct FeatureMatch
 {
-        FeatureBasePtr feature_ptr_;
-        Scalar normalized_score_;
+        FeatureBasePtr feature_ptr_; ///< Corresponding feature
+        Scalar normalized_score_;    ///< normalized similarity score (0 is bad, 1 is good)
 };
 
 }//end namespace
