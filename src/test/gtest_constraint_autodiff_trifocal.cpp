903e81b56b705ca8a96113271babc48e73bbfba8
Joan Sola <jsola@iri.upc.edu>
2018-04-03 13:02:02 +0200

    WIP gtest for trifocal constraint

--- src/test/gtest_constraint_autodiff_trifocal.cpp
+++ src/test/gtest_constraint_autodiff_trifocal.cpp
@@ -9,20 +9,19 @@ using namespace Eigen;
 using namespace wolf;
 using std::static_pointer_cast;
 
-// Use the following in case you want to initialize tests with predefines variables or methods.
-//class ConstraintAutodiffTrifocal_class : public testing::Test{
-//    public:
-//        virtual void SetUp()
-//        {
-//        }
-//};
+class ConstraintAutodiffTrifocalTest : public testing::Test{
+    public:
+        virtual void SetUp()
+        {
+        }
+};
 
-TEST(ConstraintAutodiffTrifocal, Constructor)
+TEST_F(ConstraintAutodiffTrifocalTest, Constructor)
 {
   std::cout << "\033[1;33m [WARN]:\033[0m gtest for ConstraintAutodiffTrifocal Constructor is empty." << std::endl;
 }
 
-TEST(ConstraintAutodiffTrifocal, Destructor)
+TEST_F(ConstraintAutodiffTrifocalTest, Destructor)
 {
   std::cout << "\033[1;33m [WARN]:\033[0m gtest for ConstraintAutodiffTrifocal Destructor is empty." << std::endl;
 }
