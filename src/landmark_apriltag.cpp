19327a2cf74a470466a2ef528e346d1e5df65aa8
Dinesh Atchuthan <a.dinesh@hotmail.fr>
2018-12-03 17:40:53 +0100

    created landmark_apriltag

---
+++ src/landmark_apriltag.cpp
@@ -0,0 +1,28 @@
+
+#include "landmark_apriltag.h"
+
+namespace wolf {
+
+LandmarkApriltag::LandmarkApriltag(StateBlockPtr _p_ptr, StateBlockPtr _o_ptr,  const Scalar& _tagid,  const Scalar& _tag_width) :
+	LandmarkBase("APRILTAG", _p_ptr, _o_ptr), tag_width_(_tag_width)
+{
+  	setDescriptor(Eigen::VectorXs::Constant(1,_tagid)); //change tagid to int ? do not use descriptor vector ?
+}
+
+LandmarkApriltag::~LandmarkApriltag()
+{
+    //
+}
+
+
+Scalar LandmarkApriltag::getTagWidth() const
+{
+    return tag_width_;
+}
+
+Scalar LandmarkApriltag::getTagId() const
+{
+    return descriptor_(0);
+}
+
+} // namespace wolf
---
+++ src/landmark_apriltag.h
@@ -0,0 +1,53 @@
+
+#ifndef LANDMARK_APRILTAG_H_
+#define LANDMARK_APRILTAG_H_
+
+//Wolf includes
+#include "landmark_base.h"
+
+// Std includes
+
+
+namespace wolf {
+
+WOLF_PTR_TYPEDEFS(LandmarkApriltag);
+    
+//class LandmarkApriltag
+class LandmarkApriltag : public LandmarkBase
+{
+    public:
+        /** \brief Constructor with type, time stamp and the position state pointer
+         *
+         * Constructor with type, and state pointer
+         * \param _p_ptr StateBlock shared pointer to the position
+         * \param _o_ptr StateBlock shared pointer to the orientation
+         * \param _tagid descriptor of the landmark: id of the tag
+         * \param _tag_width : total width of the tag (2 by default since tags are of 2 units)
+         *
+         **/
+		LandmarkApriltag(StateBlockPtr _p_ptr, StateBlockPtr _o_ptr, const Scalar& _tagid,  const Scalar& _tag_width=2);
+
+        virtual ~LandmarkApriltag();
+        
+        /** \brief Returns tag id
+         * 
+         * Returns id of the tag
+         * 
+         **/
+        Scalar getTagId() const;
+        
+        /** \brief Returns tag width
+         * 
+         * Returns width of the tag
+         * 
+         **/
+        Scalar getTagWidth() const;
+
+    private:
+        const Scalar tag_width_;         
+        
+};
+
+} // namespace wolf
+
+#endif
