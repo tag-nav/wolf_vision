c5d123f2ede447303b93ab3c0cc7e6e8265364d3
Dinesh Atchuthan <a.dinesh@hotmail.fr>
2018-12-11 15:22:22 +0100

    contructor unit test passing

--- src/test/gtest_constraint_autodiff_apriltag.cpp
+++ src/test/gtest_constraint_autodiff_apriltag.cpp
@@ -55,7 +55,7 @@ WOLF_REGISTER_PROCESSOR("TRACKER LANDMARK APRILTAG WRAPPER", ProcessorTrackerLan
 ////////////////////////////////////////////////////////////////
 
 // Use the following in case you want to initialize tests with predefines variables or methods.
-/*class ConstraintAutodiffApriltag_class : public testing::Test{
+class ConstraintAutodiffApriltag_class : public testing::Test{
     public:
         Vector3s    pos_camera,   pos_robot,   pos_landmark;
         Vector3s    euler_camera, euler_robot, euler_landmark;
@@ -67,12 +67,13 @@ WOLF_REGISTER_PROCESSOR("TRACKER LANDMARK APRILTAG WRAPPER", ProcessorTrackerLan
         CeresManagerPtr ceres_manager;
 
         SensorCameraPtr camera;
-        ProcessorTrackerLandmarkApriltagPtr proc_apriltag;
+        ProcessorTrackerLandmarkApriltag_WrapperPtr proc_apriltag;
 
         SensorBasePtr   S;
         FrameBasePtr    F1;
         CaptureImagePtr I1;
         FeatureApriltagPtr  f1;
+        LandmarkApriltagPtr lmk1;
         ConstraintAutodiffApriltagPtr c_tag;
 
         virtual void SetUp()
@@ -81,7 +82,7 @@ WOLF_REGISTER_PROCESSOR("TRACKER LANDMARK APRILTAG WRAPPER", ProcessorTrackerLan
 
             // configuration
 
-             * We have three poses to take into account:
+             /* We have three poses to take into account:
              *  - pose of the camera (extrinsincs)
              *  - pose of the landmark
              *  - pose of the robot (Keyframe)
@@ -109,7 +110,7 @@ WOLF_REGISTER_PROCESSOR("TRACKER LANDMARK APRILTAG WRAPPER", ProcessorTrackerLan
              * The frame pose F1, and the camera pose S
              * in the robot frame are variables subject to optimization
              *
-             * We perform a number of tests based on this configuration.
+             * We perform a number of tests based on this configuration.*/
 
 
             // camera is at origin of robot and looking forward
@@ -140,51 +141,52 @@ WOLF_REGISTER_PROCESSOR("TRACKER LANDMARK APRILTAG WRAPPER", ProcessorTrackerLan
             S      = problem->installSensor("CAMERA", "canonical", pose_camera, wolf_root + "/src/examples/camera_params_canonical.yaml");
             camera = std::static_pointer_cast<SensorCamera>(S);
 
-            ProcessorParamsTrackerLandmarkApriltagPtr params_tracker_landmark_apriltag = std::make_shared<ProcessorParamsTrackerLandmarkApriltag>();
-             Need to set some parameters ? do it now !
-            params_tracker_landmark_apriltag->                = ;
+            ProcessorParamsTrackerLandmarkApriltagPtr params = std::make_shared<ProcessorParamsTrackerLandmarkApriltag>();
+            // Need to set some parameters ? do it now !
+            params->tag_family_ = "tag36h11";
+            //params->name        = params->tag_family_;
 
+            ProcessorBasePtr proc = problem->installProcessor("TRACKER LANDMARK APRILTAG WRAPPER", "apriltags_wrapper", camera, params);
+            proc_apriltag = std::static_pointer_cast<ProcessorTrackerLandmarkApriltag_Wrapper>(proc);
 
-            ProcessorBasePtr proc = problem->installProcessor("TRACKER LANDMARK APRILTAG", "apriltag", camera, params_tracker_landmark_apriltag);
-            proc_apriltag = std::static_pointer_cast<ProcessorTrackerLandmarkApriltag>(proc);
+            // F1 is be origin KF
+            F1 = problem->setPrior(pose_robot, Matrix6s::Identity(), 0.0, 0.1);
 
             //create feature and landmark
-            // F1 should be origin KF
-            F1 = proc_apriltag->getOriginPtr()->getFramePtr();
             I1 = std::make_shared<CaptureImage>(1.0, camera, cv::Mat(2,2,CV_8UC1));
             F1-> addCapture(I1);
+            proc_apriltag->setOriginPtr(I1);
+            proc_apriltag->setLastPtr(I1);
+
             // the sensor is at origin as well as the robot. The measurement matches with the pose of the tag wrt camera (and also wrt robot and world)
-            // FeatureApriltag(Vector7s & _measurement,Matrix6s & _meas_covariance, apriltag_detection_t & _det
+            // FeatureApriltag(Vector7s & _measurement,Matrix6s & _meas_covariance, const int _tag_id)
             Eigen::Matrix6s meas_cov(Eigen::Matrix6s::Identity());
             meas_cov.topLeftCorner(3,3)     *= 1e-2;
             meas_cov.bottomRightCorner(3,3) *= 1e-3;
+            int tag_id = 1;
 
-            //we also need a detection object. This object is defined in Apriltag library...
-            //maybe we can just delete the detection obkect from FeatureApriltag class !
+            f1 = std::make_shared<FeatureApriltag>(pose_landmark, meas_cov, tag_id);
+            lmk1 = std::static_pointer_cast<LandmarkApriltag>(proc_apriltag->createLandmark(f1));
 
-            //apriltag_detection_t detection;
-            //detection.id        = 1;
-            //detection.hamming   = 1;
-            //detection.goodness  = 1.0;
-            //detection.decision_margin = 1.0;
-
-            //define the homography matrix...
-            matd_t R = *matd_create(3,3);
-            MATD_EL(&R, 0, 0) = c;
-            MATD_EL(&R, 0, 1) = -s;
-            MATD_EL(&R, 1, 0) = s;
-            MATD_EL(&R, 1, 1) = c;
-            MATD_EL(&R, 2, 2) = 1;
-            detection.H = R;
-
-            // f1 = std::make_shared<FeatureApriltag>(pose_landmark, meas_cov, det);
+            // Add the feature and the landmark in the graph as needed
+            I1->addFeature(f1); // add feature to capture
+            problem->addLandmark(lmk1); // add landmark to map
         }
-};*/
+};
 
 
-TEST(ConstraintAutodiffApriltag, Constructor)
+TEST_F(ConstraintAutodiffApriltag_class, Constructor)
 {
-    std::cout << "\033[1;33m [WARN]:\033[0m gtest for ConstraintAutodiffApriltag Constructor is empty." << std::endl;
+    ConstraintAutodiffApriltagPtr constraint = std::make_shared<ConstraintAutodiffApriltag>(
+            S,
+            F1,
+            lmk1,
+            f1,
+            false,
+            CTR_ACTIVE
+    );
+
+    ASSERT_TRUE(constraint->getType() == "AUTODIFF APRILTAG");
 }
 
 TEST(ConstraintAutodiffApriltag, Destructor)
