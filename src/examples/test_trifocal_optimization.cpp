285a3c2a31912465abbf418dd5041049e4b43d0b
asantamaria <asantamaria@iri.upc.edu>
2018-12-12 16:16:54 +0100

    WIp test

--- src/examples/test_trifocal_optimization.cpp
+++ src/examples/test_trifocal_optimization.cpp
@@ -40,8 +40,8 @@ int main(int argc, char** argv)
     // TEST IMAGES ===================================
     // ===============================================
 
-    // x,y cm displacement, negatives values are diretly added in the path string (row-wise).
-    Eigen::MatrixXi img_pos = Eigen::MatrixXi::Zero(9,2);
+    // x,y cm displacement, negatives values are directly added in the path string (row-wise).
+    Eigen::MatrixXs img_pos = Eigen::MatrixXs::Zero(9,2);
     img_pos.row(0) << 0,0;
     img_pos.row(1) << 1,0;
     img_pos.row(2) << 2,0;
@@ -57,7 +57,7 @@ int main(int argc, char** argv)
     std::vector<cv::Mat> images;
     for (unsigned int ii = 0; ii < img_pos.rows(); ++ii)
     {
-        std::string img_path = wolf_root + "/src/examples/Test_gazebo_x-" + std::to_string(img_pos(ii,0)) + "0cm_y-" + std::to_string(img_pos(ii,1)) + "0cm.png";
+        std::string img_path = wolf_root + "/src/examples/Test_gazebo_x-" + std::to_string((int)img_pos(ii,0)) + "0cm_y-" + std::to_string((int)img_pos(ii,1)) + "0cm.png";
         std::cout << " |->" << img_path << std::endl;
         images.push_back(cv::imread(img_path, CV_LOAD_IMAGE_GRAYSCALE));   // Read the file
         if(! images.at(ii).data )                              // Check for invalid input
@@ -68,8 +68,8 @@ int main(int argc, char** argv)
     }
     std::cout << std::endl;
 
-    cv::imshow( "DEBUG VIEW", images.at(0) );                   // Show our image inside it.
-    cv::waitKey(0);                                      // Wait for a keystroke in the window
+    cv::imshow( "DEBUG VIEW", images.at(0) );  // Show our image inside it.
+    cv::waitKey(0);                            // Wait for a keystroke in the window
 
     // ===============================================
     // CONFIG WOLF ===================================
@@ -101,19 +101,20 @@ int main(int argc, char** argv)
     // Set problem PRIOR
     Scalar dt = 0.01;
     TimeStamp   t(0.0);
-    Vector7s    x; x <<  0.0, 0.0, 0.0,   0.0, 0.0, 0.0, 1.0;
+    Vector7s    x; x <<  img_pos.row(0).transpose(), 0.0,   0.0, 0.0, 0.0, 1.0;
     x.segment(3,4).normalize();
     Matrix6s    P = Matrix6s::Identity() * 0.1;
 
     // ====== KF1 ======
-    problem->setPrior(x, P, t, dt/2);
+    FrameBasePtr kf1 = problem->setPrior(x, P, t, dt/2);
 
     // Process capture
     CaptureImagePtr capture_1 = make_shared<CaptureImage>(t, camera, images.at(0));
     camera->process(capture_1);
 
-    FrameBasePtr kf1 = capture_1->getFramePtr();
-    kf1->fix();
+    // Verify KFs
+    FrameBasePtr kf1_check = capture_1->getFramePtr();
+    assert(kf1->id()==kf1_check->id() && "Prior and KF1 are not the same!");
 
     problem->print(1,1,1,0);
 
@@ -122,156 +123,58 @@ int main(int argc, char** argv)
     // ===============================================
 
     t += dt; // increment t
-    x = get_random_state(0,1);
-    x.segment(3,4).normalize();
-    FrameBasePtr kf = problem->emplaceFrame(KEY_FRAME,x,t);
-    problem->keyFrameCallback(kf,nullptr,dt/2.0); // Ack KF creation
+    x << img_pos.row(1).transpose(),0.0,    0.0,0.0,0.0,1.0; // ground truth position
+    FrameBasePtr kf2 = problem->emplaceFrame(KEY_FRAME,x,t);
+    problem->keyFrameCallback(kf2,nullptr,dt/2.0); // Ack KF creation
 
     CaptureImagePtr capture_2 = make_shared<CaptureImage>(t, camera, images.at(1));
+    std::cout << "Capture 2 TS: " << capture_2->getTimeStamp() << std::endl;
     camera->process(capture_2);
 
-    FrameBasePtr kf2 = capture_2->getFramePtr();
+    problem->print(2,1,1,0);
 
-    cv::waitKey(0);                                      // Wait for a keystroke in the window
+    cv::waitKey(0); // Wait for a keystroke in the window
 
     // ===============================================
     // KF3 ===========================================
     // ===============================================
 
     t += dt; // increment t
-    x = get_random_state(0,1);
-    x.segment(3,4).normalize();
-    kf = problem->emplaceFrame(KEY_FRAME,x,t);
-    problem->keyFrameCallback(kf,nullptr,dt/2.0); // Ack KF creation
+    x << img_pos.row(2).transpose(),0.0,    0.0,0.0,0.0,1.0; // ground truth position
+    FrameBasePtr kf3 = problem->emplaceFrame(KEY_FRAME,x,t);
+    problem->keyFrameCallback(kf3,nullptr,dt/2.0); // Ack KF creation
 
     CaptureImagePtr capture_3 = make_shared<CaptureImage>(t, camera, images.at(2));
     camera->process(capture_3);
 
-    FrameBasePtr kf3 = capture_3->getFramePtr();
-
-    cv::waitKey(0);                                      // Wait for a keystroke in the window
+    cv::waitKey(0); // Wait for a keystroke in the window
 
     // ===============================================
     // KF4 ===========================================
     // ===============================================
 
     t += dt; // increment t
-    x = get_random_state(0,1);
-    kf = problem->emplaceFrame(KEY_FRAME,x,t);
-    problem->keyFrameCallback(kf,nullptr,dt/2.0); // Ack KF creation
+    x << img_pos.row(3).transpose(),0.0,    0.0,0.0,0.0,1.0; // ground truth position
+    FrameBasePtr kf4 = problem->emplaceFrame(KEY_FRAME,x,t);
+    problem->keyFrameCallback(kf4,nullptr,dt/2.0); // Ack KF creation
 
     CaptureImagePtr capture_4 = make_shared<CaptureImage>(t, camera, images.at(3));
     camera->process(capture_4);
 
-    FrameBasePtr kf4 = capture_4->getFramePtr();
-
-    cv::waitKey(0);                                      // Wait for a keystroke in the window
-
-    problem->print(1,1,1,0);
-
-//    // ===============================================
-//    // KF5 ===========================================
-//    // ===============================================
-//
-//    t += dt; // increment t
-//    x = get_random_state(0,1);
-//    kf = problem->emplaceFrame(KEY_FRAME,x,t);
-//    problem->keyFrameCallback(kf,nullptr,dt/2.0); // Ack KF creation
-//
-//    CaptureImagePtr capture_5 = make_shared<CaptureImage>(t, camera, images.at(4));
-//    camera->process(capture_5);
-//
-//    FrameBasePtr kf5 = capture_5->getFramePtr();
-//
-//    cv::waitKey(0);                                      // Wait for a keystroke in the window
-//
-//    problem->print(1,1,1,0);
-//
-//    // ===============================================
-//    // KF6 ===========================================
-//    // ===============================================
-//
-//    t += dt; // increment t
-//    x = get_random_state(0,1);
-//    kf = problem->emplaceFrame(KEY_FRAME,x,t);
-//    problem->keyFrameCallback(kf,nullptr,dt/2.0); // Ack KF creation
-//
-//    CaptureImagePtr capture_6 = make_shared<CaptureImage>(t, camera, images.at(5));
-//    camera->process(capture_6);
-//
-//    FrameBasePtr kf6 = capture_6->getFramePtr();
-//
-//    cv::waitKey(0);                                      // Wait for a keystroke in the window
-//
-//    problem->print(1,1,1,0);
-//
-//    // ===============================================
-//    // KF7 ===========================================
-//    // ===============================================
-//
-//    t += dt; // increment t
-//    x = get_random_state(0,1);
-//    kf = problem->emplaceFrame(KEY_FRAME,x,t);
-//    problem->keyFrameCallback(kf,nullptr,dt/2.0); // Ack KF creation
-//
-//    CaptureImagePtr capture_7 = make_shared<CaptureImage>(t, camera, images.at(6));
-//    camera->process(capture_7);
-//
-//    FrameBasePtr kf7 = capture_7->getFramePtr();
-//
-//    cv::waitKey(0);                                      // Wait for a keystroke in the window
-//
-//    problem->print(1,1,1,0);
-//
-//    // ===============================================
-//    // KF8 ===========================================
-//    // ===============================================
-//
-//    t += dt; // increment t
-//    x = get_random_state(0,1);
-//    kf = problem->emplaceFrame(KEY_FRAME,x,t);
-//    problem->keyFrameCallback(kf,nullptr,dt/2.0); // Ack KF creation
-//
-//    CaptureImagePtr capture_8 = make_shared<CaptureImage>(t, camera, images.at(7));
-//    camera->process(capture_8);
-//
-//    FrameBasePtr kf8 = capture_8->getFramePtr();
-//
-//    cv::waitKey(0);                                      // Wait for a keystroke in the window
-//
-//    problem->print(1,1,1,0);
-//
-//    // ===============================================
-//    // KF9 ===========================================
-//    // ===============================================
-//
-//    t += dt; // increment t
-//    x = get_random_state(0,1);
-//    kf = problem->emplaceFrame(KEY_FRAME,x,t);
-//    problem->keyFrameCallback(kf,nullptr,dt/2.0); // Ack KF creation
-//
-//    CaptureImagePtr capture_9 = make_shared<CaptureImage>(t, camera, images.at(8));
-//    camera->process(capture_9);
-//
-//    FrameBasePtr kf9 = capture_9->getFramePtr();
-//
-//    cv::waitKey(0);                                      // Wait for a keystroke in the window
-//
-//    problem->print(1,1,1,0);
-
+    cv::waitKey(0); // Wait for a keystroke in the window
 
     // ===============================================
     // SOLVE PROBLEM (1) =============================
     // ===============================================
 
-    std::string report = ceres_manager->solve(SolverManager::ReportVerbosity::FULL);
+    std::cout << "================== SOLVE 1rst TIME ========================" << std::endl;
 
+    std::string report = ceres_manager->solve(SolverManager::ReportVerbosity::FULL);
     std::cout << report << std::endl;
 
     problem->print(1,1,1,0);
 
-    std::cout << "================== AFTER SOLVE 1rst TIME ========================" << std::endl;
-
+    // Print orientation states for all KFs
     for (auto kf : problem->getTrajectoryPtr()->getFrameList())
         std::cout << wolf::q2v(Eigen::Quaternions(kf->getOPtr()->getState().data())).transpose()*180.0/3.14159 << std::endl;
 
