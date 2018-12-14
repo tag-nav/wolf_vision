320f4519ba71163a6e5938603606e2f1ec9e35eb
Dinesh Atchuthan <a.dinesh@hotmail.fr>
2018-12-14 14:40:10 +0100

    add test_apriltag
    [WIP] added example file for apriltags

---
+++ src/examples/test_apriltag.cpp
@@ -0,0 +1,92 @@
+/**
+ * \file test_apriltag.cpp
+ *
+ *  Created on: Dec 14, 2018
+ *      \author: Dinesh Atchtuhan
+ */
+
+//std
+#include <iostream>
+#include <stdlib.h>
+
+//opencv
+#include <opencv2/imgproc/imgproc.hpp>
+#include "opencv2/opencv.hpp"
+
+//Wolf
+#include "wolf.h"
+#include "ceres_wrapper/ceres_manager.h"
+#include "problem.h"
+#include "sensor_camera.h"
+#include "state_block.h"
+#include "processors/processor_tracker_landmark_apriltag.h"
+#include "capture_image.h"
+
+//#define IMAGE_OUTPUT
+
+bool str_ends_with (std::string const &fullString, std::string const &ending) {
+    if (fullString.length() >= ending.length()) {
+        return (0 == fullString.compare (fullString.length() - ending.length(), ending.length(), ending));
+    } else {
+        return false;
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    using namespace wolf;
+
+#ifdef IMAGE_OUTPUT
+    cv::namedWindow( "Display window", cv::WINDOW_AUTOSIZE );// Create a window for display.
+#endif
+
+    WOLF_INFO( "==================== processor apriltag test ======================" )
+
+    std::string wolf_root = _WOLF_ROOT_DIR;
+    // Wolf problem
+    ProblemPtr problem = Problem::create("PO 3D");
+    SensorBasePtr sen = problem->installSensor("CAMERA", "camera", (Vector7s()<<0,0,0,0,0,0,1).finished(), wolf_root + "/src/examples/camera_params_canonical.yaml");
+    SensorCameraPtr sen_cam = std::static_pointer_cast<SensorCamera>(sen);
+    ProcessorBasePtr prc = problem->installProcessor("TRACKER LANDMARK APRILTAG", "apriltags", "camera", wolf_root + "/src/examples/processor_tracker_landmark_apriltag.yaml");
+    // set prior
+    FrameBasePtr F1 = problem->setPrior(Vector7s::Zero(), Matrix6s::Identity(), 0.0, 0.1);
+    ceres::Solver::Options options;
+    CeresManagerPtr ceres_manager = std::make_shared<CeresManager>(problem, options);
+
+    WOLF_INFO( "====================       Problem set      ======================" )
+
+    // first argument is the name of the program.
+    // following arguments are path to image (from wolf_root)
+    const int inputs = argc -1;
+    WOLF_DEBUG("nb of images %d\n", inputs);
+    cv::Mat frame;
+    Scalar ts(0);
+    CaptureImagePtr cap = std::make_shared<CaptureImage>(ts, sen_cam, frame);
+
+    WOLF_INFO( "====================        Main loop       ======================" )
+    Scalar dt = 1;
+    for (int input = 1; input <= inputs; input++) {
+        ts += dt;
+        frame = cv::imread(wolf_root + argv[input], CV_LOAD_IMAGE_COLOR);
+
+        if( frame.data ) //if imread succeeded
+        {
+#ifdef IMAGE_OUTPUT
+            sleep(500); //wait for 0.5 s
+            imshow( "Display window", frame );  // display original image.
+#endif
+
+            cap->setTimeStamp(ts);
+            // cap->setImage(frame);  TODO: Why is there no setImage() method in CaptureImage class ?
+            sen->process(cap);
+        }
+    }
+
+    problem->print(4,1,1,1);
+    WOLF_INFO( "====================    Solving problem    ======================" )
+    std::string report = ceres_manager->solve(SolverManager::ReportVerbosity::QUIET); // 0: nothing, 1: BriefReport, 2: FullReport
+    problem->print(4,1,1,1);
+
+    return 0;
+
+}
