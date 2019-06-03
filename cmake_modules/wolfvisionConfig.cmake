07bd829b8a58a733a2868dbc48e2eb9fe50518a3
Joaquim Casals <jcasals@iri.upc.edu>
2019-06-03 09:20:07 +0200

    Renamed _INCLUDE_DIR -> _INCLUDE_DIRS & _LIBRARY -> _LIBRARIES

--- cmake_modules/wolfvisionConfig.cmake
+++ cmake_modules/wolfvisionConfig.cmake
@@ -1,33 +1,33 @@
 #edit the following line to add the librarie's header files
 FIND_PATH(
-    wolfvision_INCLUDE_DIR
+    wolfvision_INCLUDE_DIRS
     NAMES vision.found
     PATHS /usr/local/include/iri-algorithms/wolf/plugin_vision)
-IF(wolfvision_INCLUDE_DIR)
-  MESSAGE("Found vision include dirs: ${wolfvision_INCLUDE_DIR}")
-ELSE(wolfvision_INCLUDE_DIR)
+IF(wolfvision_INCLUDE_DIRS)
+  MESSAGE("Found vision include dirs: ${wolfvision_INCLUDE_DIRS}")
+ELSE(wolfvision_INCLUDE_DIRS)
   MESSAGE("Couldn't find vision include dirs")
-ENDIF(wolfvision_INCLUDE_DIR)
+ENDIF(wolfvision_INCLUDE_DIRS)
 
 FIND_LIBRARY(
-    wolfvision_LIBRARY
-    NAMES libwolfvision.so
+    wolfvision_LIBRARIES
+    NAMES libwolfvision.so libwolfvision.dylib
     PATHS /usr/local/lib/iri-algorithms)
-IF(wolfvision_LIBRARY)
-  MESSAGE("Found vision lib: ${wolfvision_LIBRARY}")
-ELSE(wolfvision_LIBRARY)
+IF(wolfvision_LIBRARIES)
+  MESSAGE("Found vision lib: ${wolfvision_LIBRARIES}")
+ELSE(wolfvision_LIBRARIES)
   MESSAGE("Couldn't find wolf vision lib")
-ENDIF(wolfvision_LIBRARY)
+ENDIF(wolfvision_LIBRARIES)
 
-IF (wolfvision_INCLUDE_DIR AND wolfvision_LIBRARY)
+IF (wolfvision_INCLUDE_DIRS AND wolfvision_LIBRARIES)
    SET(wolfvision_FOUND TRUE)
- ELSE(wolfvision_INCLUDE_DIR AND wolfvision_LIBRARY)
+ ELSE(wolfvision_INCLUDE_DIRS AND wolfvision_LIBRARIES)
    set(wolfvision_FOUND FALSE)
-ENDIF (wolfvision_INCLUDE_DIR AND wolfvision_LIBRARY)
+ENDIF (wolfvision_INCLUDE_DIRS AND wolfvision_LIBRARIES)
 
 IF (wolfvision_FOUND)
    IF (NOT wolfvision_FIND_QUIETLY)
-      MESSAGE(STATUS "Found vision: ${wolfvision_LIBRARY}")
+      MESSAGE(STATUS "Found vision: ${wolfvision_LIBRARIES}")
    ENDIF (NOT wolfvision_FIND_QUIETLY)
 ELSE (wolfvision_FOUND)
    IF (wolfvision_FIND_REQUIRED)
@@ -38,7 +38,7 @@ ENDIF (wolfvision_FOUND)
 
 macro(wolf_report_not_found REASON_MSG)
   set(wolfvision_FOUND FALSE)
-  unset(wolfvision_INCLUDE_DIR)
+  unset(wolfvision_INCLUDE_DIRS)
   unset(wolfvision_LIBRARIES)
 
   # Reset the CMake module path to its state when this script was called.
