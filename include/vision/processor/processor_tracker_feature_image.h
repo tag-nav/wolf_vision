b443149d77cb6cb3adb91fe755c57f427484bc94
jcasals <jcasals@iri.upc.edu>
2019-07-15 14:55:35 +0200

    Temporary fix for issue num. 229

--- include/vision/processor/processor_tracker_feature_image.h
+++ include/vision/processor/processor_tracker_feature_image.h
@@ -6,10 +6,10 @@
 #include "vision/capture/capture_image.h"
 #include "vision/feature/feature_point_image.h"
 #include "vision/processor/processor_params_image.h"
+#include "vision/factor/factor_feature_epipolar.h"
 #include <core/state_block/state_block.h>
 #include <core/state_block/state_quaternion.h>
 #include <core/processor/processor_tracker_feature.h>
-#include <core/dummy/factor_feature_dummy.h>
 #include "vision/processor/processor_params_image.h"
 
 
