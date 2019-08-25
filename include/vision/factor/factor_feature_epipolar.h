43dfa71370ade2f47153d03ec877ca355ab45910
Joan Solà <jsola@iri.upc.edu>
2019-08-26 01:19:33 +0200

    Complete factor_epipolar with gtest

--- include/vision/factor/factor_feature_epipolar.h
+++ include/vision/factor/factor_feature_epipolar.h
@@ -1,8 +1,8 @@
 #ifndef FACTOR_FEATURE_EPIPOLAR_H
 #define FACTOR_FEATURE_EPIPOLAR_H
 
-#include "core/factor/factor_autodiff.h"
-#include "core/math/rotations.h"
+#include <core/factor/factor_autodiff.h>
+#include <core/math/rotations.h>
 
 namespace wolf {
 
@@ -26,110 +26,16 @@ class FactorFeatureEpipolar : public FactorAutodiff<FactorFeatureEpipolar, 1, 3,
                          const T* const _frame_other_o,
                          const T* const _sensor_p,
                          const T* const _sensor_o,
-                         T* _residuals) const
-        {
-            using namespace Eigen;
-
-            // Map input and output
-            Map<const Matrix<T, 3, 1>>  p_own   (_frame_own_p);
-            Map<const Quaternion<T>>    q_own   (_frame_own_o);
-            Map<const Matrix<T, 3, 1>>  p_other (_frame_other_p);
-            Map<const Quaternion<T>>    q_other (_frame_other_o);
-            Map<const Matrix<T, 3, 1>>  p_sen   (_sensor_p);
-            Map<const Quaternion<T>>    q_sen   (_sensor_o);
-            Map<Matrix<T, 1, 1>>        residual(_residuals);
-
-            // Compose frames get to camera frames in absolute reference
-            Matrix<T, 3, 1> cam_own_p   = p_own + q_own * p_sen;
-            Quaternion<T> cam_own_q     = q_own * q_sen;
-            Matrix<T, 3, 1> cam_other_p = p_other + q_own * p_sen;
-            Quaternion<T> cam_other_q   = q_other * q_sen;
-
-            // Compute essential matrix
-            /* Essential matrix convention disambiguation -- beware of literature existing conventions are a mess!
-             *
-             * C1 is a camera at the origin frame or reference
-             * C2 is another camera
-             * C2 is specified by R and t wrt C1 so that
-             *   t is the position    of C2 wrt C1
-             *   R is the orientation of C2 wrt C1
-             * There is a 3D point P, noted P1 when expressed in C1 and P2 when expressed in C2:
-             *   P1 = t + R * P2
-             *
-             * Co-planarity condition: a' * (b x c) = 0 with {a,b,c} three co-planar vectors.
-             *
-             * The three vectors are the ones defined by the triangle C1-C2-P at positions 0, t and P1 respectively:
-             *
-             *        b
-             *     0 --- t
-             *       \   |
-             *     r1 \  | r2
-             *         \ |
-             *          P1
-             *
-             *   baseline: b  = t
-             *   ray 1   : r1 = P1
-             *   ray 2   : r2 = P1 - t = R*P2;
-             *
-             * so,
-             *
-             *   (r1)' * (b x r2) = 0 , which develops as:
-             *
-             *   P1' * (t x (R*P2))  = 0
-             *   P1' * [t]x * R * P2 = 0
-             *   P1' * c1Ec2 * P2    = 0 <--- Epipolar factor
-             *
-             * therefore:
-             *   c1Ec2 = [t]x * R        <--- Essential matrix
-             *
-             * If the two cameras are the same with matrix K, and observed pixels are u1 and u2, then
-             *   P1 = K.inv * u1
-             *   P2 = K.inv * u2
-             *
-             * and therefore
-             *   u1 * K.inv.tr * c1Ec2 * K.inv * u2 = 0
-             *   u1 * c1Fc2 * u2 = 0
-             *
-             * where c1Fc2 = K.inv.tr * c1Ec2 * K.inv is the fundamental matrix.
-             */
-
-            Matrix<T, 3, 1> t = cam_own_q.conjugate() * (cam_other_p - cam_own_p);  // other with respect to own
-            Matrix<T, 3, 3> R = (cam_own_q.conjugate() *  cam_other_q).matrix();    // other with respect to own
-
-            // Fundamental matrix
-            Matrix<T, 3, 3> own_F_other = K_inv_.transpose() * wolf::skew(t) * R * K_inv_;
-
-            // own and other pixels
-            Matrix<T, 3, 1> u_own   = getFeature()->getMeasurement().cast<T>();
-            Matrix<T, 3, 1> u_other = getFeatureOther()->getMeasurement().cast<T>();
-
-            // Jacobians of error e = u_own.tr * own_F_other * u_other
-            Matrix<T, 1, 3> J_e_uown    = (own_F_other * u_other).transpose();
-            Matrix<T, 1, 3> J_e_uother  = u_own.transpose() * own_F_other;
-
-            // Covariance of error
-            Matrix<T, 1, 1> Q_e = J_e_uown   * getFeature()     ->getMeasurementCovariance() * J_e_uown.transpose()
-                                + J_e_uother * getFeatureOther()->getMeasurementCovariance() * J_e_uother.transpose();
-
-            // Sqrt of inverse of the covariance is sigma^-1
-            T sigma_inv = 1.0 / sqrt(Q_e(0,0));
-
-            // Final residual
-            // residual = sigma_inv * u_own.transpose() * own_F_other * u_other; // use expression below which is the same but reuses computations
-            residual = sigma_inv * J_e_uother * u_other;
-
-            return true;
-
-        }
+                               T*       _residuals) const;
 
     public:
         static FactorBasePtr create(const FeatureBasePtr& _feature_ptr,
-                                              const NodeBasePtr& _correspondant_ptr,
-                                              const ProcessorBasePtr& _processor_ptr = nullptr);
+                                    const NodeBasePtr& _correspondant_ptr,
+                                    const ProcessorBasePtr& _processor_ptr = nullptr);
 
     private:
         SensorCameraPtr camera_;
-        Matrix3s K_, K_inv_; ///< Intrinsic matrix and its inverse
+        Eigen::Matrix3s K_, K_inv_; ///< Intrinsic matrix and its inverse
 
 };
 
@@ -157,6 +63,101 @@ inline FactorFeatureEpipolar::FactorFeatureEpipolar(const FeatureBasePtr& _featu
     K_      = camera_->getIntrinsicMatrix();
     K_inv_  = K_.inverse();
     //
+}
+
+template<typename T>
+inline bool FactorFeatureEpipolar::operator ()(const T* const _frame_own_p,
+                                               const T* const _frame_own_o,
+                                               const T* const _frame_other_p,
+                                               const T* const _frame_other_o,
+                                               const T* const _sensor_p,
+                                               const T* const _sensor_o,
+                                                     T*       _residuals) const
+{
+    using namespace Eigen;
+    // Map input and output
+    Map<const Matrix<T, 3, 1> > p_own(_frame_own_p);
+    Map<const Quaternion<T> > q_own(_frame_own_o);
+    Map<const Matrix<T, 3, 1> > p_other(_frame_other_p);
+    Map<const Quaternion<T> > q_other(_frame_other_o);
+    Map<const Matrix<T, 3, 1> > p_sen(_sensor_p);
+    Map<const Quaternion<T> > q_sen(_sensor_o);
+    Map<Matrix<T, 1, 1> > residual(_residuals);
+    // Compose frames get to camera frames in absolute reference
+    Matrix<T, 3, 1> cam_own_p = p_own + q_own * p_sen;
+    Quaternion<T> cam_own_q = q_own * q_sen;
+    Matrix<T, 3, 1> cam_other_p = p_other + q_own * p_sen;
+    Quaternion<T> cam_other_q = q_other * q_sen;
+    // Compute essential matrix
+    /* Essential matrix convention disambiguation -- beware of literature existing conventions are a mess!
+     *
+     * C1 is a camera at the origin frame or reference
+     * C2 is another camera
+     * C2 is specified by R and t wrt C1 so that
+     *   t is the position    of C2 wrt C1
+     *   R is the orientation of C2 wrt C1
+     * There is a 3D point P, noted P1 when expressed in C1 and P2 when expressed in C2:
+     *   P1 = t + R * P2
+     *
+     * Co-planarity condition: a' * (b x c) = 0 with {a,b,c} three co-planar vectors.
+     *
+     * The three vectors are the ones defined by the triangle C1-C2-P at positions 0, t and P1 respectively:
+     *
+     *        b
+     *     0 --- t
+     *       \   |
+     *     r1 \  | r2
+     *         \ |
+     *          P1
+     *
+     *   baseline: b  = t
+     *   ray 1   : r1 = P1
+     *   ray 2   : r2 = P1 - t = R*P2;
+     *
+     * so,
+     *
+     *   (r1)' * (b x r2) = 0 , which develops as:
+     *
+     *   P1' * (t x (R*P2))  = 0
+     *   P1' * [t]x * R * P2 = 0
+     *   P1' * c1Ec2 * P2    = 0 <--- Epipolar factor
+     *
+     * therefore:
+     *   c1Ec2 = [t]x * R        <--- Essential matrix
+     *
+     * If the two cameras are the same with matrix K, and observed pixels are u1 and u2, then
+     *   P1 = K.inv * u1
+     *   P2 = K.inv * u2
+     *
+     * and therefore
+     *   u1 * K.inv.tr * c1Ec2 * K.inv * u2 = 0
+     *   u1 * c1Fc2 * u2 = 0
+     *
+     * where c1Fc2 = K.inv.tr * c1Ec2 * K.inv is the fundamental matrix.
+     */
+    Matrix<T, 3, 1> t = cam_own_q.conjugate() * (cam_other_p - cam_own_p); // other with respect to own
+    Matrix<T, 3, 3> R = (cam_own_q.conjugate() * cam_other_q).matrix(); // other with respect to own
+    // Fundamental matrix
+    Matrix<T, 3, 3> own_F_other = K_inv_.transpose() * wolf::skew(t) * R * K_inv_;
+    // own and other pixels
+    Matrix<T, 3, 1> u_own;
+    u_own << getFeature()->getMeasurement().cast<T>(), (T)(1.0);
+    Matrix<T, 3, 1> u_other;
+    u_other << getFeatureOther()->getMeasurement().cast<T>(), (T)(1.0);
+    // Jacobians of error e = u_own.tr * own_F_other * u_other
+    Matrix<T, 1, 3> J_e_uown = (own_F_other * u_other).transpose();
+    Matrix<T, 1, 3> J_e_uother = u_own.transpose() * own_F_other;
+    // Covariance of error
+    Matrix<T, 1, 1> Q_e = J_e_uown.template block<1, 2>(0, 0) * getFeature()->getMeasurementCovariance()
+            * J_e_uown.template block<1, 2>(0, 0).template transpose()
+            + J_e_uother.template block<1, 2>(0, 0) * getFeatureOther()->getMeasurementCovariance()
+                    * J_e_uother.template block<1, 2>(0, 0).template transpose();
+    // Sqrt of inverse of the covariance is sigma^-1
+    T sigma_inv = 1.0 / sqrt(Q_e(0, 0));
+    // Final residual
+    // residual = sigma_inv * u_own.transpose() * own_F_other * u_other; // use expression below which is the same but reuses computations
+    residual = sigma_inv * J_e_uother * u_other;
+    return true;
 }
 
 inline FactorBasePtr FactorFeatureEpipolar::create(const FeatureBasePtr& _feature_ptr, const NodeBasePtr& _correspondant_ptr,
---
+++ test/gtest_factor_epipolar.cpp
@@ -0,0 +1,108 @@
+/**
+ * \file gtest_factor_epipolar.cpp
+ *
+ *  Created on: Aug 25, 2019
+ *      \author: jsola
+ */
+
+#include <core/utils/utils_gtest.h>
+#include <core/problem/problem.h>
+
+#include "vision/internal/config.h"
+
+#include "vision/capture/capture_image.h"
+#include "vision/feature/feature_point_image.h"
+#include "vision/factor/factor_feature_epipolar.h"
+
+#include <core/frame/frame_base.h>
+
+using namespace wolf;
+using namespace Eigen;
+
+TEST(FactorFeatureEpipolar, exemple)
+{
+    Vector7s pose0, pose1, posecam;
+    pose0   << 0,0,0, 0,0,0,1;
+    pose1   << 1,0,0, 0,0,0,1;
+    posecam << 0,1,0, 0,0,0,1;
+
+    Vector2s pix0, pix1;
+    pix0 << 320,240; // central line, central point
+    pix1 << 300,240; // same line but more on the left than pix0.
+
+    auto P  = Problem::create("PO", 3);
+
+    // Install sensor and processor
+    IntrinsicsCameraPtr intr      = std::make_shared<IntrinsicsCamera>();
+    intr->pinhole_model_raw       = Eigen::Vector4s(320,240,320,320);
+    intr->pinhole_model_rectified = Eigen::Vector4s(320,240,320,320);
+    intr->width  = 640;
+    intr->height = 480;
+    auto S      = P->installSensor("CAMERA", "camera", posecam, intr);
+    auto camera = std::static_pointer_cast<SensorCamera>(S);
+
+    auto F0 = P             ->emplaceFrame(KEY, pose0, 0.0);
+    auto F1 = P             ->emplaceFrame(KEY, pose1, 1.0);
+    auto C0 = CaptureBase   ::emplace<CaptureImage>(F0, F0->getTimeStamp(), camera, cv::Mat());
+    auto C1 = CaptureBase   ::emplace<CaptureImage>(F1, F1->getTimeStamp(), camera, cv::Mat());
+    auto f0 = FeatureBase   ::emplace<FeaturePointImage>(C0, pix0, 0, cv::Mat(), Matrix2s::Identity());
+    auto f1 = FeatureBase   ::emplace<FeaturePointImage>(C1, pix1, 0, cv::Mat(), Matrix2s::Identity());
+    auto c  = FactorBase    ::emplace<FactorFeatureEpipolar>(f0, f0, f1, nullptr, false);
+
+    Scalar residual;
+
+    c->operator()(F0->getP()->getState().data(),
+                  F0->getO()->getState().data(),
+                  F1->getP()->getState().data(),
+                  F1->getO()->getState().data(),
+                  camera->getP()->getState().data(),
+                  camera->getO()->getState().data(),
+                  &residual);
+
+    WOLF_TRACE("residual @  0 pix: ", residual);
+
+    f1->setMeasurement(Vector2s(300, 241));
+
+    c->operator()(F0->getP()->getState().data(),
+                  F0->getO()->getState().data(),
+                  F1->getP()->getState().data(),
+                  F1->getO()->getState().data(),
+                  camera->getP()->getState().data(),
+                  camera->getO()->getState().data(),
+                  &residual);
+
+    WOLF_TRACE("residual @  1 pix : ", residual);
+
+    f1->setMeasurement(Vector2s(300, 242));
+
+    c->operator()(F0->getP()->getState().data(),
+                  F0->getO()->getState().data(),
+                  F1->getP()->getState().data(),
+                  F1->getO()->getState().data(),
+                  camera->getP()->getState().data(),
+                  camera->getO()->getState().data(),
+                  &residual);
+
+    WOLF_TRACE("residual @  2 pix : ", residual);
+
+    f1->setMeasurement(Vector2s(300, 239));
+
+    c->operator()(F0->getP()->getState().data(),
+                  F0->getO()->getState().data(),
+                  F1->getP()->getState().data(),
+                  F1->getO()->getState().data(),
+                  camera->getP()->getState().data(),
+                  camera->getO()->getState().data(),
+                  &residual);
+
+    WOLF_TRACE("residual @ -1 pix : ", residual);
+
+}
+
+int main(int argc, char **argv)
+{
+  testing::InitGoogleTest(&argc, argv);
+//::testing::GTEST_FLAG(filter) = "TestGroup.DummyTestExample";
+  return RUN_ALL_TESTS();
+}
+
