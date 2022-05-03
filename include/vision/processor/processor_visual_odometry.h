245ef65171b705c8920d70f7d33c82f9f33d2b19
Mederic Fourmy <mederic.fourmy@gmail.com>
2022-05-03 19:26:15 +0200

    Remove useless include

--- include/vision/processor/processor_visual_odometry.h
+++ include/vision/processor/processor_visual_odometry.h
@@ -41,7 +41,6 @@
 // Opencv includes
 #include <opencv2/core.hpp>
 #include <opencv2/imgcodecs.hpp>
-#include <opencv2/highgui.hpp>
 #include <opencv2/features2d.hpp>
 #include <opencv2/calib3d/calib3d.hpp>
 #include <opencv2/video/tracking.hpp>
